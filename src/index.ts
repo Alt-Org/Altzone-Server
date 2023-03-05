c057fdac281ffa3c6487b10f19218dbdb5a2ec40
MikhailDeriabin <mikko.deriabin@gmail.com>
2023-03-06 01:37:45 +0200

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import {connect as connectToDB, set as mongooseSet } from 'mongoose';
 import dotenv from 'dotenv'
-dotenv.config({ path: './.env' });
+dotenv.config();
 
 //Routes imports
 import { rootRouter } from './root';
@@ -11,9 +11,9 @@ const app = express();
 
 // Set up database connection
 const mongoString = `${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}` || 'mongodb://127.0.0.1:27017/altzone';
-console.log('mongoString: ', mongoString);
+
 mongooseSet('strictQuery', true);
-connectToDB('mongodb://127.0.0.1:27017/altzone').then(
+connectToDB(mongoString).then(
     ()=>{ console.log('Connected to DB successfully'); },
     (err)=>{ console.log(err); }
 );
