201952d9cfb5abd466b751e3ad2e5b929c8bca11
Mikhail <mikko.deriabin@gmail.com>
2023-03-19 16:32:45 +0200

    add parser, which converts game fields names to API

--- src/characterClass/characterClass.parser.ts
+++ src/characterClass/characterClass.parser.ts
@@ -1,7 +1,37 @@
 import {NextFunction, Request, Response} from "express";
 
-export default class CharacterClassParser{
-    public parseCreate = (req: Request, res: Response, next: NextFunction) => {
+const gameToAPI: any = {
+    CharacterClassId: "gameId",
+    Name: "name",
+    DefenceClass: "mainDefence",
+    Speed: "speed",
+    Resistance: "resistance",
+    Attack: "attack",
+    Defence: "defence"
+}
 
+export default class CharacterClassParser{
+
+    constructor(dictionary: any) {
+        this.dictionary = dictionary;
+    }
+
+    private readonly dictionary: any;
+
+    public parseFromGameToAPI = (req: Request, res: Response, next: NextFunction) => {
+        const body = req.body;
+        const bodyKeys = Object.keys(body);
+
+        for(let i=0; i<bodyKeys.length; i++){
+            const currentKey = bodyKeys[i];
+            const apiAnalog = this.dictionary[currentKey];
+
+            if(apiAnalog !== undefined)
+                body[apiAnalog] = body[currentKey];
+            else
+                console.error(`Warning: field ${currentKey} is not recognized`);
+        }
+
+        next();
     }
 }
