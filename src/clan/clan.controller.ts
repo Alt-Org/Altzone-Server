bcaea8aad8207e9c31ecd3a63d35076aa813e5cd
Mikhail <mikko.deriabin@gmail.com>
2023-03-22 16:14:30 +0200

    add parser for /clan output

--- src/clan/clan.controller.ts
+++ src/clan/clan.controller.ts
@@ -1,17 +1,19 @@
-import {ICreateClanInput, IUpdateClanInput} from "./clan";
 import {Request, Response } from "express";
 import { sendErrorsToClient } from "../util/response/errorHandler";
 import ClanService from "./clan.service";
 import DefaultResponseErrorThrower from "../util/response/defaultResponseErrorThrower";
+import {FieldParserFactory, ParserType} from "../util/parser";
 
 const clanService = new ClanService();
 const errorThrower = new DefaultResponseErrorThrower();
+const parser = new FieldParserFactory().createParser(ParserType.CLAN);
 
 export default class ClanController{
     create = async (req: Request, res: Response): Promise<void> => {
         try{
-            const result = await clanService.create(req.body);
+            const respObj = await clanService.create(req.body);
 
+            const result = parser.parseFromAPIToGame(respObj);
             res.status(201).json(result);
         }catch (err) {
             sendErrorsToClient(err, res);
@@ -20,9 +22,10 @@ export default class ClanController{
 
     get = async (req: Request, res: Response): Promise<void> => {
         try{
-            const result = await clanService.readById(req.params._id);
-            errorThrower.throwReadErrorsIfFound(result, 'Clan', '_id');
+            const respObj = await clanService.readById(req.params._id);
+            errorThrower.throwReadErrorsIfFound(respObj, 'Clan', '_id');
 
+            const result = parser.parseFromAPIToGame(respObj);
             res.status(200).json(result);
         }catch (err) {
             sendErrorsToClient(err, res);
@@ -31,9 +34,10 @@ export default class ClanController{
 
     getAll = async (req: Request, res: Response): Promise<void> => {
         try{
-            const result = await clanService.readAll();
-            errorThrower.throwReadErrorsIfFound(result, 'Clan', '_id');
+            const respObj = await clanService.readAll();
+            errorThrower.throwReadErrorsIfFound(respObj, 'Clan', '_id');
 
+            const result = parser.parseFromAPIToGame(respObj);
             res.status(200).json(result);
         }catch (err) {
             sendErrorsToClient(err, res);
@@ -42,8 +46,8 @@ export default class ClanController{
 
     update = async (req: Request, res: Response): Promise<void> => {
         try{
-            const result = await clanService.updateById(req.body);
-            errorThrower.throwUpdateErrorsIfFound(result, 'Clan', '_id');
+            const respObj = await clanService.updateById(req.body);
+            errorThrower.throwUpdateErrorsIfFound(respObj, 'Clan', '_id');
 
             res.status(204).send();
         }catch (err: unknown) {
@@ -53,8 +57,8 @@ export default class ClanController{
 
     delete = async (req: Request, res: Response): Promise<void> => {
         try{
-            const result = await clanService.deleteById(req.params._id);
-            errorThrower.throwDeleteErrorsIfFound(result, 'Clan', '_id');
+            const respObj = await clanService.deleteById(req.params._id);
+            errorThrower.throwDeleteErrorsIfFound(respObj, 'Clan', '_id');
 
             res.status(204).send();
         }catch (err) {
