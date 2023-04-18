78765dcc647cdd3346f1fa88f96697e3ca6542d7
Mikhail <mikko.deriabin@gmail.com>
2023-04-18 20:24:18 +0300

    connect Clan and ClanFurniture collections

--- src/clan/clan.model.ts
+++ src/clan/clan.model.ts
@@ -12,9 +12,25 @@ const schema = new Schema({
 schema.pre('deleteOne', { document: false, query: true },async function () {
     const {_id} = this.getQuery();
 
+    //Null all playerData references
     const playerDataModel = mongoose.model(ClassName.PLAYER_DATA);
     const nullIds = { clan_id: null };
     await playerDataModel.updateMany({clan_id: _id}, nullIds);
+
+    //Delete all ClanFurniture objects related to the Clan
+    const clanFurnitureModel = mongoose.model(ClassName.CLAN_FURNITURE);
+    await clanFurnitureModel.deleteMany({clan_id: _id});
+});
+
+schema.pre('deleteMany', { document: false, query: true },async function () {
+    const {_id} = this.getQuery();
+
+    const playerDataModel = mongoose.model(ClassName.PLAYER_DATA);
+    const nullIds = { clan_id: null };
+    await playerDataModel.updateMany({clan_id: _id}, nullIds);
+
+    const clanFurnitureModel = mongoose.model(ClassName.CLAN_FURNITURE);
+    await clanFurnitureModel.deleteMany({clan_id: _id});
 });
 
 export default mongoose.model<IClan>(ClassName.CLAN, schema);
