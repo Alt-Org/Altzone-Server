32d38541c9ac39cbe1cba4633a1a3ceea823484a
Mikhail <mikko.deriabin@gmail.com>
2023-05-14 22:44:43 +0300

    add all models to modelFactory

--- src/util/baseAPIClasses/factory/modelFactory.ts
+++ src/util/baseAPIClasses/factory/modelFactory.ts
@@ -1,15 +1,30 @@
 import {Model} from "mongoose";
 import {ClassName} from "../../dictionary";
 import PlayerDataModel from "../../../playerData/playerData.model";
+import CharacterClassModel from "../../../characterClass/characterClass.model";
+import ClanModel from "../../../clan/clan.model";
+import CustomCharacterModel from "../../../customCharacter/customCharacter.model";
+import BattleCharacterModel from "../../../battleCharacter/battleCharacter.model";
+import FurnitureModel from "../../../furniture/furniture.model";
+import RaidRoomModel from "../../../raidRoom/raidRoom.model";
 
-//TODO: remove default from switch
 export default class ModelFactory {
     public create = (modelName: ClassName): Model<any> => {
         switch (modelName) {
+            case ClassName.CHARACTER_CLASS:
+                return CharacterClassModel;
+            case ClassName.CLAN:
+                return ClanModel;
+            case ClassName.CUSTOM_CHARACTER:
+                return CustomCharacterModel;
             case ClassName.PLAYER_DATA:
                 return PlayerDataModel;
-            default:
-                return PlayerDataModel;
+            case ClassName.BATTLE_CHARACTER:
+                return BattleCharacterModel;
+            case ClassName.FURNITURE:
+                return FurnitureModel;
+            case ClassName.RAID_ROOM:
+                return RaidRoomModel;
         }
     }
 }
