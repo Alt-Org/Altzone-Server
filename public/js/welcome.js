fd8180f41883eb311ddc1004dd22ad9a44dba103
Mikhail <mikko.deriabin@gmail.com>
2023-06-05 18:49:48 +0300

    convert imgs format to webp

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,7 +1,7 @@
 const mainImg = document.querySelector("#mainImg");
 
 const imgPathBase = 'public/images/';
-const mainImgPaths = ['catMeme1.jpg', 'catMeme2.png', 'catMeme3.jpg', 'catMeme4.jpg', 'catMeme5.jpg'];
+const mainImgPaths = ['catMeme1.webp', 'catMeme2.webp', 'catMeme3.webp', 'catMeme4.webp', 'catMeme5.webp'];
 
 //Get random mainImg each time the page download
 const mainImgIndex = Math.floor(Math.random() * mainImgPaths.length);
