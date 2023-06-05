25a75a54e9801e45afcfa3161e4649ee4fe9bd86
Mikhail <mikko.deriabin@gmail.com>
2023-06-05 15:02:28 +0300

    remove unused dictionary

--- src/util/dictionary/index.ts
+++ src/util/dictionary/index.ts
@@ -1,5 +1,4 @@
-import Dictionary from "./dictionary";
 import {ClassName} from "./className";
 import CollectionRefs from "./collectionRefs";
 
-export { Dictionary, ClassName, CollectionRefs };
+export { ClassName, CollectionRefs };
