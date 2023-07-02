870606e2ab5992555613f99044a9a727254e53ab
Mikhail <mikko.deriabin@gmail.com>
2023-07-02 16:00:34 +0300

    fix bugs

--- src/base/base.module.ts
+++ src/base/base.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { BaseService } from './base.service';
 
 @Module({
 })
--- src/base/base.service.ts
+++ src/base/base.service.ts
@@ -9,27 +9,30 @@ import {Injectable} from "@nestjs/common";
 @Injectable()
 export abstract class BaseService<T> implements IService{
 
-    protected constructor() {
+    protected constructor(isWarningOn: boolean = true) {
         this.refsInModel = [];
+        this.isWarningOn = isWarningOn;
     }
 
-    protected baseModel: Model<T>;
+    public model: Model<T>;
     protected refsInModel: ClassName[];
+    private readonly isWarningOn: boolean;
 
     public create = async (input: any): Promise<object | MongooseError> => {
         this.checkClassValidity();
-        return this.baseModel.create(input);
+        return this.model.create(input);
     }
 
+
     public readById = async (_id: string): Promise<object | null | MongooseError> => {
-        this.checkClassValidity();
-        return this.baseModel.findById(_id);
+        //this.checkClassValidity();
+        return this.model.findById(_id);
     }
 
     public readOneWithCollections = async (_id: string, withQuery: string): Promise<object | null | MongooseError> => {
         this.checkClassValidity();
         const withRefs: ClassName[] = withQuery.split('_') as ClassName[];
-        const dbQuery = this.baseModel.findById(_id);
+        const dbQuery = this.model.findById(_id);
 
         if(withRefs.length === 0)
             return dbQuery;
@@ -49,7 +52,7 @@ export abstract class BaseService<T> implements IService{
 
     public readOneWithAllCollections = async (_id: string): Promise<object | null | MongooseError> => {
         this.checkClassValidity();
-        const dbQuery = this.baseModel.findById(_id) as any;
+        const dbQuery = this.model.findById(_id) as any;
 
         for(let i=0; i<this.refsInModel.length; i++)
             dbQuery.populate(this.refsInModel[i]);
@@ -64,28 +67,28 @@ export abstract class BaseService<T> implements IService{
 
     public readAll = async (): Promise<Array<object>> => {
         this.checkClassValidity();
-        return this.baseModel.find();
+        return this.model.find();
     }
 
     public updateById = async (input: any): Promise<object | MongooseError> => {
         this.checkClassValidity();
-        return this.baseModel.updateOne({_id: input._id}, input, {rawResult: true});
+        return this.model.updateOne({_id: input._id}, input, {rawResult: true});
     }
 
     public deleteById = async (_id: string, ignoreReferences?: IgnoreReferencesType): Promise<Object | null | MongooseError> => {
         this.checkClassValidity();
-        const entityToDelete = await this.baseModel.findById(_id);
+        const entityToDelete = await this.model.findById(_id);
         if(!entityToDelete)
             return null;
 
         await this.clearCollectionReferences(_id, ignoreReferences);
 
-        return this.baseModel.deleteOne({_id});
+        return this.model.deleteOne({_id});
     }
 
     public deleteByCondition = async (condition: object, ignoreReferences?: IgnoreReferencesType): Promise<Object | null | MongooseError> => {
         this.checkClassValidity();
-        const entityToDelete = await this.baseModel.findOne(condition);
+        const entityToDelete = await this.model.findOne(condition);
         if(!entityToDelete)
             return null;
 
@@ -93,18 +96,18 @@ export abstract class BaseService<T> implements IService{
 
         await this.clearCollectionReferences(_id, ignoreReferences);
 
-        return this.baseModel.deleteOne({_id: entityToDelete._id});
-    }
-    public setBaseModel = (model: Model<T>) => {
-        this.baseModel = model;
-    }
-    protected setRefsInModel = (refsInModel: ClassName[]) => {
-        this.refsInModel = refsInModel;
+        return this.model.deleteOne({_id: entityToDelete._id});
     }
 
     private checkClassValidity = () => {
-        if(!this.baseModel)
-            throw new Error('Model is not defined for BaseService class. Please set the model for the class with setBaseModel() before use');
+        if(!this.model)
+            throw new Error('Model is not defined for BaseService class. Please set the model for the extending class');
+        if(this.isWarningOn && this.refsInModel.length === 0)
+            console.log(
+                'Warning: the field refsInModel[] is not specified, that may cause problems with querying related collections. \n' +
+                'Ignore this warning, if there is no references to the model. \n' +
+                'You may want to hide this warning by providing false in the BaseService constructor function.'
+            );
     }
 
     protected abstract clearCollectionReferences(_id: string | Types.ObjectId, ignoreReferences?: IgnoreReferencesType): Promise<void>;
