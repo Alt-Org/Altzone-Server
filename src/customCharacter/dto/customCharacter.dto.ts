5f92cfbcbe374dd643f1a16469bc3d208d529c21
Mikhail <mikko.deriabin@gmail.com>
2023-07-10 11:58:09 +0300

    add model references' fields to customCharacter DTOs

--- src/customCharacter/dto/customCharacter.dto.ts
+++ src/customCharacter/dto/customCharacter.dto.ts
@@ -1,5 +1,7 @@
-import {Expose} from "class-transformer";
+import {Expose, Type} from "class-transformer";
 import {ExtractField} from "../../common/decorator/response/ExtractField";
+import {PlayerDto} from "../../player/dto/player.dto";
+import {CharacterClassDto} from "../../characterClass/dto/characterClass.dto";
 
 export class CustomCharacterDto {
     @ExtractField()
@@ -31,4 +33,12 @@ export class CustomCharacterDto {
     @ExtractField()
     @Expose()
     player_id: string;
+
+    @Type(() => CharacterClassDto)
+    @Expose()
+    CharacterClass: CharacterClassDto;
+
+    @Type(() => PlayerDto)
+    @Expose()
+    Player: PlayerDto;
 }
