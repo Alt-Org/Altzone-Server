ca6c7a047e6c79542acdf5e5196f5f9ea13fcf3e
Mikhail <mikko.deriabin@gmail.com>
2023-07-13 13:11:01 +0300

    rename IService

--- src/common/base/interface/IBasicService.ts
+++ src/common/base/interface/IBasicService.ts
@@ -2,13 +2,16 @@ import {MongooseError} from "mongoose";
 import {IgnoreReferencesType} from "../../type/ignoreReferences.type";
 import {DeleteOptionsType} from "../type/deleteOptions.type";
 
-export interface IService {
+export interface IBasicService {
     create(input: any): Promise<object | MongooseError>;
+
     readById(_id: string): Promise<object | null | MongooseError>;
     readOneWithCollections(_id: string, withQuery: string): Promise<Object | null | MongooseError>;
     readOneWithAllCollections(_id: string): Promise<object | null | MongooseError>;
     readAll(): Promise<Array<object>>;
+
     updateById(input: any): Promise<object | MongooseError>;
+
     deleteById(_id: string, ignoreReferences?: IgnoreReferencesType): Promise<object | MongooseError>;
     deleteByCondition(condition: object, options?: DeleteOptionsType, ignoreReferences?: IgnoreReferencesType): Promise<object | MongooseError>;
 }
