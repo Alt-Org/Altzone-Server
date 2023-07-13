68abed1dd01fddf59da307086f86100dcd750810
Mikhail <mikko.deriabin@gmail.com>
2023-07-13 13:17:40 +0300

    improve ThrowUnimplementedError decorator

--- src/common/base/decorator/ThrowNotImplementedMethod.decorator.ts
+++ src/common/base/decorator/ThrowNotImplementedMethod.decorator.ts
@@ -1,9 +1,18 @@
-export const ThrowNotImplementedMethod = (className: string): any => {
+export const ThrowNotImplementedMethod = (className: string, decoratorName: string | string[]): any => {
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
         descriptor.value = function () {
+            let decorator = '';
+            if(decoratorName instanceof Array){
+                for(let i=0; i<decoratorName.length; i++){
+                    const currentDecorator = '@' + decoratorName[i] + '() ';
+                    decorator += currentDecorator;
+                }
+            } else
+                decorator = '@' + decoratorName + '() ';
+
             const notImplementedError = new Error(
                 `You are trying to use ${propertyKey}() of ${className} class, which does implement any methods by itself.\n` +
-                `You have to implement the method yourself or use the @AddBaseService() with a class, you are extending with ${className} class`
+                `You have to implement the method yourself or use the ${decorator}with a class, you are extending with ${className} class`
             );
 
             return Promise.reject(() => { throw notImplementedError});
