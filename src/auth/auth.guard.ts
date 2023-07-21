1f7eab8e3a2602b511f64a1a25e98b574ee85b63
Mikhail <mikko.deriabin@gmail.com>
2023-07-21 13:47:12 +0300

    improve Unauthorized errors msg

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,17 +7,35 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constant';
 import { Request } from 'express';
+import {Reflector} from "@nestjs/core";
+import {NO_AUTH_REQUIRED} from "./decorator/NoAuth";
+
+//TODO: remove or change error messages to less specific for production
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    public constructor(private jwtService: JwtService) {}
+    public constructor(
+        private readonly jwtService: JwtService,
+        private readonly reflector: Reflector
+    ) {}
 
     public async canActivate(context: ExecutionContext): Promise<boolean> {
+        const noAuthRequired = this.reflector.getAllAndOverride(NO_AUTH_REQUIRED, [
+            context.getHandler(),
+            context.getClass()
+        ]);
+        if(noAuthRequired)
+            return true;
+
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
-        if (!token) {
-            throw new UnauthorizedException();
-        }
+        if (!token)
+            throw new UnauthorizedException(
+                'The access token is not provided. ' +
+                'Please add `authorization` field with access token(in bearer token form): ' +
+                '`Bearer access-token-here` to request header. The access token you can get from /auth endpoint'
+            );
+
         try {
             const payload = await this.jwtService.verifyAsync(
                 token,
@@ -28,8 +46,13 @@ export class AuthGuard implements CanActivate {
 
             request['profile'] = payload;
         } catch {
-            throw new UnauthorizedException();
+            throw new UnauthorizedException(
+                'The access token is expired or invalid. ' +
+                'Please update `authorization` field with access token(in bearer token form): ' +
+                '`Bearer access-token-here` in your request header. The access token you can get from /auth endpoint'
+            );
         }
+
         return true;
     }
 
