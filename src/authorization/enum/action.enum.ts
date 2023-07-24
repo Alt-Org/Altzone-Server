2e296282f932bfacf0d21d1d40be5a80fd2968a5
Mikhail <mikko.deriabin@gmail.com>
2023-07-24 14:39:15 +0300

    add new action types

--- src/authorization/enum/action.enum.ts
+++ src/authorization/enum/action.enum.ts
@@ -1,7 +1,17 @@
 export enum Action {
-    MANAGE = 'manage',
-    CREATE = 'create',
-    READ = 'read',
-    UPDATE = 'update',
-    DELETE = 'delete',
+    manage = 'manage',
+    create = 'create',
+    read = 'read',
+    update = 'update',
+    delete = 'delete',
+
+    create_request = 'create_request',
+    read_request = 'read_request',
+    update_request = 'update_request',
+    delete_request = 'delete_request',
+
+    create_response = 'create_response',
+    read_response = 'read_response',
+    update_response = 'update_response',
+    delete_response = 'delete_response',
 }
