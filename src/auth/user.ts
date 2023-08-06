8373f8a94f0d6580b52f6d6f8e2f271264f0fd75
Mikhail <mikko.deriabin@gmail.com>
2023-08-06 13:59:41 +0300

    add isSystemAdmin to user class

--- src/auth/user.ts
+++ src/auth/user.ts
@@ -1,11 +1,13 @@
 export class User{
-    public constructor(username: string, profile_id: string, player_id: string) {
+    public constructor(username: string, profile_id: string, player_id: string, isSystemAdmin: boolean = false) {
         this.username = username;
         this.profile_id = profile_id;
         this.player_id = player_id;
+        this.isSystemAdmin = isSystemAdmin;
     }
 
     public readonly username: string;
     public readonly profile_id: string;
     public readonly player_id: string;
+    public readonly isSystemAdmin: boolean;
 }
