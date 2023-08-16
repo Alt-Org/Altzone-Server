a7fee93b79b363b1be2161435c0430a0ea89557c
Mikhail <mikko.deriabin@gmail.com>
2023-08-16 14:56:21 +0300

    add clan_id field

--- src/auth/user.ts
+++ src/auth/user.ts
@@ -1,13 +1,13 @@
 export class User{
-    public constructor(username: string, profile_id: string, player_id: string, isSystemAdmin: boolean = false) {
-        this.username = username;
+    public constructor(profile_id: string, player_id: string, clan_id?: string, isSystemAdmin: boolean = false) {
         this.profile_id = profile_id;
         this.player_id = player_id;
+        this.clan_id = clan_id;
         this.isSystemAdmin = isSystemAdmin;
     }
 
-    public readonly username: string;
     public readonly profile_id: string;
     public readonly player_id: string;
+    public readonly clan_id: string;
     public readonly isSystemAdmin: boolean;
 }
