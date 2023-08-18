41f4cb6230bd2e5fc69765f9ff8dcfb748f30ce8
Mikhail <mikko.deriabin@gmail.com>
2023-08-18 13:03:22 +0300

    remove password from res body for system admin

--- src/authorization/rule/systemAdminRules.ts
+++ src/authorization/rule/systemAdminRules.ts
@@ -1,10 +1,12 @@
 import {AbilityBuilder, createMongoAbility, ExtractSubjectType} from "@casl/ability";
 import {Action} from "../enum/action.enum";
+import {ProfileDto} from "../../profile/dto/profile.dto";
 
 export const systemAdminRules = () => {
     const { can, build } = new AbilityBuilder<any>(createMongoAbility);
 
     can(Action.manage, 'all');
+    can(Action.read_response, ProfileDto, ['_id', 'username', 'Player']);
 
     return build({
         detectSubjectType: (item) =>
