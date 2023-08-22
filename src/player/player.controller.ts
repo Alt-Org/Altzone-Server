45bc5470995154c383ad982931d2dd42c28823f7
Mikhail <mikko.deriabin@gmail.com>
2023-08-22 15:28:48 +0300

    add to player delete method logic

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, Query, Req} from "@nestjs/common";
+import {BadRequestException, Body, Controller, Delete, Get, Param, Post, Put, Query, Req} from "@nestjs/common";
 import {PlayerService} from "./player.service";
 import {CreatePlayerDto} from "./dto/createPlayer.dto";
 import {UpdatePlayerDto} from "./dto/updatePlayer.dto";
@@ -15,10 +15,15 @@ import {CatchCreateUpdateErrors} from "../common/decorator/response/CatchCreateU
 import {Serialize} from "../common/interceptor/response/Serialize";
 import {Authorize} from "../authorization/decorator/Authorize";
 import {Action} from "../authorization/enum/action.enum";
+import {ClanDto} from "../clan/dto/clan.dto";
+import {RequestHelperService} from "../requestHelper/requestHelper.service";
 
 @Controller('player')
 export default class PlayerController{
-    public constructor(private readonly service: PlayerService) {
+    public constructor(
+        private readonly service: PlayerService,
+        private readonly requestHelperService: RequestHelperService
+    ) {
     }
 
     @NoAuth()
@@ -55,6 +60,39 @@ export default class PlayerController{
     @Authorize({action: Action.delete, subject: PlayerDto})
     @BasicDELETE(ModelName.PLAYER)
     public async delete(@Param() param: _idDto) {
+        const clansWithPlayerAsAdmin: ClanDto[] = await this.requestHelperService.getModelInstanceByCondition(
+            ModelName.CLAN,
+            {admin_ids: {$in: [param._id]}},
+            ClanDto
+        );
+
+        if(clansWithPlayerAsAdmin && clansWithPlayerAsAdmin.length > 0){
+            //Check that there will be no clans left without admins
+            let isLastAdmin = false;
+            let clan_idLastAdmin: string;
+            for(let i=0; i<clansWithPlayerAsAdmin.length; i++){
+                const currentClan = clansWithPlayerAsAdmin[i];
+                if(currentClan.admin_ids.length === 1){
+                    isLastAdmin = true;
+                    clan_idLastAdmin = currentClan._id;
+                    break;
+                }
+            }
+
+            if(isLastAdmin){
+                throw new BadRequestException(
+                    `Player can not be deleted, because it is the only one admin in clan with _id '${clan_idLastAdmin}'. ` +
+                    `Please add another admin to this clan before deleting this Player or delete this clan first.`
+                );
+            }
+
+            for(let i=0; i<clansWithPlayerAsAdmin.length; i++){
+                const currentClan = clansWithPlayerAsAdmin[i];
+                const newAdmin_ids = currentClan.admin_ids.filter(value => value !== param._id);
+                await this.requestHelperService.updateOneById(ModelName.CLAN, currentClan._id, {admin_ids: newAdmin_ids})
+            }
+        }
+
         return this.service.deleteOneById(param._id);
     }
 }
