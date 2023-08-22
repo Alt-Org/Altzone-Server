07e7520ce0ae61db8444f74612ebdb03e4bb6866
Mikhail <mikko.deriabin@gmail.com>
2023-08-22 15:26:14 +0300

    fix bug with determining is logged user clan admin

--- src/authorization/rule/clanRules.ts
+++ src/authorization/rule/clanRules.ts
@@ -3,30 +3,35 @@ import {AbilityBuilder, createMongoAbility, ExtractSubjectType} from "@casl/abil
 import {Action} from "../enum/action.enum";
 import {InferSubjects, MongoAbility} from "@casl/ability/dist/types";
 import {RulesSetterAsync} from "../type/RulesSetter.type";
-import { ClanDto } from "../../clan/dto/clan.dto";
-import { UpdateClanDto } from "../../clan/dto/updateClan.dto";
-import { ModelName } from "src/common/enum/modelName.enum";
+import {ClanDto} from "../../clan/dto/clan.dto";
+import {UpdateClanDto} from "../../clan/dto/updateClan.dto";
+import {ModelName} from "../../common/enum/modelName.enum";
+import {MongooseError} from "mongoose";
+import {NotFoundException} from "@nestjs/common";
 
 type Subjects = InferSubjects<typeof ClanDto | typeof UpdateClanDto>;
 type Ability = MongoAbility<[AllowedAction | Action.manage, Subjects | 'all']>;
+export const clanRules: RulesSetterAsync<Ability, Subjects> = async (user, subject: any, action, subject_id, requestHelperService) => {
+    const { can, build } = new AbilityBuilder<Ability>(createMongoAbility);
 
-export const clanRules: RulesSetterAsync<Ability, Subjects> = async (user, subject, requestHelperService) => {
-    const { can, cannot, build } = new AbilityBuilder<Ability>(createMongoAbility);
-
-    if(subject === ClanDto){
+    if(action === Action.create || action === Action.read){
         can(Action.create_request, subject);
 
-        //const publicFields = ['_id', 'name', 'uniqueIdentifier'];
+        // const publicFields = ['_id', 'name', 'uniqueIdentifier'];
         can(Action.read_request, subject);
         can(Action.read_response, subject, {_id: user.clan_id});
     }
 
-    if(subject === UpdateClanDto){
-        const clan: ClanDto = await requestHelperService.getModelInstanceById(ModelName.CLAN, user.clan_id, ClanDto);
-        const isClanAdmin = clan.admin_ids.includes(user.player_id);
-        if(isClanAdmin){
-            can(Action.update_request, subject);
-            can(Action.delete_request, subject);
+    if(action === Action.update || action === Action.delete){
+        const clan: ClanDto = await requestHelperService.getModelInstanceById(ModelName.CLAN, subject_id, ClanDto);
+        if(clan && !(clan instanceof MongooseError)){
+            const isClanAdmin = clan.admin_ids.includes(user.player_id);
+            if(isClanAdmin){
+                can(Action.update_request, subject);
+                can(Action.delete_request, subject);
+            }
+        } else {
+            throw new NotFoundException('The clan with that _id is not found');
         }
     }
 
