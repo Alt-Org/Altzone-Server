27cd43f6322764f508dd9673d445376ffc0be510
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2023-11-18 17:08:54 +0200

    add random values generators

--- src/util/dataMock/playerMock.ts
+++ src/util/dataMock/playerMock.ts
@@ -17,6 +17,50 @@ export class PlayerMock {
         return newPlayer;
     }
 
+    public static generatePlayerWithoutFields(fieldsToEscape: string[]): Partial<PlayerDto> {
+        const player = this.generatePlayer();
+        let result = {};
+        for(let field in player){
+            if(!fieldsToEscape.includes(field)){
+                result[field] = player[field];
+            }
+        }
+
+        return result;
+    }
+
+    public static generatePlayerWithWrongDataTypes(fieldsToBeWrong: string[]): any {
+
+    }
+
+    private static getWrongFieldDataType(fieldValue: any): any{
+        const dataTypesForBoolean = ['number', 'string'];
+        const dataTypesForNumber = ['boolean', 'string'];
+        const dataTypesForString = ['boolean', 'number'];
+
+        if(typeof fieldValue === 'boolean'){
+            const type = this.chooseRandArrElem(dataTypesForBoolean);
+            if(type === 'number')
+                return this.getRandNum();
+            else if(type === 'string')
+                return this.getRandString();
+        }else if(typeof fieldValue === 'number'){
+            const type = this.chooseRandArrElem(dataTypesForNumber);
+            if(type === 'number')
+                return this.getRandNum();
+            else if(type === 'string')
+                return this.getRandString();
+        }
+
+        if(typeof fieldValue === 'boolean'){
+            const type = this.chooseRandArrElem(dataTypesForBoolean);
+            if(type === 'number')
+                return this.getRandNum();
+            else if(type === 'string')
+                return this.getRandString();
+        }
+    }
+
     private static getName(){
         if(this.nameIndex >= this.names.length){
             this.nameIndex = 0;
@@ -26,7 +70,20 @@ export class PlayerMock {
         this.nameIndex++;
         return result;
     }
-    private static getRandNum(min: number=0, max: number=100){
-        return Math.floor(Math.random() * (max - min) ) + min;
+    private static getRandBoolean(): boolean{
+        const values = [true, false];
+        return this.chooseRandArrElem(values);
+    }
+    private static getRandNum(min: number=0, max: number=100): number{
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    private static getRandString(): string{
+        const values = ['randomString', 'anotherString', 'helloWorld', 'testing'];
+        return this.chooseRandArrElem(values) + this.getRandNum(0,1000);
+    }
+
+    private static chooseRandArrElem(arr: any[]){
+        const randIndex = this.getRandNum(0,arr.length-1);
+        return arr[randIndex];
     }
 }
