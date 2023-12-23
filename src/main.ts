073a586c64b902eb3763ef2cc29a7ad239b43481
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2023-12-23 14:46:38 +0200

    fix bug CORS for web pages

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,20 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://altzone.netlify.app',
-      'https://altzone.fi',
-      'http://localhost:5173'
-    ],
-    methods: ["GET", "PUT", "POST", "DELETE"],
-    allowedHeaders: [
-      'Content-Type',
-      'Access-Control-Allow-Origin',
-      'Authorization'
-    ],
-    credentials: true
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://altzone.netlify.app',
+  //     'https://altzone.fi',
+  //     'http://localhost:5173'
+  //   ],
+  //   methods: ["GET", "PUT", "POST", "DELETE"],
+  //   allowedHeaders: [
+  //     'Content-Type',
+  //     'Access-Control-Allow-Origin',
+  //     'Authorization'
+  //   ],
+  //   credentials: true
+  // });
 
   app.useGlobalPipes(
       new ValidationPipe({ whitelist: true, transform: true })
