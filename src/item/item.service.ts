da69dc6b4d7a04dd213eb700fb1399775202b529
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-03-04 13:35:38 +0200

    add crateMany()

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@nestjs/common";
-import {Model, Types} from "mongoose";
+import {Model, MongooseError, Types} from "mongoose";
 import {InjectModel} from "@nestjs/mongoose";
 import {RequestHelperService} from "../requestHelper/requestHelper.service";
 import {IBasicService} from "../common/base/interface/IBasicService";
@@ -8,6 +8,7 @@ import {ModelName} from "../common/enum/modelName.enum";
 import {Item} from "./item.schema";
 import {AddBasicService} from "../common/base/decorator/AddBasicService.decorator";
 import {BasicServiceDummyAbstract} from "../common/base/abstract/basicServiceDummy.abstract";
+import {CreateItemDto} from "./dto/createItem.dto";
 
 @Injectable()
 @AddBasicService()
@@ -23,6 +24,12 @@ export class ItemService extends BasicServiceDummyAbstract<Item> implements IBas
 
     public readonly refsInModel: ModelName[];
     public readonly modelName: ModelName;
+
+    public createMany = async (items: CreateItemDto[]): Promise<boolean> => {
+        const resp = await this.model.insertMany(items);
+        return resp && !(resp instanceof MongooseError);
+    }
+
     public clearCollectionReferences = async (_id: Types.ObjectId, ignoreReferences?: IgnoreReferencesType): Promise<void> => {
     }
 }
