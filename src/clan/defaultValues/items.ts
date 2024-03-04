ccc0924a12289fac743198089c5978f7c239df72
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2024-03-04 13:34:53 +0200

    add default clan items

---
+++ src/clan/defaultValues/items.ts
@@ -0,0 +1,33 @@
+import {CreateItemDto} from "../../item/dto/createItem.dto";
+
+export const getDefaultItems = (stock_id: string) : CreateItemDto[] => {
+    return [
+        { unityKey: 'huonekasvi', name: 'huonekasvi', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'roskakori', name: 'roskakori', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'ruokatuoli', name: 'ruokatuoli', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'nojatuoli', name: 'nojatuoli', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'koristepatsas', name: 'koristepatsas', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'nalle', name: 'nalle', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'heikko.pommi', name: 'heikko.pommi', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'hella', name: 'hella', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'pieni.pyöreä.sivupöytä', name: 'pieni.pyöreä.sivupöytä', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'kahden.istuttava.sohva', name: 'kahden.istuttava.sohva', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'matto,.ovaali', name: 'matto,.ovaali', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'tuplapommi', name: 'tuplapommi', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'arkkupakastin', name: 'arkkupakastin', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'soutulaite', name: 'soutulaite', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'sohvapöytä', name: 'sohvapöytä', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'kolmen.istuttava.sohva', name: 'kolmen.istuttava.sohva', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'pitkä.ruokapöytä', name: 'pitkä.ruokapöytä', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'käytävämatto', name: 'käytävämatto', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'keittiösaareke', name: 'keittiösaareke', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'kirjahylly', name: 'kirjahylly', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'tv.taso', name: 'tv.taso', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'kolmen.istuttava.kulmasohva', name: 'kolmen.istuttava.kulmasohva', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'kulmahylly', name: 'kulmahylly', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'ruokapöytä', name: 'ruokapöytä', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'superpommi', name: 'superpommi', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'iso.matto', name: 'iso.matto', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true },
+        { unityKey: 'parisänky', name: 'parisänky', stock_id, rowNumber: 0, columnNumber: 0, filename: 'filename', recycling: 'recycling', material: 'material', weight: 0, shape: 'shape', isFurniture: true, isInStock: true }
+    ];
+}
