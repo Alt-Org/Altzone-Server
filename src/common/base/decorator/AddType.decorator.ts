ee5f4f6fba18267d47d0d2d21387807fab4005c8
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-02 23:00:19 +0300

    improve AddType decorator

--- src/common/base/decorator/AddType.decorator.ts
+++ src/common/base/decorator/AddType.decorator.ts
@@ -1,21 +1,35 @@
+/**
+ * Constant, which determines the field used to determine the type of the object
+ */
+export const OBJECT_TYPE_FIELD = 'objectType';
+
+/**
+ * Interface can be used together with AddType decorator to add the objectType field and isType() function for TS
+ */
 export interface ObjectType {
-    objectType: string;
+    [OBJECT_TYPE_FIELD]: string;
     isType: (type: string) => boolean;
 }
 
 /**
- * Add objectType to the class for later comparisons
+ * Add objectType to the class for comparisons as well as isType(), which can determine whenever the object is right type or not
+ *
+ * In case you want to use the objectType and isType() inside the API code, please add this two statements to your class:
+ * - declare objectType: string;
+ * - declare isType: (type: string) => boolean;
  * @param type class objectType field to be added
  */
 export default function AddType(type: string) {
     return function <T extends { new(...args: any[]): {} }>(constructor: T) {
-        return class extends constructor implements ObjectType {
-            objectType = type;
-            
-            isType(compareType: string): boolean {
-                return this.objectType === compareType;
-            }
+        return ObjectTypeMixin(constructor, type);
+    }
+}
+
+function ObjectTypeMixin<T extends { new(...args: any[]): {} }>(Base: T, type: string) {
+    return class extends Base implements ObjectType {
+        [OBJECT_TYPE_FIELD] = type;
+        isType(compareType: string): boolean {
+            return this[OBJECT_TYPE_FIELD] === compareType;
         }
     };
 }
-
