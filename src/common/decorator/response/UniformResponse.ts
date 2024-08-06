fe33f144b78e3b903c608caefb1d03bb3883d7fe
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-06 19:27:58 +0300

    add decorator, which catches APIErrors and ServiceErrors, format the response shape and sends 204 if nothing is returned

---
+++ src/common/decorator/response/UniformResponse.ts
@@ -0,0 +1,30 @@
+import {applyDecorators, UseFilters, UseInterceptors} from '@nestjs/common';
+import { ModelName } from 'src/common/enum/modelName.enum';
+import { ValidationExceptionFilter } from './ValidationExceptionFilter';
+import { FormatAPIResponse } from './FormatAPIResponse';
+import { Send204OnEmptyRes } from 'src/common/interceptor/response/Send204OnEmptyRes';
+
+/**
+ * Uniform response sent to the client side as follows
+ * 
+ * - If ServiceError, ServiceError[], APIError or APIError[] is returned or thrown 
+ * from/in the controller method. It will sent an appropriate error response
+ * - If the controller method does not return a ServiceError or an APIError, 
+ * it will be treated as a success and this data will be formatted into {data: ..., metaData: ...} form.
+ * - If nothing is returned the response with 204 (No Content) status will be returned
+ * @param modelName name of the model, what the controller returning on success
+ * @returns 
+ */
+export function UniformResponse(modelName?: ModelName) {
+    const decorators = [
+        Send204OnEmptyRes(), 
+        UseFilters(new ValidationExceptionFilter()), 
+        FormatAPIResponse(modelName)
+    ];
+
+    return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+        decorators.reverse().forEach(decorator => {
+            decorator(target, propertyKey, descriptor);
+        });
+    };
+}
