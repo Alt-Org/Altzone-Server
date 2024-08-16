ca9a867d834551bc52cb7c91a9ff2226ca862fc7
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-16 14:19:11 +0300

    remove unused imports from item.controller

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,20 +1,11 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, Req} from "@nestjs/common";
-import {AddGetQueries} from "../common/decorator/request/AddGetQueries.decorator";
+import {Controller, Get, Param, Req} from "@nestjs/common";
 import {_idDto} from "../common/dto/_id.dto";
 import {ModelName} from "../common/enum/modelName.enum";
 import {ItemService} from "./item.service";
 import {ItemDto} from "./dto/item.dto";
-import {CreateItemDto} from "./dto/createItem.dto";
-import {UpdateItemDto} from "./dto/updateItem.dto";
 import { Authorize } from "src/authorization/decorator/Authorize";
 import { Action } from "src/authorization/enum/action.enum";
-import {AddSearchQuery} from "../common/interceptor/request/addSearchQuery.interceptor";
-import {GetAllQuery} from "../common/decorator/param/GetAllQuery";
-import {IGetAllQuery} from "../common/interface/IGetAllQuery";
-import { OffsetPaginate } from "src/common/interceptor/request/offsetPagination.interceptor";
-import { AddSortQuery } from "src/common/interceptor/request/addSortQuery.interceptor";
 import { UniformResponse } from "src/common/decorator/response/UniformResponse";
-import { Serialize } from "src/common/interceptor/response/Serialize";
 
 @Controller('item')
 export class ItemController {
@@ -24,8 +15,7 @@ export class ItemController {
     @Get('/:_id')
     @Authorize({action: Action.read, subject: ItemDto})
     @UniformResponse(ModelName.ITEM)
-    //@AddGetQueries()
     public get(@Param() param: _idDto, @Req() request: Request) {
-        return this.service.readOneById(param._id);// request['mongoPopulate']);
+        return this.service.readOneById(param._id);
     }
 }
