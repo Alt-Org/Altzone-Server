1df7470904a8358ce94c492d0b1d04a464777309
Antti Pulkkinen <apulkki78@welho.com>
2024-08-19 20:18:51 +0300

    Deleted comments from stock.controller.ts

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,10 +1,6 @@
 import {Body, Controller, Delete, Get, Param, Post, Put, Req} from "@nestjs/common";
-//import {BasicPOST} from "../common/base/decorator/BasicPOST.decorator";
-//import {BasicGET} from "../common/base/decorator/BasicGET.decorator";
 import {AddGetQueries} from "../common/decorator/request/AddGetQueries.decorator";
 import {_idDto} from "../common/dto/_id.dto";
-//import {BasicDELETE} from "../common/base/decorator/BasicDELETE.decorator";
-import {BasicPUT} from "../common/base/decorator/BasicPUT.decorator";
 import {ModelName} from "../common/enum/modelName.enum";
 import {StockService} from "./stock.service";
 import {StockDto} from "./dto/stock.dto";
@@ -24,13 +20,6 @@ export class StockController {
     public constructor(private readonly service: StockService) {
     }
 
-    @Post()
-    //@Authorize({action: Action.create, subject: StockDto})
-    @UniformResponse(ModelName.STOCK)
-    public create(@Body() body: CreateStockDto) {
-        return this.service.createOne(body);
-    }
-
     @Get('/:_id')
     @Authorize({action: Action.read, subject: StockDto})
     @UniformResponse(ModelName.STOCK)
@@ -48,18 +37,4 @@ export class StockController {
     public getAll(@GetAllQuery() query: IGetAllQuery) {
         return this.service.readAll(query);
     }
-
-    @Put()
-    //@Authorize({action: Action.update, subject: UpdateStockDto})
-    @BasicPUT(ModelName.STOCK)
-    public update(@Body() body: UpdateStockDto){
-        return this.service.updateOneBycellcount(body);
-    }
-
-    //@Delete('/:_id')
-    //@Authorize({action: Action.delete, subject: UpdateStockDto})
-    //@BasicDELETE(ModelName.STOCK)
-    //public delete(@Param() param: _idDto) {
-    //    return this.service.deleteOneById(param._id);
-    // }
 }
