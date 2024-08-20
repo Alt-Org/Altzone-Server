f788daccb90fb40272a3cc237fa520f455265132
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-20 14:25:55 +0300

    fix JSODcs typos in stock.service

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -25,23 +25,51 @@ export class StockService {
     public readonly modelName: ModelName;
     public readonly basicService: BasicService;
 
-  /**
-    * Creates an new Stock in DB.
-    * 
-    * @param item - The Stock data to create.
-    * @returns  created Stock or an array of service errors if any occurred.
+    /**
+      * Creates a new Stock in DB.
+      * 
+      * @param stock - The Stock data to create.
+      * @returns created Stock or an array of service errors if any occurred.
     */
     async createOne(stock: CreateStockDto) {
         return this.basicService.createOne<CreateStockDto, StockDto>(stock);
     }
 
-  /**
-    * Updates a Stock cellCount field by the specified amount.
-    * 
-    * @param _id - The Mongo _id of the Stock to be updated
-    * @param cellCountChange - the amount the cellCount field will be updated on. It can be ever negative or positive number.
-    * @returns _true_ if Stock was updated successfully, _false_ if nothing was updated for the Stock, 
-     * or a ServiceError array if Stock was not found or something else went wrong.
+    /**
+      * Reads a Stock by its _id in DB.
+      * 
+      * @param _id - The Mongo _id of the Stock to read.
+      * @param options - Options for reading the Stock.
+      * @returns Stock with the given _id on succeed or an array of ServiceErrors if any occurred.
+    */
+    async readOneById(_id: string, options?: TReadByIdOptions) {
+        let optionsToApply = options;
+        if(options?.includeRefs)
+            optionsToApply.includeRefs = options.includeRefs.filter((ref) => this.refsInModel.includes(ref));
+        return this.basicService.readOneById<StockDto>(_id, optionsToApply);
+     }
+ 
+    /**
+      * Reads Stocks by specified options from DB.
+      * 
+      * @param options - Options for reading CharacterClasses.
+      * @returns An array of Stocks if succeed or an array of ServiceErrors if any occurred.
+     */
+     async readAll(options?: TIServiceReadManyOptions) {
+         let optionsToApply = options;
+         if(options?.includeRefs)
+             optionsToApply.includeRefs = options.includeRefs.filter((ref) => this.refsInModel.includes(ref));
+ 
+         return this.basicService.readMany<StockDto>(optionsToApply);
+     }
+
+    /**
+      * Updates a Stock cellCount field by the specified amount.
+      * 
+      * @param _id - The Mongo _id of the Stock to be updated
+      * @param cellCountChange - the amount the cellCount field will be updated on. It can be ever negative or positive number.
+      * @returns _true_ if Stock was updated successfully, _false_ if nothing was updated for the Stock, 
+      * or a ServiceError array if Stock was not found or something else went wrong.
     */
     public updateStockCellCount = async (_id: string, cellCountChange: number) => {
         const dbResp = await this.basicService.readOneById<StockDto>(_id);
@@ -59,39 +87,11 @@ export class StockService {
     }
 
     /**
-    * Reads an Stock by its _id in DB.
-    * 
-    * @param _id - The Mongo _id of the Stock to read.
-    * @param options - Options for reading the Stock.
-    * @returns Stock with the given _id on succeed or an array of ServiceErrors if any occurred.
-    */
-    async readOneById(_id: string, options?: TReadByIdOptions) {
-       let optionsToApply = options;
-       if(options?.includeRefs)
-           optionsToApply.includeRefs = options.includeRefs.filter((ref) => this.refsInModel.includes(ref));
-       return this.basicService.readOneById<StockDto>(_id, optionsToApply);
-    }
-
-    /**
-    * Reads Stocks by specified options from DB.
-    * 
-    * @param options - Options for reading CharacterClasses.
-    * @returns An array of Stocks if succeed or an array of ServiceErrors if any occurred.
-    */
-    async readAll(options?: TIServiceReadManyOptions) {
-        let optionsToApply = options;
-        if(options?.includeRefs)
-            optionsToApply.includeRefs = options.includeRefs.filter((ref) => this.refsInModel.includes(ref));
-
-        return this.basicService.readMany<StockDto>(optionsToApply);
-    }
-
-    /**
-    * Updates an Stock by its _id in DB. The _id field is read-only and must be found from the parameter
-    * 
-    * @param Stock - The data needs to be updated for the Stock.
-    * @returns _true_ if Item was updated successfully, _false_ if nothing was updated for the Stock, 
-     * or a ServiceError array if Stock was not found or something else went wrong.
+      * Updates a Stock by its _id in DB. The _id field is read-only and must be found from the parameter
+      * 
+      * @param Stock - The data needs to be updated for the Stock.
+      * @returns _true_ if Stock was updated successfully, _false_ if nothing was updated for the Stock, 
+      * or a ServiceError array if Stock was not found or something else went wrong.
     */
     async updateOneById(stock: UpdateStockDto) {
         const {_id, ...fieldsToUpdate} = stock;
@@ -99,10 +99,10 @@ export class StockService {
     }
 
     /**
-    * Deletes an Stock its _id from DB.
-    * 
-    * @param _id - The Mongo _id of the Stock to delete.
-    * @returns _true_ if Item was removed successfully, or a ServiceError array if the Stock was not found or something else went wrong
+      * Deletes a Stock its _id from DB.
+      * 
+      * @param _id - The Mongo _id of the Stock to delete.
+      * @returns _true_ if Stock was removed successfully, or a ServiceError array if the Stock was not found or something else went wrong
     */
     async deleteOneById(_id: string) {
         return this.basicService.deleteOneById(_id);
