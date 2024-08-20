22fb5f72e56028cf714aa2546ba83b30f9797834
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-20 14:07:58 +0300

    remove unused imports and commented out code from StockService

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -1,14 +1,8 @@
-import {Injectable, NotFoundException } from "@nestjs/common";
-import {Model, MongooseError, Types} from "mongoose";
+import {Injectable} from "@nestjs/common";
+import {Model} from "mongoose";
 import {InjectModel} from "@nestjs/mongoose";
-import {RequestHelperService} from "../requestHelper/requestHelper.service";
-import {IBasicService} from "../common/base/interface/IBasicService";
-import {IgnoreReferencesType} from "../common/type/ignoreReferences.type";
 import {ModelName} from "../common/enum/modelName.enum";
 import {Stock} from "./stock.schema";
-import {AddBasicService} from "../common/base/decorator/AddBasicService.decorator";
-import {BasicServiceDummyAbstract} from "../common/base/abstract/basicServiceDummy.abstract";
-import {IHookImplementer, PostCreateHookFunction, PostHookFunction} from "../common/interface/IHookImplementer";
 import {CreateStockDto} from "./dto/createStock.dto";
 import {UpdateStockDto} from "./dto/updateStock.dto";
 import {StockDto} from "./dto/stock.dto";
@@ -18,13 +12,10 @@ import ServiceError from "src/common/service/basicService/ServiceError";
 import { isServiceError } from "src/common/service/basicService/ServiceError";
 
 @Injectable()
-//@AddBasicService()
-export class StockService {//extends BasicServiceDummyAbstract<Stock> implements IBasicService<Stock>, IHookImplementer{
+export class StockService {
     public constructor(
-        @InjectModel(Stock.name) public readonly model: Model<Stock>,
-        private readonly requestHelperService: RequestHelperService
+        @InjectModel(Stock.name) public readonly model: Model<Stock>
     ){
-        //super();
         this.refsInModel = [ModelName.CLAN, ModelName.ITEM];
         this.modelName = ModelName.STOCK;
         this.basicService = new BasicService(model);
@@ -33,41 +24,6 @@ export class StockService {//extends BasicServiceDummyAbstract<Stock> implements
     public readonly refsInModel: ModelName[];
     public readonly modelName: ModelName;
     public readonly basicService: BasicService;
-
-/*    public createOnePostHook: PostCreateHookFunction = async (input: CreateStockDto, output: Stock): Promise<boolean> => {
-        if(!input?.clan_id)
-            return true;
-
-        const isRaidRoomCountIncreased = await this.requestHelperService.changeCounterValue(ModelName.CLAN, {_id: input.clan_id}, 'stockCount', 1);
-        return isRaidRoomCountIncreased;
-    }
-
-    public updateOnePostHook: PostHookFunction = async (input: Partial<UpdateStockDto>, oldDoc: Stock, output: Stock): Promise<boolean> => {
-        if(!input?.clan_id)
-            return true;
-
-        const changeCounterValue = this.requestHelperService.changeCounterValue;
-
-        const clanRemoveFrom_id = oldDoc.clan_id;
-        if(clanRemoveFrom_id)
-            await changeCounterValue(ModelName.CLAN, {_id: clanRemoveFrom_id}, 'stockCount', -1);
-
-        const isRaidRoomCountIncreased = await changeCounterValue(ModelName.CLAN, {_id: input.clan_id}, 'stockCount', 1);
-        return isRaidRoomCountIncreased;
-    }
-
-    public deleteOnePostHook: PostHookFunction = async (input: any, oldDoc: Stock, output: Stock): Promise<boolean> => {
-        const clan_id = oldDoc.clan_id;
-
-        if(!clan_id)
-            return true;
-
-        const isRaidRoomCountIncreased = await this.requestHelperService.changeCounterValue(ModelName.CLAN, {_id: clan_id}, 'stockCount', -1);
-        return isRaidRoomCountIncreased;
-    }
-
-    public clearCollectionReferences = async (_id: Types.ObjectId, ignoreReferences?: IgnoreReferencesType): Promise<void> => {
-    }*/
 
   /**
     * Creates an new Stock in DB.
@@ -78,27 +34,6 @@ export class StockService {//extends BasicServiceDummyAbstract<Stock> implements
     async createOne(stock: CreateStockDto) {
         return this.basicService.createOne<CreateStockDto, StockDto>(stock);
     }
-
-    /**
-    * Creates many new Items in DB.
-    * 
-    * @param items - The Items data to create.
-    * @returns created Item or an array of service errors if any occurred.
-    */
-    //async createMany(items: CreateItemDto[]) {
-	//    return this.basicService.createMany<CreateItemDto, ItemDto[]>(items);
-    //}
-
-    /**
-    * Creates many new Items in DB.
-    * 
-    * @deprecated The createMany() method should be used instead
-    * @param items - The Items data to create.
-    * @returns created Item or an array of service errors if any occurred.
-    */
-    //async createManyWithResponse(items: CreateItemDto[]) {
-    //    return this.basicService.createMany<CreateItemDto, ItemDto[]>(items);
-   //}
 
   /**
     * Updates an Stock by its cellCount in DB. The cellCount field is read-only and must be found from the parameter
