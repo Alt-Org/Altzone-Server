d4add04f89f2867356f212451e952b33ce646f94
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-20 14:00:28 +0300

    remove unused import and commented out code

--- src/stock/dto/createStock.dto.ts
+++ src/stock/dto/createStock.dto.ts
@@ -1,14 +1,9 @@
-import {IsEnum, IsInt, IsMongoId, IsOptional} from "class-validator";
+import {IsInt, IsMongoId} from "class-validator";
 import {IsClanExists} from "../../clan/decorator/validation/IsClanExists.decorator";
-import {StockType} from "../../common/enum/stockType.enum";
 import AddType from "../../common/base/decorator/AddType.decorator";
 
 @AddType('CreateStockDto')
 export class CreateStockDto {
-    //@IsEnum(StockType)
-    //@IsInt()
-    //type: StockType;
-
     @IsInt()
     cellCount: number;
 
