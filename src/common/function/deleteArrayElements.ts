86cb88115e7093e857d8ab3f744053c9c95d3ce5
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-25 13:31:32 +0300

    add JSDocs for /common/function package files

--- src/common/function/deleteArrayElements.ts
+++ src/common/function/deleteArrayElements.ts
@@ -1,3 +1,11 @@
+/**
+ * Removes elements from `arr` that are present in the `elems` array.
+ *
+ * @template T The type of elements in the arrays.
+ * @param arr The original array from which elements will be removed.
+ * @param elems The array containing elements to remove. If null or empty, `arr` is returned unchanged.
+ * @returns A new array with the elements from `elems` removed from `arr`.
+ */
 export function deleteArrayElements<T=any>(arr: T[], elems: T[] | null): T[] {
     if(!elems || elems.length === 0)
         return arr
