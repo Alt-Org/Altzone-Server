0513f93ead48c83c941f05aa18da9cc6a96f0c95
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-25 13:05:12 +0300

    add JSDocs for /common/dto package files

--- src/common/dto/_id.dto.ts
+++ src/common/dto/_id.dto.ts
@@ -1,5 +1,16 @@
 import {IsMongoId} from "class-validator";
 
+/**
+ * Class for validating whenever an object contain _id field and that it has a valid Mongo _id value.
+ *
+ * Typically used to check url parameter
+ *
+ * @example ```ts
+ * public getClanById(\@Param() param: _idDto) {
+ *     return this.service.readOneById(param._id);
+ * }
+ *```
+ */
 export class _idDto {
     @IsMongoId()
     _id: string;
