65fbe68f06c668b4a5c47fb6239c0a251b0d9f92
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-26 18:32:54 +0300

    add IncludeQuery decorator

---
+++ src/common/decorator/param/IncludeQuery.decorator.ts
@@ -0,0 +1,36 @@
+import {createParamDecorator, ExecutionContext} from "@nestjs/common";
+import {Request} from "express";
+import { ModelName } from "../../enum/modelName.enum";
+
+
+/**
+ * Get the `with` or `all` query from the client request.
+ *
+ * The `with` query specifies which related documents must be fetched. 
+ * For example if Clan data was fetched, and the Clan has Stock and SoulHome they data can be fetched as well together with Clan data.
+ *
+ * The `all` query specifies that all related documents to the collection must be fetched.
+ * 
+ * If both the `all` and the `with` queries are specified `all` query will overwrite the `with` query.
+ *
+ * The returned value is an array of the ModelNames, which need to be retrieved
+ *
+ * @see [Mongoose Populate](https://mongoosejs.com/docs/populate.html)
+ */
+export const IncludeQuery = createParamDecorator((allowedReferences: ModelName[], context: ExecutionContext): ModelName[] => {
+    const request = context.switchToHttp().getRequest<Request>();
+    const withQuery = request.query['with'];
+    const allQuery = request.query['all'];
+
+    if(allQuery || allQuery === '')
+        return allowedReferences;
+
+    if(!withQuery || typeof withQuery !== 'string')
+        return [];
+
+    const withRefs: ModelName[] = withQuery.split('_') as ModelName[];
+    if(withRefs.length === 0)
+        return [];
+
+    return withRefs.filter(requestedRef => allowedReferences.includes(requestedRef));
+});
