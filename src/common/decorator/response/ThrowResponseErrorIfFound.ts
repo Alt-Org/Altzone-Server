24c4f11328af7c5b9dbad88455415694af555c0e
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-30 14:50:07 +0300

    update response error form to new with APIError[] in ThrowResponseErrorIfFound

--- src/common/decorator/response/ThrowResponseErrorIfFound.ts
+++ src/common/decorator/response/ThrowResponseErrorIfFound.ts
@@ -3,6 +3,8 @@ import {ResponseType} from "./responseType";
 import {ModelName} from "../../enum/modelName.enum";
 import {IResponseShape} from "../../interface/IResponseShape";
 import {APIObjectName} from "../../enum/apiObjectName.enum";
+import { APIError } from "../../controller/APIError";
+import { APIErrorReason } from "../../controller/APIErrorReason";
 
 type ResponseHandler = (data: any, modelName: ModelName | APIObjectName) => any;
 
@@ -44,31 +46,86 @@ function getResponseHandler(responseType: ResponseType): ResponseHandler {
 }
 
 function handleGetResponse (data: any, modelName: ModelName | APIObjectName) {
+    const errorResp = {
+        statusCode: 404,
+        error: 'Not Found'
+    }
     if(data == null)
-        throw new NotFoundException(`Can not find ${modelName} with that query`);
+        throw new NotFoundException({...errorResp,
+            message: `Can not find ${modelName} with that query`,
+            errors: [ new APIError({
+                reason: APIErrorReason.NOT_FOUND,
+                message: `Can not find ${modelName} with that query`
+            }) ]
+        });
 
     const parsedData = data as IResponseShape;
 
     if(!parsedData || parsedData.metaData?.dataCount === 0)
-        throw new NotFoundException(`Can not find any ${modelName} instances`);
+        throw new NotFoundException({...errorResp,
+            message: `Can not find any ${modelName} instances`,
+            errors: [ new APIError({
+                reason: APIErrorReason.NOT_FOUND,
+                message: `Can not find any ${modelName} instances`
+            }) ]
+        });
 
     return data;
 }
 
 function handleUpdateResponse (data: any, modelName: ModelName | APIObjectName){
+    const notFoundErrorResp = {
+        statusCode: 404,
+        error: 'Not Found'
+    }
     if(data instanceof Object && data.matchedCount === 0)
-        throw new NotFoundException(`No ${modelName} with that query found`);
+        throw new NotFoundException({...notFoundErrorResp,
+            message: `No ${modelName} with that query found`,
+            errors: [ new APIError({
+                reason: APIErrorReason.NOT_FOUND,
+                message: `No ${modelName} with that query found`
+            }) ]
+        });
+
     if(data instanceof Object && data.modifiedCount === 0)
-        throw new BadRequestException('Nothing to update');
+        throw new BadRequestException({
+            message: 'Nothing to update',
+            errors: [ new APIError({
+                reason: APIErrorReason.BAD_REQUEST, 
+                message: 'Nothing to update'}
+            ) ],
+
+            statusCode: 400,
+            error: 'Bad Request'
+        });
+
     if(typeof data === 'boolean' && !data)
-        throw new BadRequestException(`Can not update ${modelName}`);
+        throw new BadRequestException({
+            message: `Can not update ${modelName}`,
+            errors: [ new APIError({
+                reason: APIErrorReason.BAD_REQUEST, 
+                message: `Can not update ${modelName}`}
+            ) ],
+
+            statusCode: 400,
+            error: 'Bad Request'
+        });
 
     return;
 }
 
 function handleDeleteResponse(data: any, modelName: ModelName | APIObjectName){
     if(data == null || (data instanceof Object && data.deletedCount != null && data.deletedCount === 0))
-        throw new NotFoundException(`Can not find ${modelName} with that query`);
+        throw new NotFoundException({
+            message: `Can not find ${modelName} with that query`,
+            errors: [ new APIError({
+                reason: APIErrorReason.NOT_FOUND,
+                message: `Can not find ${modelName} with that query`
+            }) ],
+
+            statusCode: 404,
+            error: 'Not Found'
+        });
 
     return;
 }
