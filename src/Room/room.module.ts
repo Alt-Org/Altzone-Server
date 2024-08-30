4536e04ca182d39674dd6c89d0a922e505e88073
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-08-30 18:20:48 +0300

    add import of SoulhomeSchema into RoomModule

--- src/Room/room.module.ts
+++ src/Room/room.module.ts
@@ -8,9 +8,11 @@ import { RoomSchema } from "./room.schema";
 import { PlayerModule } from "../player/player.module";
 import { ClanSchema } from "../clan/clan.schema";
 import RoomHelperService from "./utils/room.helper.service";
+import { SoulhomeSchema } from "../soulhome/soulhome.schema";
 
 @Module({
     imports: [
+        MongooseModule.forFeature([ {name: ModelName.SOULHOME, schema: SoulhomeSchema } ]),
         MongooseModule.forFeature([ {name: ModelName.ROOM, schema: RoomSchema } ]),
         MongooseModule.forFeature([ {name: ModelName.CLAN, schema: ClanSchema } ]),
         PlayerModule,
