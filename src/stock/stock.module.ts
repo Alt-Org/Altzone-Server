71de38d326ccf484a3e877d368dc16c15c47f5cd
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-01 16:38:44 +0300

    add import of ItemModule to stock module

--- src/stock/stock.module.ts
+++ src/stock/stock.module.ts
@@ -6,11 +6,13 @@ import {ModelName} from "../common/enum/modelName.enum";
 import {StockController} from "./stock.controller";
 import {StockService} from "./stock.service";
 import {isStockExists} from "./decorator/validation/IsStockExists.decorator";
+import { ItemModule } from '../item/item.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([ {name: ModelName.STOCK, schema: StockSchema} ]),
-        RequestHelperModule
+        RequestHelperModule,
+        ItemModule
     ],
     controllers: [StockController],
     providers: [ StockService, isStockExists ],
