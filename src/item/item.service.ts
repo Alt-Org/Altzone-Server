b6a218abd6651d69f29c219b6e9f02a6d01605f8
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-01 16:06:02 +0300

    add items deletion from stock and room methods to item.service

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -21,45 +21,45 @@ export class ItemService {
 
     public readonly refsInModel: ModelName[];
     public readonly modelName: ModelName;
-    public readonly basicService: BasicService;
+    private readonly basicService: BasicService;
 
-  /**
-    * Creates an new Item in DB.
-    * 
-    * @param item - The Item data to create.
-    * @returns  created Item or an array of service errors if any occurred.
+    /**
+     * Creates an new Item in DB.
+     * 
+     * @param item - The Item data to create.
+     * @returns  created Item or an array of service errors if any occurred.
     */
     async createOne(item: CreateItemDto) {
         return this.basicService.createOne<CreateItemDto, ItemDto>(item);
     }
 
     /**
-    * Creates many new Items in DB.
-    * 
-    * @param items - The Items data to create.
-    * @returns created Item or an array of service errors if any occurred.
+     * Creates many new Items in DB.
+     * 
+     * @param items - The Items data to create.
+     * @returns created Item or an array of service errors if any occurred.
     */
     async createMany(items: CreateItemDto[]) {
 	    return this.basicService.createMany<CreateItemDto, ItemDto[]>(items);
     }
 
     /**
-    * Creates many new Items in DB.
-    * 
-    * @deprecated The createMany() method should be used instead
-    * @param items - The Items data to create.
-    * @returns created Item or an array of service errors if any occurred.
+     * Creates many new Items in DB.
+     * 
+     * @deprecated The createMany() method should be used instead
+     * @param items - The Items data to create.
+     * @returns created Item or an array of service errors if any occurred.
     */
     async createManyWithResponse(items: CreateItemDto[]) {
         return this.basicService.createMany<CreateItemDto, ItemDto[]>(items);
     }
 
     /**
-    * Reads an Item by its _id in DB.
-    * 
-    * @param _id - The Mongo _id of the Item to read.
-    * @param options - Options for reading the Item.
-    * @returns Item with the given _id on succeed or an array of ServiceErrors if any occurred.
+     * Reads an Item by its _id in DB.
+     * 
+     * @param _id - The Mongo _id of the Item to read.
+     * @param options - Options for reading the Item.
+     * @returns Item with the given _id on succeed or an array of ServiceErrors if any occurred.
     */
     async readOneById(_id: string, options?: TReadByIdOptions) {
        let optionsToApply = options;
@@ -69,24 +69,44 @@ export class ItemService {
     }
 
     /**
-    * Updates an Item by its _id in DB. The _id field is read-only and must be found from the parameter
-    * 
-    * @param Item - The data needs to be updated for the Item.
-    * @returns _true_ if Item was updated successfully, _false_ if nothing was updated for the Item, 
+     * Updates an Item by its _id in DB. The _id field is read-only and must be found from the parameter
+     * 
+     * @param Item - The data needs to be updated for the Item.
+     * @returns _true_ if Item was updated successfully, _false_ if nothing was updated for the Item, 
      * or a ServiceError array if Item was not found or something else went wrong.
-    */
+     */
     async updateOneById(Item: UpdateItemDto) {
         const {_id, ...fieldsToUpdate} = Item
         return this.basicService.updateOneById(_id, fieldsToUpdate);
     }
 
     /**
-    * Deletes an Item its _id from DB.
-    * 
-    * @param _id - The Mongo _id of the Item to delete.
-    * @returns _true_ if Item was removed successfully, or a ServiceError array if the Item was not found or something else went wrong
-    */
+     * Deletes an Item by its _id from DB.
+     * 
+     * @param _id - The Mongo _id of the Item to delete.
+     * @returns _true_ if Item was removed successfully, or a ServiceError array if the Item was not found or something else went wrong
+     */
     async deleteOneById(_id: string) {
         return this.basicService.deleteOneById(_id);
+    }
+
+    /**
+     * Deletes all Items of the specified by _id Stock from DB.
+     * 
+     * @param stock_id - The Mongo _id of the Stock from which all items should be deleted
+     * @returns _true_ if Items was removed successfully, or a ServiceError array if any Items of the Stock was not found or something else went wrong
+     */
+    async deleteAllStockItems(stock_id: string) {
+        return this.basicService.deleteMany({filter: { stock_id }});
+    }
+
+    /**
+     * Deletes all Items of the specified by _id Room from DB.
+     * 
+     * @param room_id - The Mongo _id of the Stock from which all items should be deleted
+     * @returns _true_ if Items was removed successfully, or a ServiceError array if any Items of the Room was not found or something else went wrong
+     */
+    async deleteAllRoomItems(room_id: string) {
+        return this.basicService.deleteMany({filter: { room_id }});
     }
 }
