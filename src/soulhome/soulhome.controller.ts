542d800a497192d2ae2a0e7ab6643621eec19c14
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-02 17:10:14 +0300

    change SoulHome controller to meet the swagger description

--- src/soulhome/soulhome.controller.ts
+++ src/soulhome/soulhome.controller.ts
@@ -4,8 +4,6 @@ import { _idDto } from "../common/dto/_id.dto";
 import { OffsetPaginate } from "../common/interceptor/request/offsetPagination.interceptor";
 import { AddSearchQuery } from "../common/interceptor/request/addSearchQuery.interceptor";
 import { AddSortQuery } from "../common/interceptor/request/addSortQuery.interceptor";
-import { GetAllQuery } from "../common/decorator/param/GetAllQuery";
-import { IGetAllQuery } from "../common/interface/IGetAllQuery";
 import { SoulHomeDto } from "./dto/soulhome.dto";
 import { Authorize } from "../authorization/decorator/Authorize";
 import { Action } from "../authorization/enum/action.enum";
@@ -14,11 +12,16 @@ import { Serialize } from "../common/interceptor/response/Serialize";
 import { UniformResponse } from "../common/decorator/response/UniformResponse";
 import { LoggedUser } from "../common/decorator/param/LoggedUser.decorator";
 import { User } from "../auth/user";
+import SoulHomeHelperService from "./utils/soulHomeHelper.service";
+import { isServiceError } from "../common/service/basicService/ServiceError";
+import { publicReferences } from "./soulhome.schema";
+import { IncludeQuery } from "../common/decorator/param/IncludeQuery.decorator";
 
 @Controller('soulhome')
 export class SoulHomeController {
     public constructor(
-        private readonly service: SoulHomeService
+        private readonly service: SoulHomeService,
+        private readonly helper: SoulHomeHelperService
     ) {
     }
 
@@ -29,9 +32,12 @@ export class SoulHomeController {
     @AddSortQuery(SoulHomeDto)
     @Serialize(SoulHomeDto)
     @UniformResponse(ModelName.SOULHOME)
-    public async getAll(@GetAllQuery() query: IGetAllQuery, @LoggedUser() user: User) {
-        
-        return this.service.readAll(query);
-    }
+    public async getPlayerSoulHome(@IncludeQuery(publicReferences) includeRefs: ModelName[], @LoggedUser() user: User) {
+        const playerSoulHomeResp = await this.helper.getPlayerSoulHome(user.player_id);
+        if(isServiceError(playerSoulHomeResp))
+            return playerSoulHomeResp;
 
+        const soulHome = playerSoulHomeResp as SoulHomeDto;
+        return this.service.readOneById(soulHome._id, {includeRefs});
+    }
 }
