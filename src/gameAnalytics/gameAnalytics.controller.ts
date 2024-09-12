f6c2949d26c7953c7e42232ff9bfd49e149b2596
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-12 18:57:00 +0300

    add basic error conditions to the GameAnalyticsController

--- src/gameAnalytics/gameAnalytics.controller.ts
+++ src/gameAnalytics/gameAnalytics.controller.ts
@@ -1,8 +1,43 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Post, UploadedFile, UseFilters, UseInterceptors} from "@nestjs/common";
 import {_idDto} from "../common/dto/_id.dto";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { FileValidationFilter } from "./FileValidation.filter";
+import { UniformResponse } from "../common/decorator/response/UniformResponse";
+import { SecretHeader } from "./SecretHeader.decorator";
+import { LoggedUser } from "../common/decorator/param/LoggedUser.decorator";
+import { User } from "../auth/user";
+import { APIError } from "../common/controller/APIError";
+import { APIErrorReason } from "../common/controller/APIErrorReason";
 
 @Controller('gameAnalytics')
 export class GameAnalyticsController {
     public constructor() {
+    }
+
+    @Post('/logFile')
+    @UseFilters(new FileValidationFilter())
+    @UseInterceptors(FileInterceptor('logFile'))
+    @UniformResponse()
+    async uploadFile(
+        @UploadedFile() file: Express.Multer.File, 
+        @SecretHeader() secret: string,
+        @LoggedUser() user: User
+    ) {
+        if(!secret)
+            return [null, [
+                new APIError({reason: APIErrorReason.REQUIRED, message: 'The "Secret" header is required'})
+            ]];
+
+        if(secret !== 'my_secret')
+            return [null, [
+                new APIError({reason: APIErrorReason.NOT_AUTHORIZED, message: 'The "Secret" header is not valid'})
+            ]];
+
+        if(!file)
+            return [null, [
+                new APIError({reason: APIErrorReason.REQUIRED, message: 'Could not define a file sent. The file is required'})
+            ]];
+
+        
     }
 }
