3c69f8b4c373f63c825896ba9b020ea26a832136
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-09-12 16:11:39 +0300

    add skeleton for game analytics

---
+++ src/gameAnalytics/gameAnalytics.controller.ts
@@ -0,0 +1,8 @@
+import {Controller} from "@nestjs/common";
+import {_idDto} from "../common/dto/_id.dto";
+
+@Controller('gameAnalytics')
+export class GameAnalyticsController {
+    public constructor() {
+    }
+}
---
+++ src/gameAnalytics/gameAnalytics.module.ts
@@ -0,0 +1,16 @@
+import {Module} from '@nestjs/common';
+import {RequestHelperModule} from "../requestHelper/requestHelper.module";
+import {GameAnalyticsController} from "./gameAnalytics.controller";
+import {GameAnalyticsService} from "./gameAnalytics.service";
+import { ItemModule } from '../item/item.module';
+
+@Module({
+    imports: [
+        RequestHelperModule,
+        ItemModule
+    ],
+    controllers: [GameAnalyticsController],
+    providers: [ GameAnalyticsService ],
+    exports: [ GameAnalyticsService ]
+})
+export class StockModule {}
