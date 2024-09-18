8c36b51555cc1c128c6d7f4107944b696b47a213
PlayJeri <nurmi.jeri@gmail.com>
2024-09-18 05:48:31 +0300

    add imports

--- src/gameData/gameData.module.ts
+++ src/gameData/gameData.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { GameDataService } from './gameData.service';
 import { GameDataController } from './gameData.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Game, GameSchema } from './game.schema';
+import { PlayerModule } from '../player/player.module';
+import { ClanModule } from '../clan/clan.module';
+import { RoomModule } from '../room/room.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([ { name: Game.name, schema: GameSchema } ]),
+    forwardRef(() => PlayerModule),
+    forwardRef(() => ClanModule),
+    forwardRef(() => RoomModule),
+  ],
   providers: [GameDataService],
   controllers: [GameDataController]
 })
