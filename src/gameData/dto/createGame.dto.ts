533136cc399993dca47b1f668d2fdb4f23517797
PlayJeri <nurmi.jeri@gmail.com>
2024-09-18 05:48:53 +0300

    Add CreateGameDto

---
+++ src/gameData/dto/createGame.dto.ts
@@ -0,0 +1,37 @@
+import { IsArray, IsMongoId, IsNumber, IsEnum, IsDate, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateGameDto {
+  @IsArray()
+  @IsMongoId({ each: true })
+  @IsNotEmpty()
+  team1: string[];
+
+  @IsArray()
+  @IsMongoId({ each: true })
+  @IsNotEmpty()
+  team2: string[];
+
+  @IsMongoId()
+  @IsNotEmpty()
+  team1Clan: string;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  team2Clan: string;
+
+  @IsNumber()
+  @IsEnum([1, 2])
+  @IsNotEmpty()
+  winner: number;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsNotEmpty()
+  startedAt: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsNotEmpty()
+  endedAt: Date;
+}
