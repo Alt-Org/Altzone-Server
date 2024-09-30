0e5c1a429a8751cfbec2ea3173a329c86553354b
sebastyijan-fi <niklas@sebastyijan.fi>
2024-09-30 09:05:49 +0300

    Replaced 'tag' with 'labels' in CreateClanDto, added validation with class-validator decorators

--- src/clan/dto/createClan.dto.ts
+++ src/clan/dto/createClan.dto.ts
@@ -1,17 +1,21 @@
-import {IsBoolean, IsInt, IsOptional, IsString} from "class-validator";
+import { IsArray, ArrayMaxSize, IsEnum, IsInt, IsBoolean, IsOptional, IsString } from "class-validator";
 import AddType from "../../common/base/decorator/AddType.decorator";
+import { ClanLabel } from '../../common/enum/clanLabel.enum';
 
 @AddType('CreateClanDto')
 export class CreateClanDto {
     @IsString()
     name: string;
 
-    @IsString()
-    tag: string;
+    @IsArray()
+    @ArrayMaxSize(5)
+    @IsEnum(ClanLabel, { each: true })
+    @IsOptional()
+    labels?: string[];
 
     @IsInt()
     gameCoins: number;
-    
+
     @IsBoolean()
     @IsOptional()
     isOpen:boolean
