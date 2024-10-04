4cbe41c2164c75e6f97e63b90b30bb483c9e9567
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-10-04 16:25:13 +0300

    - moved PlayerTasks to a separate file. - JSDocs added

--- src/playerTasks/type/task.type.ts
+++ src/playerTasks/type/task.type.ts
@@ -1,5 +1,18 @@
 import { TaskName } from "../enum/taskName.enum";
 
+/**
+ * Represents a task with its details.
+ * 
+ * @property id - The unique identifier for the task.
+ * @property title - The title of the task.
+ * @property title.fi - The Finnish title of the task.
+ * @property content - The content/description of the task.
+ * @property content.fi - The Finnish content/description of the task.
+ * @property amount - The amount required to complete the task.
+ * @property type - The type of the task.
+ * @property coins - The number of coins rewarded for completing the task.
+ * @property points - The number of points rewarded for completing the task.
+ */
 export type Task = {
 	id: number;
 	title: {
@@ -12,15 +25,4 @@ export type Task = {
 	type: TaskName;
 	coins: number;
 	points: number;
-}
-
-export type PlayerTasks = {
-	monday: Task[];
-	tuesday: Task[];
-	wednesday: Task[];
-	friday: Task[];
-	saturday: Task[];
-	sunday: Task[];
-	weekly: Task[];
-	monthly: Task[];
 }
---
+++ src/playerTasks/type/tasks.type.ts
@@ -0,0 +1,26 @@
+import { Task } from "./task.type";
+
+/**
+ * Represents the tasks that players can complete.
+ * Loaded from playerTasks.json
+ * This type needs to updated if the structure of the JSON file changes.
+ * 
+ * @property monday - The tasks assigned for Monday.
+ * @property tuesday - The tasks assigned for Tuesday.
+ * @property wednesday - The tasks assigned for Wednesday.
+ * @property friday - The tasks assigned for Friday.
+ * @property saturday - The tasks assigned for Saturday.
+ * @property sunday - The tasks assigned for Sunday.
+ * @property weekly - The tasks assigned for the week.
+ * @property monthly - The tasks assigned for the month.
+ */
+export type PlayerTasks = {
+	monday: Task[];
+	tuesday: Task[];
+	wednesday: Task[];
+	friday: Task[];
+	saturday: Task[];
+	sunday: Task[];
+	weekly: Task[];
+	monthly: Task[];
+}
