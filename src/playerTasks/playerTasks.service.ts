13fc85dcd62b5a1f957771ff4a2349d4c3012f74
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-10-04 16:23:58 +0300

    add JSDocs

--- src/playerTasks/playerTasks.service.ts
+++ src/playerTasks/playerTasks.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
-import { PlayerTasks } from './type/task.type';
+import { PlayerTasks } from './type/tasks.type';
 
 @Injectable()
 export class PlayerTasksService implements OnModuleInit {
 	private tasks: PlayerTasks;
 
+	/**
+	 * Initializes the tasks by loading and parsing them from a JSON file.
+	 * This method is called once the module has been initialized.
+	 */
 	onModuleInit() {
 		const filePath = path.join(__dirname, 'playerTasks.json');
 		const fileContent = fs.readFileSync(filePath, 'utf-8');
