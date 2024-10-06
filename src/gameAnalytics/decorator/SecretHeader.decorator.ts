669b421c4b9974cfb4f11b0edf600a384ec2eb31
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-06 14:06:29 +0300

    add BadRequest throwing if Secret header is not provided to SecretHeader.decorator

--- src/gameAnalytics/decorator/SecretHeader.decorator.ts
+++ src/gameAnalytics/decorator/SecretHeader.decorator.ts
@@ -1,11 +1,18 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { APIErrorReason } from "../../common/controller/APIErrorReason";
+import { APIError } from "../../common/controller/APIError";
 
 /**
- * Get the `Secret` field from the request header.
+ * Extracts the `secret` field from the request header.
+ *
+ * Notice that it will throw `APIError REQUIRED` if no _Secret_ header can be found
  */
 export const SecretHeader = createParamDecorator((data: unknown, context: ExecutionContext): string => {
     const request = context.switchToHttp().getRequest<Request>();
     const secret = request.headers['secret'] as string;
+
+    if(!secret)
+        throw new APIError({reason: APIErrorReason.REQUIRED, message: 'The "Secret" header is required'})
 
     return secret;
 });
