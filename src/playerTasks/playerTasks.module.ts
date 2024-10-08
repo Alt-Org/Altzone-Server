390e7398d4f2330a11f56133878b70b129945d5d
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-10-08 23:20:26 +0300

    Add MongooseModule for TaskProgress in playerTasks.module.ts

--- src/playerTasks/playerTasks.module.ts
+++ src/playerTasks/playerTasks.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PlayerTasksService } from './playerTasks.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskProgress, TaskProgressSchema } from './playerTasks.schema';
 import { PlayerTasksController } from './playerTasks.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskProgress, TaskProgressSchema } from './playerTask.schema';
