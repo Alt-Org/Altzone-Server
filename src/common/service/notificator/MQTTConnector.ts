d2547ccab5408f1c4fbcd6f904e4eab692cf45d3
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-10 20:53:32 +0300

    add MQTTConnector class for publishing messages to broker

---
+++ src/common/service/notificator/MQTTConnector.ts
@@ -0,0 +1,36 @@
+import mqtt, { MqttClient } from "mqtt/*";
+import { envVars } from "../envHandler/envVars";
+
+export default class MQTTConnector {
+    private constructor() {
+        MQTTConnector.client = this.connect();
+    }
+
+    private static client: MqttClient;
+    private static instance: MQTTConnector; 
+
+    static getInstance(){
+        if(!MQTTConnector.instance)
+            MQTTConnector.instance = new MQTTConnector();
+
+        return MQTTConnector.instance;
+    }
+
+    connect(){
+        return mqtt.connect(
+            `mqtt://${envVars.MOSQUITTO_HOST}:${envVars.MOSQUITTO_PORT}`, 
+            { 
+                username: envVars.MOSQUITTO_PUBLISHER, 
+                password: envVars.MOSQUITTO_PUBLISHER_PASSWORD
+            }
+        );
+    }
+
+    async disconnect(){
+        return MQTTConnector.client.endAsync();
+    }
+
+    async publish(topic: string, payload: any){
+        MQTTConnector.client.publishAsync(topic, payload)
+    }
+}
