f061e3effce22ecbd5405779a65c158872a47f47
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-11 21:27:38 +0300

    add config for using TS in jest

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+const root = '<rootDir>'
+const codeDir = `${root}/src`;
+const testDir = `${root}/src/test`;
+
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+  
+  verbose: true,
+    //Get test coverage
+    // collectCoverage: true,
+    // collectCoverageFrom: [
+    //     `${codeDir}/**/*.ts`
+    // ],
+
+    //Run test from this folder(s) only
+    testMatch: [
+        `${testDir}/**/*.test.ts`
+    ],
+
+    // transform: {
+    //     "^.+\\.[t|j]sx?$": "babel-jest"
+    // },
+    setupFiles: [
+        `${testDir}/test_utils/env.ts`,
+        `${testDir}/test_utils/disable-console-log.ts`
+    ],
+    setupFilesAfterEnv: [
+        `${testDir}/test_utils/connect-to-db.ts`
+    ]
+};
