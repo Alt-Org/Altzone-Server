cad5774e2f79819d1b9ab13e4b6f653f3801816c
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-11 13:44:37 +0300

    add JSDocs to MQTTConnector

--- src/common/service/notificator/MQTTConnector.ts
+++ src/common/service/notificator/MQTTConnector.ts
@@ -1,6 +1,11 @@
 import mqtt, { MqttClient } from "mqtt";
 import { envVars } from "../envHandler/envVars";
 
+/**
+ * Class for handling direct communications with MQTT broker.
+ *
+ * Notice that it is a singleton
+ */
 export default class MQTTConnector {
     private constructor() {
         MQTTConnector.client = this.connect();
@@ -9,6 +14,10 @@ export default class MQTTConnector {
     private static client: MqttClient;
     private static instance: MQTTConnector; 
 
+    /**
+     * Gets instance of the MQTTConnector class.
+     * @returns MQTTConnector class instance
+     */
     static getInstance(){
         if(!MQTTConnector.instance)
             MQTTConnector.instance = new MQTTConnector();
@@ -16,6 +25,14 @@ export default class MQTTConnector {
         return MQTTConnector.instance;
     }
 
+    /**
+     * Connects to MQTT broker.
+     *
+     * Notice that most likely the class is already connected 
+     * since it is happen on the MQTTConnector class instance creation. 
+     * So if you did not disconnected, you do not need to use this method.
+     * @returns MQTTClient from the mqtt library.
+     */
     connect(){
         return mqtt.connect(
             `mqtt://${envVars.MOSQUITTO_HOST}:${envVars.MOSQUITTO_PORT}`, 
@@ -26,10 +43,20 @@ export default class MQTTConnector {
         );
     }
 
+    /**
+     * Disconnects from the MQTT broker.
+     */
     async disconnect(){
-        return MQTTConnector.client.endAsync();
+        MQTTConnector.client.endAsync();
     }
 
+    /**
+     * Publish a message to specified topic.
+     *
+     * Notice that you will not get any success / error response since it is not supported by MQTT.
+     * @param topic where to publish
+     * @param payload what to publish
+     */
     async publish(topic: string, payload: string | Buffer){
         MQTTConnector.client.publishAsync(topic, payload);
     }
