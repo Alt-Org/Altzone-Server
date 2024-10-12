6d8254691c90d605bc40f538ef957b4a17a581b1
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2024-10-12 17:28:29 +0300

    add index file to clan_module/soulhome

---
+++ src/clan_module/soulhome/index.ts
@@ -0,0 +1,9 @@
+import { SoulHomeController } from './soulhome.controller';
+import { SoulHomeService } from './soulhome.service';
+import SoulHomeHelperService from './utils/soulHomeHelper.service';
+
+export {
+    SoulHomeController,
+    SoulHomeService,
+    SoulHomeHelperService
+}
