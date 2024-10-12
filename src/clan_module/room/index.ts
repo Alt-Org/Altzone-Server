6a65648dba8c10e651fd5f80a7d4d9ce3ac6d9f9
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2024-10-12 17:28:19 +0300

    add index file to clan_module/room

---
+++ src/clan_module/room/index.ts
@@ -0,0 +1,9 @@
+import { RoomController } from './room.controller';
+import { RoomService } from './room.service';
+import RoomHelperService from './utils/room.helper.service';
+
+export {
+    RoomController,
+    RoomService,
+    RoomHelperService
+}
