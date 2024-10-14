5d93c0995c31c508a34dad93704e47e69b14eb7c
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-14 20:35:33 +0300

    fix all problems with import for clan module test suite

--- src/test/clan/clan.service.test.ts
+++ src/test/clan/clan.service.test.ts
@@ -5,29 +5,35 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RequestHelperModule } from '../../requestHelper/requestHelper.module';
 import { PlayerSchema } from '../../player/player.schema';
 import { PlayerModule } from '../../player/player.module';
+import { envVars } from '../../common/service/envHandler/envVars';
 
 import { ModelName } from '../../common/enum/modelName.enum';
 
 import { ClanController, ClanHelperService, ClanSchema, ClanService, isClanExists, JoinSchema, JoinService, PlayerCounterFactory } from '../../clan_module/clan';
 import { isStockExists, StockController, StockSchema, StockService } from '../../clan_module/stock';
-import { isItemExists, ItemController, ItemHelperService, ItemMoverService, ItemService } from '../../clan_module/item';
+import { isItemExists, ItemController, ItemHelperService, ItemMoverService, ItemSchema, ItemService } from '../../clan_module/item';
 import { RoomController, RoomHelperService, RoomSchema, RoomService } from '../../clan_module/room';
 import { SoulHomeController, SoulHomeHelperService, SoulhomeSchema, SoulHomeService } from '../../clan_module/soulhome';
+import { AuthModule } from '../../auth/auth.module';
 
 describe('clan module test suite', () => {
     it('Should work', async () => {
         
         const moduleRef = await Test.createTestingModule({
             imports: [
+                MongooseModule.forRoot('mongodb://127.0.0.1:27017/', {dbName: envVars.MONGO_DB_NAME}),
                 MongooseModule.forFeature([ 
                     { name: ModelName.CLAN, schema: ClanSchema }, 
                     { name: ModelName.JOIN, schema: JoinSchema },
                     { name: ModelName.PLAYER, schema: PlayerSchema },
                     { name: ModelName.STOCK, schema: StockSchema },
                     { name: ModelName.SOULHOME, schema: SoulhomeSchema },
-                    { name: ModelName.ROOM, schema: RoomSchema }
+                    { name: ModelName.ROOM, schema: RoomSchema },
+                    { name: ModelName.ITEM, schema: ItemSchema }
                 ]),
-        
+                
+                AuthModule,
+                
                 forwardRef(() => PlayerModule),
                 RequestHelperModule
             ],
@@ -44,10 +50,12 @@ describe('clan module test suite', () => {
             ],
         }).compile();
         
-        //const service = await moduleRef.resolve(ClanService);
+        const service = await moduleRef.resolve(ClanService);
 
-        //console.log(service);
+        console.log(await service.readAll());
+
+        const [resp, errors] = await service.readAll();
   
-        expect(1).toBe(1);
+        expect(errors).toHaveLength(1);
     });
 });
