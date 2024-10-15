1d53ea9a3bc3a2117115659684e19b990ccb437d
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-15 19:48:23 +0300

    add creation of default user in DB

--- src/test/test_utils/connect-to-db.ts
+++ src/test/test_utils/connect-to-db.ts
@@ -1,33 +1,55 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { envVars } from '../../common/service/envHandler/envVars';
-
-//Establishing connection
+import { mongodOptions, mongooseOptions, mongoString } from './const/db';
+import { ModelName } from '../../common/enum/modelName.enum';
+import { ProfileSchema } from '../../profile/profile.schema';
+import LoggedUser from './const/loggedUser';
+import { PlayerSchema } from '../../player/player.schema';
 
 let mongod: MongoMemoryServer;
 
 beforeAll(async () => {
-    mongod = await MongoMemoryServer.create({
-        auth: {
-            customRootName: envVars.MONGO_USERNAME,
-            customRootPwd: envVars.MONGO_PASSWORD,
-            enable: true
-        },
-        instance: {
-            dbName: envVars.MONGO_DB_NAME,
-            port: Number.parseInt(envVars.MONGO_PORT)
-        }
-    });
+    mongod = await MongoMemoryServer.create(mongodOptions);
+
+    try {
+        await mongoose.connect(mongoString, mongooseOptions);
+    } catch (error) {
+        console.error('Could not connect to DB', error);
+    }
+
+    // try {
+    //     const profileModel = mongoose.model(ModelName.PROFILE, ProfileSchema);
+    //     const defaultProfile = LoggedUser.getProfile();
+    //     const {_id: profileId, ...profileToCreate} = defaultProfile;
+    //     const profileResp = await profileModel.create(profileToCreate);
+    //     LoggedUser.setProfile_id(profileResp._id.toString());
+    
+    //     const playerModel = mongoose.model(ModelName.PLAYER, PlayerSchema);
+    //     const defaultPlayer = LoggedUser.getPlayer();
+    //     const {_id: playerId, ...playerToCreate} = defaultPlayer;
+    //     const playerResp = await playerModel.create(playerToCreate);
+    //     LoggedUser.setPlayer_id(playerResp._id.toString());
+    // } catch (error) {
+    //     console.error('Could not insert default data to DB', error);
+    // }
 });
 
 beforeEach(async () => {
-    await mongoose.connect(mongod.getUri(), { 
-        dbName: envVars.MONGO_DB_NAME, 
-        auth: { 
-            username: envVars.MONGO_USERNAME, 
-            password: envVars.MONGO_PASSWORD 
-        }
-    });
+    try {
+        const profileModel = mongoose.model(ModelName.PROFILE, ProfileSchema);
+        const defaultProfile = LoggedUser.getProfile();
+        const {_id: profileId, ...profileToCreate} = defaultProfile;
+        const profileResp = await profileModel.create(profileToCreate);
+        LoggedUser.setProfile_id(profileResp._id.toString());
+    
+        const playerModel = mongoose.model(ModelName.PLAYER, PlayerSchema);
+        const defaultPlayer = LoggedUser.getPlayer();
+        const {_id: playerId, ...playerToCreate} = defaultPlayer;
+        const playerResp = await playerModel.create(playerToCreate);
+        LoggedUser.setPlayer_id(playerResp._id.toString());
+    } catch (error) {
+        console.error('Could not insert default data to DB', error);
+    }
 });
 
 afterAll(async () => {
