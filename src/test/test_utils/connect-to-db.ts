9d7bce3ce6ab6af5a33ff134d85661371a46558b
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-16 17:06:42 +0300

    add db init before each test and db cleaning after each

--- src/test/test_utils/connect-to-db.ts
+++ src/test/test_utils/connect-to-db.ts
@@ -1,58 +1,64 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { mongodOptions, mongooseOptions, mongoString } from './const/db';
+import { mongoString } from './const/db';
 import { ModelName } from '../../common/enum/modelName.enum';
 import { ProfileSchema } from '../../profile/profile.schema';
 import LoggedUser from './const/loggedUser';
 import { PlayerSchema } from '../../player/player.schema';
+import { envVars } from '../../common/service/envHandler/envVars';
 
 let mongod: MongoMemoryServer;
 
 beforeAll(async () => {
-    mongod = await MongoMemoryServer.create(mongodOptions);
+    const dbName = `altzone_${process.env.JEST_WORKER_ID}`;
+
+    mongod = await MongoMemoryServer.create({
+        instance: {
+            dbName: dbName,
+            port: Number.parseInt(envVars.MONGO_PORT)
+        }
+    });
 
     try {
-        await mongoose.connect(mongoString, mongooseOptions);
+        await mongoose.connect(mongoString, {dbName: dbName});
     } catch (error) {
         console.error('Could not connect to DB', error);
     }
-
-    // try {
-    //     const profileModel = mongoose.model(ModelName.PROFILE, ProfileSchema);
-    //     const defaultProfile = LoggedUser.getProfile();
-    //     const {_id: profileId, ...profileToCreate} = defaultProfile;
-    //     const profileResp = await profileModel.create(profileToCreate);
-    //     LoggedUser.setProfile_id(profileResp._id.toString());
-    
-    //     const playerModel = mongoose.model(ModelName.PLAYER, PlayerSchema);
-    //     const defaultPlayer = LoggedUser.getPlayer();
-    //     const {_id: playerId, ...playerToCreate} = defaultPlayer;
-    //     const playerResp = await playerModel.create(playerToCreate);
-    //     LoggedUser.setPlayer_id(playerResp._id.toString());
-    // } catch (error) {
-    //     console.error('Could not insert default data to DB', error);
-    // }
 });
 
 beforeEach(async () => {
-    try {
-        const profileModel = mongoose.model(ModelName.PROFILE, ProfileSchema);
-        const defaultProfile = LoggedUser.getProfile();
-        const {_id: profileId, ...profileToCreate} = defaultProfile;
-        const profileResp = await profileModel.create(profileToCreate);
-        LoggedUser.setProfile_id(profileResp._id.toString());
-    
-        const playerModel = mongoose.model(ModelName.PLAYER, PlayerSchema);
-        const defaultPlayer = LoggedUser.getPlayer();
-        const {_id: playerId, ...playerToCreate} = defaultPlayer;
-        const playerResp = await playerModel.create(playerToCreate);
-        LoggedUser.setPlayer_id(playerResp._id.toString());
-    } catch (error) {
-        console.error('Could not insert default data to DB', error);
-    }
+    await initDB();
+});
+
+afterEach(async () => {
+    await clearDB();
 });
 
 afterAll(async () => {
-    await mongoose.disconnect()
+    await mongoose.disconnect();
     await mongod.stop();
 });
+
+
+async function initDB() {
+    const profileModel = mongoose.model(ModelName.PROFILE, ProfileSchema);
+    const defaultProfile = LoggedUser.getProfile();
+    const {_id: profileId, ...profileToCreate} = defaultProfile;
+    const profileResp = await profileModel.create(profileToCreate);
+    LoggedUser.setProfile_id(profileResp._id.toString());
+
+    const playerModel = mongoose.model(ModelName.PLAYER, PlayerSchema);
+    const defaultPlayer = LoggedUser.getPlayer();
+    const {_id: playerId, ...playerToCreate} = defaultPlayer;
+    const playerResp = await playerModel.create(playerToCreate);
+    LoggedUser.setPlayer_id(playerResp._id.toString());
+}
+
+async function clearDB(){
+    const collections = mongoose.connection.collections;
+    
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+};
