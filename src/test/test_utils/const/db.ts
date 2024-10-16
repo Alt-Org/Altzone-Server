219b3aa8ecc0851fc7ea47235618ef85e12c41fa
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-16 17:05:52 +0300

    remove authentication from test db

--- src/test/test_utils/const/db.ts
+++ src/test/test_utils/const/db.ts
@@ -1,21 +1,12 @@
 import { envVars } from '../../../common/service/envHandler/envVars';
 
-export const mongoString = `mongodb://${envVars.MONGO_USERNAME}:${envVars.MONGO_PASSWORD}@${envVars.MONGO_HOST}:${envVars.MONGO_PORT}/`;
+export const mongoString = `mongodb://${envVars.MONGO_HOST}:${envVars.MONGO_PORT}/`;
 
 export const mongooseOptions = { 
-    dbName: envVars.MONGO_DB_NAME, 
-    auth: { 
-        username: envVars.MONGO_USERNAME, 
-        password: envVars.MONGO_PASSWORD 
-    }
+    dbName: envVars.MONGO_DB_NAME,
 }
 
 export const mongodOptions = {
-    auth: {
-        customRootName: envVars.MONGO_USERNAME,
-        customRootPwd: envVars.MONGO_PASSWORD,
-        enable: true
-    },
     instance: {
         dbName: envVars.MONGO_DB_NAME,
         port: Number.parseInt(envVars.MONGO_PORT)
