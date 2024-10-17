b026848484f2a267be56021f3d0c8a9042e4dd68
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-10-17 04:10:50 +0300

    create param decorator to get the period from request

---
+++ src/playerTasks/decorator/period.decorator.ts
@@ -0,0 +1,24 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { TaskFrequency } from "../enum/taskFrequency.enum";
+
+/**
+ * Decorator to get the period query parameter from the request.
+ *
+ * @param context - The execution context of the request.
+ * @returns Task frequency and defaults to daily if period is not provided in the query;
+ */
+export const Period = createParamDecorator((_, context: ExecutionContext) => {
+	const request = context.switchToHttp().getRequest();
+	const period = request.query["period"] ?? "today";
+
+	switch (period) {
+		case "today":
+			return TaskFrequency.DAILY;
+		case "week":
+			return TaskFrequency.WEEKLY;
+		case "month":
+			return TaskFrequency.MONTHLY;
+		default:
+			return TaskFrequency.DAILY;
+	}
+});
