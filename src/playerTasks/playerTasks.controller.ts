e1abf67a80547c2edb84505ca4ff5526f15dfc84
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-10-17 04:18:42 +0300

    Added logic for the getPlayerTasks

--- src/playerTasks/playerTasks.controller.ts
+++ src/playerTasks/playerTasks.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PlayerTasksService } from './playerTasks.service';
+import { LoggedUser } from '../common/decorator/param/LoggedUser.decorator';
+import { User } from '../auth/user';
+import { TaskFrequency } from './enum/taskFrequency.enum';
+import { UniformResponse } from '../common/decorator/response/UniformResponse';
+import { ModelName } from '../common/enum/modelName.enum';
+import { Period } from './decorator/period.decorator';
 
 @Controller('playerTasks')
 export class PlayerTasksController {
@@ -8,6 +14,8 @@ export class PlayerTasksController {
 	){}
 
 	@Get()
-	getPlayerTasks() {
+	@UniformResponse(ModelName.PLAYER_TASK)
+	async getPlayerTasks(@Period() period: TaskFrequency, @LoggedUser() user: User) {
+		return await this.playerTasksService.getPlayerTasks(user.player_id, period)
 	}
 }
