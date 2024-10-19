e99c74793fa8f8aa73e65525e36f07008c31147f
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-19 14:49:43 +0300

    move clan service creation to beforeAll hook

--- src/test/clan_module/clan.service.test.ts
+++ src/test/clan_module/clan.service.test.ts
@@ -2,14 +2,19 @@ import { ObjectId } from 'mongodb';
 import LoggedUser from '../test_utils/const/loggedUser';
 import Factory from './data/factory';
 import ClanModule from './modules/clan.module';
+import { ClanService } from '../../clan_module/clan/clan.service';
 
 describe('ClanService test suite', () => {
+    let clanService: ClanService;
     const clanBuilder = Factory.getBuilder('CreateClanDto');
     const loggedPlayer = LoggedUser.getPlayer();
     const clanModel = ClanModule.getClanModel();
 
+    beforeAll(async () => {
+        clanService = await ClanModule.getClanService(); 
+    });
+
     it('Should create a clan in DB, if valid params provided', async () => {
-        const clanService = await ClanModule.getClanService();
         const clanName = 'clan1';
         const clanToCreate = clanBuilder.setName(clanName).build();
 
