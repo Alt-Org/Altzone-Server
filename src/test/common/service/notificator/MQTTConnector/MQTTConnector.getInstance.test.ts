bbb072593a0ee86c3c70f759c1ff9da1e1378f3e
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-23 11:56:34 +0300

    add skeleton for MQTTConnector.getInstance() test

---
+++ src/test/common/service/notificator/MQTTConnector/MQTTConnector.getInstance.test.ts
@@ -0,0 +1,9 @@
+describe('MQTTConnector.getInstance() test suite', () => {
+    it('Should return an instance of the MQTTConnector', () => {
+        
+    });
+
+    it('Should call mqtt.connect() only once', () => {
+        
+    });
+});
