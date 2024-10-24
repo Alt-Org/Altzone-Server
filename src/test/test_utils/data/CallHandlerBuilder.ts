836a118c1fa282dfe93300c6c02307fe5b63fe79
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-24 16:29:57 +0300

    add builder for CallHandler objects of NestJS

---
+++ src/test/test_utils/data/CallHandlerBuilder.ts
@@ -0,0 +1,26 @@
+import { CallHandler } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+
+export default class CallHandlerBuilder {
+    private readonly base: Partial<CallHandler> = {
+        handle: function (): Observable<any> {
+            return of(null);
+        },
+    };
+
+    build(): CallHandler {
+        return { ...this.base } as CallHandler;
+    }
+
+    setHandleResponse(response: any) {
+        this.base.handle = function (): Observable<any> {
+            return of(response);
+        };
+        return this;
+    }
+
+    setHandle(handlerFunction: () => Observable<any>) {
+        this.base.handle = handlerFunction;
+        return this;
+    }
+}
