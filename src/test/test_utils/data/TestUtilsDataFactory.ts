d5eff613695ec92cb890816589bb6e26d7c67bde
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-10-25 13:06:49 +0300

    add APIErrorBuilder test data builder

--- src/test/test_utils/data/TestUtilsDataFactory.ts
+++ src/test/test_utils/data/TestUtilsDataFactory.ts
@@ -1,3 +1,4 @@
+import APIErrorBuilder from "./APIErrorBuilder";
 import ArgumentsHostBuilder from "./ArgumentsHostBuilder";
 import BodyBuilder from "./BodyBuilder";
 import CallHandlerBuilder from "./CallHandlerBuilder";
@@ -7,7 +8,8 @@ import RequestBuilder from "./RequestBuilder";
 
 type BuilderName = 
     'Body' | 'Request' | 
-    'ExecutionContext' | 'CallHandler' | 'ArgumentsHost';
+    'ExecutionContext' | 'CallHandler' | 'ArgumentsHost' |
+    'APIError';
 
 type BuilderMap = {
     Body: BodyBuilder,
@@ -15,7 +17,9 @@ type BuilderMap = {
 
     ExecutionContext: ExecutionContextBuilder,
     CallHandler: CallHandlerBuilder,
-    ArgumentsHost: ArgumentsHostBuilder
+    ArgumentsHost: ArgumentsHostBuilder,
+
+    APIError: APIErrorBuilder
 };
 
 export default class TestUtilDataFactory {
@@ -25,12 +29,16 @@ export default class TestUtilDataFactory {
                 return new BodyBuilder() as BuilderMap[T];
             case 'Request':
                 return new RequestBuilder() as BuilderMap[T];
+
             case 'ExecutionContext':
                 return new ExecutionContextBuilder() as BuilderMap[T];
             case 'CallHandler':
                 return new CallHandlerBuilder() as BuilderMap[T];
             case 'ArgumentsHost':
                 return new ArgumentsHostBuilder() as BuilderMap[T];
+
+            case 'APIError':
+                return new APIErrorBuilder() as BuilderMap[T];
 
             default:
                 throw new Error(`Unknown builder name: ${builderName}`);
