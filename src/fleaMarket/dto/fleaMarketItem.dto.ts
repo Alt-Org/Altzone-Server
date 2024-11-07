f8c03d508e91148259cbf9fd920a5e322d0ccc3c
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-07 07:46:27 +0200

    add FleaMarketItemDto with properties for item representation

---
+++ src/fleaMarket/dto/fleaMarketItem.dto.ts
@@ -0,0 +1,39 @@
+import { Expose } from "class-transformer";
+import AddType from "../../common/base/decorator/AddType.decorator";
+import { ExtractField } from "../../common/decorator/response/ExtractField";
+import { ItemName } from "../../clanInventory/item/enum/itemName.enum";
+import { Recycling } from "../../clanInventory/item/enum/recycling.enum";
+import { QualityLevel } from "../../clanInventory/item/enum/qualityLevel.enum";
+import { Status } from "../enum/status.enum";
+
+@AddType("FleaMarketItemDto")
+export class FleaMarketItemDto {
+	@ExtractField()
+	@Expose()
+	_id: string;
+
+	@Expose()
+	name: ItemName;
+
+	@Expose()
+	weight: number;
+
+	@Expose()
+	recycling: Recycling;
+
+	@Expose()
+	qualityLevel: QualityLevel;
+
+	@Expose()
+	unityKey: string;
+
+	@Expose()
+	status: Status;
+
+	@Expose()
+	isFurniture: boolean;
+
+	@ExtractField()
+	@Expose()
+	clan_id: string;
+}
