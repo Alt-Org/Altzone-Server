9fb68116f29683c753c5cd1c298be97846a9d209
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-09 18:19:42 +0200

    add builder for profile schema

--- src/test/profile/data/profileBuilderFactory.ts
+++ src/test/profile/data/profileBuilderFactory.ts
@@ -1,14 +1,17 @@
 import UpdateProfileDtoBuilder from "./profile/updateProfileDtoBuilder";
 import CreateProfileDtoBuilder from "./profile/createProfileDtoBuilder";
 import ProfileDtoBuilder from "./profile/profileDtoBuilder";
+import ProfileBuilder from "./profile/profileBuilder";
 
 type BuilderName =
-    'CreateProfileDto' | 'ProfileDto' | 'UpdateProfileDto';
+    'CreateProfileDto' | 'ProfileDto' | 'UpdateProfileDto' |
+    'Profile';
 
 type BuilderMap = {
     CreateProfileDto: CreateProfileDtoBuilder,
     ProfileDto: ProfileDtoBuilder,
-    UpdateProfileDto: UpdateProfileDtoBuilder
+    UpdateProfileDto: UpdateProfileDtoBuilder,
+    Profile: ProfileBuilder
 };
 
 export default class ProfileBuilderFactory {
@@ -20,6 +23,8 @@ export default class ProfileBuilderFactory {
                 return new ProfileDtoBuilder() as BuilderMap[T];
             case 'UpdateProfileDto':
                 return new UpdateProfileDtoBuilder() as BuilderMap[T];
+            case 'Profile':
+                return new ProfileBuilder() as BuilderMap[T];
             default:
                 throw new Error(`Unknown builder name: ${builderName}`);
         }
