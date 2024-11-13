42ed47367312a3e526a3770513f12065e6015331
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-13 12:11:06 +0200

    improve security

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -9,31 +9,78 @@ import {PlayerService} from "../player/player.service";
 import {AddBasicService} from "../common/base/decorator/AddBasicService.decorator";
 import {BasicServiceDummyAbstract} from "../common/base/abstract/basicServiceDummy.abstract";
 import {IBasicService} from "../common/base/interface/IBasicService";
+import * as argon2 from 'argon2';
+import {envVars} from "../common/service/envHandler/envVars";
+import ServiceError from "../common/service/basicService/ServiceError";
+import {SEReason} from "../common/service/basicService/SEReason";
+import BasicService from "../common/service/basicService/BasicService";
+import {CreateProfileDto} from "./dto/createProfile.dto";
+import {ProfileDto} from "./dto/profile.dto";
 
-//TODO: password hashing via bcrypt
+const ARGON2_CONFIG = {
+    type: argon2.argon2id,
+    memoryCost: parseInt(envVars.PSW_MEMORY),
+    timeCost: parseInt(envVars.PSW_TIME),
+    parallelism: parseInt(envVars.PSW_PARALLELISM)
+};
 
 @Injectable()
 @AddBasicService()
-export class ProfileService extends BasicServiceDummyAbstract<ProfileDocument> implements IBasicService<ProfileDocument>{
+export class ProfileService extends BasicServiceDummyAbstract<ProfileDocument> implements IBasicService<ProfileDocument> {
     public constructor(
         @InjectModel(Profile.name) public readonly model: Model<Profile>,
         private readonly playerService: PlayerService,
         private readonly requestHelperService: RequestHelperService
-    ){
+    ) {
         super();
         this.refsInModel = [ModelName.PLAYER];
         this.modelName = ModelName.PROFILE;
+        this.basicService = new BasicService(model);
     }
 
     public readonly refsInModel: ModelName[];
     public readonly modelName: ModelName;
+    public readonly basicService: BasicService;
 
-    public createWithHashedPassword(condition: object): Promise<MongooseError | Profile | null> {
-        //TODO: hash psw here
-        return ;
+    /**
+     * Creates a new Profile in DB with hashed password.
+     *
+     * @param profile - The Profile data to create.
+     * @returns  created Profile or an array of service errors if any occurred.
+     */
+    async createWithHashedPassword(profile: CreateProfileDto): Promise<[ProfileDto | null, ServiceError[] | null]> {
+        const [hashedPassword, errors] = await this.hashPassword(profile.password);
+
+        if(errors)
+            return [ null, errors ];
+
+        return this.basicService.createOne<CreateProfileDto, ProfileDto>({...profile, password: hashedPassword});
     }
 
     public clearCollectionReferences = async (_id: Types.ObjectId, ignoreReferences?: IgnoreReferencesType): Promise<void> => {
         await this.playerService.deleteByCondition({profile_id: _id}, {isOne: true});
+    }
+
+    /**
+     * Hash the provided password
+     * @param password password to hash
+     * @return hashed password if everything is ok, or ServiceError if it could not be hashed
+     */
+    private async hashPassword(password: string): Promise<[string | null, ServiceError[] | null]> {
+        try {
+            const hashedPassword = await argon2.hash(password, ARGON2_CONFIG);
+            return [ hashedPassword, null ];
+        } catch (error) {
+            return [
+                null,
+                [
+                    new ServiceError({
+                        reason: SEReason.UNEXPECTED,
+                        message: 'Could not hash the provided password',
+                        additional: error
+                    })
+                ]
+            ];
+        }
     }
 }
