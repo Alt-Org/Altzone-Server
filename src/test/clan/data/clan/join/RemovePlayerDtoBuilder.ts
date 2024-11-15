5964e81c5bf16fd0724db332be86c44dfc758a67
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-15 15:42:52 +0200

    add test data builders for clan

---
+++ src/test/clan/data/clan/join/RemovePlayerDtoBuilder.ts
@@ -0,0 +1,17 @@
+import { RemovePlayerDTO } from "../../../../../clan_module/clan/join/dto/removePlayer.dto";
+import IDataBuilder from "../../../../test_utils/interface/IDataBuilder";
+
+export default class RemovePlayerDtoBuilder implements IDataBuilder<RemovePlayerDTO> {
+    private readonly base: RemovePlayerDTO = {
+        player_id: undefined
+    };
+
+    build() {
+        return { ...this.base };
+    }
+
+    setPlayerId(player_id: string) {
+        this.base.player_id = player_id;
+        return this;
+    }
+}
