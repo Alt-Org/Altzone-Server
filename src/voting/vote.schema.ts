0f28689c6045e69346940ae1c39a28a72f1ea256
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-25 17:28:04 +0200

    refactor Vote schema to import Choice from the correct type module

--- src/voting/vote.schema.ts
+++ src/voting/vote.schema.ts
@@ -1,7 +1,8 @@
 import { Schema as MongooseSchema } from "mongoose";
 import { ModelName } from "../common/enum/modelName.enum";
 import { Prop } from "@nestjs/mongoose";
-import { Choice, ItemVoteChoice } from "./enum/choiceType.enum";
+import { ItemVoteChoice } from "./enum/choiceType.enum";
+import { Choice } from "./type/choice.type";
 
 export class Vote {
 	@Prop({
