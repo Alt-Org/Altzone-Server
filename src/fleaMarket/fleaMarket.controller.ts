b7712b1b5d9e5569f246099ebf3eadf4ee62a01f
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-25 17:11:38 +0200

    add sell endpoint to FleaMarketController for item selling functionality

--- src/fleaMarket/fleaMarket.controller.ts
+++ src/fleaMarket/fleaMarket.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { FleaMarketService } from "./fleaMarket.service";
 import { UniformResponse } from "../common/decorator/response/UniformResponse";
 import { ModelName } from "../common/enum/modelName.enum";
@@ -8,6 +8,12 @@ import { Serialize } from "../common/interceptor/response/Serialize";
 import { FleaMarketItemDto } from "./dto/fleaMarketItem.dto";
 import { GetAllQuery } from "../common/decorator/param/GetAllQuery";
 import { IGetAllQuery } from "../common/interface/IGetAllQuery";
+import { LoggedUser } from "../common/decorator/param/LoggedUser.decorator";
+import { User } from "../auth/user";
+import { SellItemDto } from "./dto/sellItem.dto";
+import { VotingType } from "../voting/enum/VotingType.enum";
+import { APIError } from "../common/controller/APIError";
+import { APIErrorReason } from "../common/controller/APIErrorReason";
 
 @Controller("fleaMarket")
 export class FleaMarketController {
@@ -26,5 +32,27 @@ export class FleaMarketController {
 	@UniformResponse(ModelName.FLEA_MARKET_ITEM)
 	async getAll(@GetAllQuery() query: IGetAllQuery) {
 		return await this.service.readMany(query);
+	}
+
+	@Post("sell")
+	@UniformResponse()
+	async sell(@Body() sellItemDto: SellItemDto, @LoggedUser() user: User) {
+		const clanId = await this.service.getClanId(
+			sellItemDto.item_id,
+			user.player_id
+		);
+
+		if (!clanId)
+			throw new APIError({
+				reason: APIErrorReason.NOT_AUTHORIZED,
+				message: "The item does not belong to the clan of logged in player",
+			});
+
+		await this.service.handleSellItem(
+			sellItemDto.item_id,
+			clanId,
+			user.player_id,
+			VotingType.SELLING_ITEM
+		);
 	}
 }
