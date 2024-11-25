02ef97b19ef7df079fa3c5413249823203ff4439
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-25 17:19:20 +0200

    add VotingType enum to define types of voting actions

---
+++ src/voting/enum/VotingType.enum.ts
@@ -0,0 +1,10 @@
+/**
+ * Enum representing the types of voting in the voting schema.
+ * 
+ * This enum is used to specify the type of voting action being performed,
+ * such as voting for selling an item or buying an item.
+ */
+export enum VotingType {
+	SELLING_ITEM = "selling_item",
+	BUYING_ITEM = "buying_item",
+}
