c7d70db3a68ac04748690f4b1b14a9100258eb12
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-25 17:28:14 +0200

    add ItemVoteChoice enum and Choice type for vote options

---
+++ src/voting/enum/choiceType.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Enum for item vote choices.
+ */
+export enum ItemVoteChoice {
+    /**
+     * Represents a vote to accept the item.
+     */
+    YES = 'accept',
+
+    /**
+     * Represents a vote to reject the item.
+     */
+    NO = 'reject',
+}
---
+++ src/voting/type/choice.type.ts
@@ -0,0 +1,9 @@
+import { ItemVoteChoice } from "../enum/choiceType.enum";
+
+/**
+ * Represents the possible choices for a vote.
+ *
+ * This type is used to define the choices that a player can make when voting.
+ * It is an alias and should contain all the possible voting option enums.
+ */
+export type Choice = ItemVoteChoice;
