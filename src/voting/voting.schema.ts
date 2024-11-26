d5023ec9d94ba89676f059e3fba98533b2504ab0
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-26 18:05:08 +0200

    changed item_id to entity_id to be more generic and removed the unnecessary pre save validation

--- src/voting/voting.schema.ts
+++ src/voting/voting.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument, Schema as MongooseSchema } from "mongoose";
 import { ModelName } from "../common/enum/modelName.enum";
-import { Player } from "../player/player.schema";
 import { VotingType } from "./enum/VotingType.enum";
 import { Vote } from "./vote.schema";
 
@@ -41,12 +40,8 @@ export class Voting {
 	@Prop({ type: [Vote], default: [] })
 	votes: Vote[];
 
-	@Prop({
-		type: MongooseSchema.Types.ObjectId,
-		ref: ModelName.ITEM,
-		required: true,
-	})
-	item_id?: string;
+	@Prop({ type: MongooseSchema.Types.ObjectId })
+	entity_id?: string;
 }
 
 export const VotingSchema = SchemaFactory.createForClass(Voting);
@@ -56,19 +51,4 @@ VotingSchema.virtual(ModelName.PLAYER, {
 	localField: "organizer_id",
 	foreignField: "_id",
 	justOne: true,
-});
-
-VotingSchema.pre("save", function (next) {
-	if (
-		(this.type === VotingType.BUYING_ITEM ||
-			this.type === VotingType.SELLING_ITEM) &&
-		!this.item_id
-	) {
-		return next(
-			new Error(
-				"item_id is required for SELLING_ITEM and BUYING_ITEM voting types"
-			)
-		);
-	}
-	next();
 });
