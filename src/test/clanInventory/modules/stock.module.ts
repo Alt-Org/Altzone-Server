addea40847d7180dcf6983ba3f7168d684df6bce
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-26 15:27:13 +0200

    add stock testing module

---
+++ src/test/clanInventory/modules/stock.module.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import ClanCommonModule from "./clanInventoryCommon";
+import {ModelName} from "../../../common/enum/modelName.enum";
+import {StockService} from "../../../clanInventory/stock/stock.service";
+import {isStockExists} from "../../../clanInventory/stock/decorator/validation/IsStockExists.decorator";
+import {StockSchema} from "../../../clanInventory/stock/stock.schema";
+
+export default class RoomModule {
+    private constructor() {
+    }
+
+    static async getStockService() {
+        const module = await ClanCommonModule.getModule();
+        return await module.resolve(StockService);
+    }
+
+    static async getIsStockExists() {
+        const module = await ClanCommonModule.getModule();
+        return await module.resolve(isStockExists);
+    }
+
+    static getItemModel() {
+        return mongoose.model(ModelName.STOCK, StockSchema);
+    }
+}
