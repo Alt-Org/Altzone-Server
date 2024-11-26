93a9b5b5febe210aa957a04e9ab6294398e1e33a
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-26 15:14:33 +0200

    add tests data builders for room module

---
+++ src/test/clanInventory/data/room/ActivateRoomDtoBuilder.ts
@@ -0,0 +1,22 @@
+import {ActivateRoomDto} from "../../../../clanInventory/room/dto/ActivateRoom.dto";
+
+export default class ActivateRoomDtoBuilder {
+    private readonly base: Partial<ActivateRoomDto> = {
+        room_ids: [],
+        durationS: 300,
+    };
+
+    build(): ActivateRoomDto {
+        return { ...this.base } as ActivateRoomDto;
+    }
+
+    setRoomIds(roomIds: string[]) {
+        this.base.room_ids = roomIds;
+        return this;
+    }
+
+    setDuration(durationS: number) {
+        this.base.durationS = durationS;
+        return this;
+    }
+}
