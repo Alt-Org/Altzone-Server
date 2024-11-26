6ba089eac8eabbb220a8c9110550e8b9282f8d1a
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-11-26 18:04:18 +0200

    remove choice validation logic from Vote schema

--- src/voting/vote.schema.ts
+++ src/voting/vote.schema.ts
@@ -1,7 +1,6 @@
 import { Schema as MongooseSchema } from "mongoose";
 import { ModelName } from "../common/enum/modelName.enum";
 import { Prop } from "@nestjs/mongoose";
-import { ItemVoteChoice } from "./enum/choiceType.enum";
 import { Choice } from "./type/choice.type";
 
 export class Vote {
@@ -15,13 +14,6 @@ export class Vote {
 	@Prop({
 		type: String,
 		required: true,
-		validate: {
-			validator: function (value: any) {
-				const validChoices = [...Object.values(ItemVoteChoice)];
-				return validChoices.includes(value);
-			},
-			message: (props) => `${props.value} is not a valid choice`,
-		},
 	})
 	choice: Choice;
 }
