79471c1749ed16153db242e72d6c9b9e553ce486
Mikhail Deriabin <47395053+MikhailDeriabin@users.noreply.github.com>
2024-11-26 15:14:02 +0200

    add tests data builders for item module

---
+++ src/test/clanInventory/data/item/createItemDtoBuilder.ts
@@ -0,0 +1,73 @@
+import {CreateItemDto} from "../../../../clanInventory/item/dto/createItem.dto";
+import {ItemName} from "../../../../clanInventory/item/enum/itemName.enum";
+import {Recycling} from "../../../../clanInventory/item/enum/recycling.enum";
+import {QualityLevel} from "../../../../clanInventory/item/enum/qualityLevel.enum";
+
+export default class CreateItemDtoBuilder {
+    private readonly base: Partial<CreateItemDto> = {
+        name: ItemName.ARMCHAIR_RAKKAUS,
+        weight: 1,
+        recycling: Recycling.GLASS,
+        qualityLevel: QualityLevel.common,
+        unityKey: 'defaultUnityKey',
+        location: [0, 0],
+        price: 10,
+        isFurniture: false,
+        stock_id: undefined,
+        room_id: undefined
+    };
+
+    build() {
+        return { ...this.base } as CreateItemDto;
+    }
+
+    setName(name: ItemName) {
+        this.base.name = name;
+        return this;
+    }
+
+    setWeight(weight: number) {
+        this.base.weight = weight;
+        return this;
+    }
+
+    setRecycling(recycling: Recycling) {
+        this.base.recycling = recycling;
+        return this;
+    }
+
+    setQualityLevel(qualityLevel: QualityLevel) {
+        this.base.qualityLevel = qualityLevel;
+        return this;
+    }
+
+    setUnityKey(unityKey: string) {
+        this.base.unityKey = unityKey;
+        return this;
+    }
+
+    setLocation(location: Array<number>) {
+        this.base.location = location;
+        return this;
+    }
+
+    setPrice(price: number) {
+        this.base.price = price;
+        return this;
+    }
+
+    setIsFurniture(isFurniture: boolean) {
+        this.base.isFurniture = isFurniture;
+        return this;
+    }
+
+    setStockId(stockId: string) {
+        this.base.stock_id = stockId;
+        return this;
+    }
+
+    setRoomId(roomId: string) {
+        this.base.room_id = roomId;
+        return this;
+    }
+}
---
+++ src/test/clanInventory/data/item/stealItemsDtoBuilder.ts
@@ -0,0 +1,28 @@
+import {StealItemsDto} from "../../../../clanInventory/item/dto/stealItems.dto";
+
+export default class StealItemsDtoBuilder {
+    private readonly base: Partial<StealItemsDto> = {
+        steal_token: undefined,
+        item_ids: [],
+        room_id: undefined,
+    };
+
+    build(): StealItemsDto {
+        return { ...this.base } as StealItemsDto;
+    }
+
+    setStealToken(stealToken: string) {
+        this.base.steal_token = stealToken;
+        return this;
+    }
+
+    setItemIds(itemIds: string[]) {
+        this.base.item_ids = itemIds;
+        return this;
+    }
+
+    setRoomId(roomId: string) {
+        this.base.room_id = roomId;
+        return this;
+    }
+}
