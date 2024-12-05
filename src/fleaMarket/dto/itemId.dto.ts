adef7afd95c88136264089bdd787726365b4d1c2
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-12-05 17:50:53 +0200

    rename SellItemDto to ItemIdDto for clarity and consistency

--- src/fleaMarket/dto/itemId.dto.ts
+++ src/fleaMarket/dto/itemId.dto.ts
@@ -1,8 +1,8 @@
 import { IsMongoId, IsString } from "class-validator";
 import AddType from "../../common/base/decorator/AddType.decorator";
 
-@AddType('SellItemDto')
-export class SellItemDto {
+@AddType('ItemIdDto')
+export class ItemIdDto {
 	@IsString()
 	@IsMongoId()
 	item_id: string;
