e53efb47e03e36d2a83c302e25233d7eeb13c5b9
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-12-05 17:50:01 +0200

    added voting queue and helper services

--- src/fleaMarket/fleaMarket.module.ts
+++ src/fleaMarket/fleaMarket.module.ts
@@ -8,19 +8,32 @@ import { RequestHelperModule } from "../requestHelper/requestHelper.module";
 import { PlayerModule } from "../player/player.module";
 import { ClanInventoryModule } from "../clanInventory/clanInventory.module";
 import { VotingModule } from "../voting/voting.module";
+import { ClanModule } from "../clan/clan.module";
+import { TestProcessor, VotingQueue } from "./voting.queue";
+import { BullModule } from "@nestjs/bullmq";
+import { FleaMarketHelperService } from "./fleaMarketHelper.service";
 
 @Module({
 	imports: [
 		MongooseModule.forFeature([
 			{ name: ModelName.FLEA_MARKET_ITEM, schema: FleaMarketItemSchema },
 		]),
+		BullModule.registerQueue({
+			name: "voting",
+		}),
 		ClanInventoryModule,
 		PlayerModule,
 		VotingModule,
+		ClanModule,
 		RequestHelperModule,
 	],
 	controllers: [FleaMarketController],
-	providers: [FleaMarketService],
+	providers: [
+		FleaMarketService,
+		FleaMarketHelperService,
+		VotingQueue,
+		TestProcessor,
+	],
 	exports: [],
 })
 export class FleaMarketModule {}
