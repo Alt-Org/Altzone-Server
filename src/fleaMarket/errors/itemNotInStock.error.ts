c40b96d18f37f60c20721b6f700aba0efb33852e
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-12-05 17:56:24 +0200

    add error handling for unavailable items and insufficient coins in FleaMarket

---
+++ src/fleaMarket/errors/itemNotInStock.error.ts
@@ -0,0 +1,7 @@
+import { SEReason } from "../../common/service/basicService/SEReason";
+import ServiceError from "../../common/service/basicService/ServiceError";
+
+export const itemNotInStockError = new ServiceError({
+	reason: SEReason.NOT_ALLOWED,
+	message: "Item is not in stock",
+});
