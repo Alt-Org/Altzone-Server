04e477e0d0691bb454390a0062fa02637bac03d3
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-12-13 23:22:09 +0200

    Add: Implement custom error handling for voting errors

---
+++ src/voting/error/addVote.error.ts
@@ -0,0 +1,7 @@
+import { SEReason } from "../../common/service/basicService/SEReason";
+import ServiceError from "../../common/service/basicService/ServiceError";
+
+export const addVoteError = new ServiceError({
+	reason: SEReason.UNEXPECTED,
+	message: "Error adding the vote",
+});
