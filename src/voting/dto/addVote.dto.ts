f5107ed370d8072c554b579ce1c393508accd3ba
Jeri Nurmi <nurmi.jeri@gmail.com>
2024-12-13 23:20:42 +0200

    Add: AddVoteDto for vote validation with MongoDB ID and enum choice

---
+++ src/voting/dto/addVote.dto.ts
@@ -0,0 +1,12 @@
+import { IsEnum, IsMongoId } from "class-validator";
+import AddType from "../../common/base/decorator/AddType.decorator";
+import { ItemVoteChoice } from "../enum/choiceType.enum";
+
+@AddType("AddVoteDto")
+export class AddVoteDto {
+	@IsMongoId()
+	voting_id: string;
+
+	@IsEnum(ItemVoteChoice)
+	choice: ItemVoteChoice;
+}
