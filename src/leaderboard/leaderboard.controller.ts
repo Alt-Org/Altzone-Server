c73b51ee16502da58b84bf3461e47cabde6b4fee
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-01-04 14:40:14 +0200

    add: clan position endpoint to leaderboard controller
    change: changed the NoAuth decorator from controller level to route specific

--- src/leaderboard/leaderboard.controller.ts
+++ src/leaderboard/leaderboard.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 import { LeaderboardService } from "./leaderboard.service";
 import { UniformResponse } from "../common/decorator/response/UniformResponse";
 import { ModelName } from "../common/enum/modelName.enum";
@@ -8,26 +8,40 @@ import { OffsetPaginate } from "../common/interceptor/request/offsetPagination.i
 import { Serialize } from "../common/interceptor/response/Serialize";
 import { PlayerDto } from "../player/dto/player.dto";
 import { ClanDto } from "../clan/dto/clan.dto";
-import {NoAuth} from "../auth/decorator/NoAuth.decorator";
+import { NoAuth } from "../auth/decorator/NoAuth.decorator";
+import { LoggedUser } from "../common/decorator/param/LoggedUser.decorator";
+import { User } from "../auth/user";
+import { PlayerService } from "../player/player.service";
 
-@NoAuth()
 @Controller("leaderboard")
 export class LeaderboardController {
-	constructor(private readonly leaderBoardService: LeaderboardService) {}
+	constructor(
+		private readonly leaderBoardService: LeaderboardService,
+		private readonly playerService: PlayerService
+	) {}
 
 	@Get("player")
-    @OffsetPaginate(ModelName.PLAYER)
-    @Serialize(PlayerDto)
-    @UniformResponse(ModelName.PLAYER)
+	@NoAuth()
+	@OffsetPaginate(ModelName.PLAYER)
+	@Serialize(PlayerDto)
+	@UniformResponse(ModelName.PLAYER)
 	async getPlayerLeaderboard(@GetAllQuery() query: IGetAllQuery) {
 		return await this.leaderBoardService.getPlayerLeaderboard(query);
 	}
 
 	@Get("clan")
+	@NoAuth()
 	@OffsetPaginate(ModelName.CLAN)
 	@Serialize(ClanDto)
 	@UniformResponse(ModelName.CLAN)
 	async getClanLeaderboard(@GetAllQuery() query: IGetAllQuery) {
 		return await this.leaderBoardService.getClanLeaderboard(query);
+	}
+
+	@Get("clan/position")
+	@UniformResponse()
+	async getClanPosition(@LoggedUser() user: User) {
+		const clanId = await this.playerService.getPlayerClanId(user.player_id);
+		return await this.leaderBoardService.getClanPosition(clanId);
 	}
 }
