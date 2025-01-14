df333e7b7a61acccba9f95f3db153d7d9c982f37
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-01-14 22:09:25 +0200

    update test for getRoomDefaultItems to include new items

--- src/test/clan/utils/defaultValues/getRoomDefaultItems.test.ts
+++ src/test/clan/utils/defaultValues/getRoomDefaultItems.test.ts
@@ -6,12 +6,41 @@ describe('getRoomDefaultItems() test suite', () => {
     it('Should return valid room default items', () => {
         const room_id = '';
         const expected = [
-            {...itemProperties.Sofa_Rakkaus, stock_id: null, room_id, unityKey: ItemName.SOFA_RAKKAUS, location : [1,1] },
-            {...itemProperties.Carpet_Rakkaus, stock_id: null, room_id, unityKey: ItemName.CARPET_RAKKAUS, location : [1,2] },
-            {...itemProperties.Chair_Neuro, stock_id: null, room_id, unityKey: ItemName.CHAIR_NEURO, location : [1,3] },
-            {...itemProperties.SideTable_Taakka, stock_id: null, room_id, unityKey: ItemName.SIDETABLE_TAAKKA, location : [1,4] },
-            {...itemProperties.Floorlamp_Taakka, stock_id: null, room_id, unityKey: ItemName.FLOORLAMP_TAAKKA, location : [1,5] },
-            {...itemProperties.Sofa_Taakka, stock_id: null, room_id, unityKey: ItemName.SOFA_TAAKKA, location : [1,6] }
+            {...itemProperties.Sofa_Rakkaus, stock_id: null, room_id, unityKey: ItemName.SOFA_RAKKAUS, location: [1, 1]},
+            {
+                ...itemProperties.ArmChair_Rakkaus,
+                stock_id: null,
+                room_id,
+                unityKey: ItemName.ARMCHAIR_RAKKAUS,
+                location: [1, 2]
+            },
+            {
+                ...itemProperties.Floorlamp_Rakkaus,
+                stock_id: null,
+                room_id,
+                unityKey: ItemName.FLOORLAMP_RAKKAUS,
+                location: [1, 3]
+            },
+            {
+                ...itemProperties.Diningtable_Rakkaus,
+                stock_id: null,
+                room_id,
+                unityKey: ItemName.DININGTABLE_RAKKAUS,
+                location: [1, 4]
+            },
+            {
+                ...itemProperties.SofaTable_Rakkaus,
+                stock_id: null,
+                room_id,
+                unityKey: ItemName.SOFATABLE_RAKKAUS,
+                location: [1, 5]
+            },
+            {
+                ...itemProperties.Bed_Rakkaus,
+                stock_id: null, room_id,
+                unityKey: ItemName.BED_RAKKAUS,
+                location: [1, 6]
+            }
         ];
 
         expect(getRoomDefaultItems(room_id)).toEqual(expect.arrayContaining(expected));
