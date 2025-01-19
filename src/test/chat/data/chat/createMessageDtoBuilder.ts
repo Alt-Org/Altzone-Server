abb68564faa6279980a3bca6d664fdd530ac4ef0
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-01-20 00:56:46 +0200

    add CreateMessageDtoBuilder for building message DTO instances in tests

---
+++ src/test/chat/data/chat/createMessageDtoBuilder.ts
@@ -0,0 +1,35 @@
+import { CreateMessageDto } from "../../../../chat/dto/createMessage.dto";
+import IDataBuilder from "../../../test_utils/interface/IDataBuilder";
+
+export default class CreateMessageDtoBuilder implements IDataBuilder<CreateMessageDto> {
+	private readonly base: CreateMessageDto = {
+		id: undefined,
+		senderUsername: "testSender",
+		content: "test content",
+		feeling: 420,
+	}
+
+	setId(id: number): CreateMessageDtoBuilder {
+		this.base.id = id;
+		return this;
+	}
+
+	setSenderUsername(senderUsername: string): CreateMessageDtoBuilder {
+		this.base.senderUsername = senderUsername;
+		return this;
+	}
+
+	setContent(content: string): CreateMessageDtoBuilder {
+		this.base.content = content;
+		return this;
+	}
+
+	setFeeling(feeling: number): CreateMessageDtoBuilder {
+		this.base.feeling = feeling;
+		return this;
+	}
+
+	build(): CreateMessageDto {
+		return {...this.base};
+	}
+}
