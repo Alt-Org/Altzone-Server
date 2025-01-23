e3e6b19ebff96b532986a1291519df5825fd3e5d
mikhail <mikko.deriabin.work@gmail.com>
2025-01-23 13:50:09 +0200

    fix typo in clanInventory testing modules

--- src/test/clanInventory/modules/room.module.ts
+++ src/test/clanInventory/modules/room.module.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
-import ClanCommonModule from "./clanInventoryCommon";
 import {ModelName} from "../../../common/enum/modelName.enum";
 import {RoomService} from "../../../clanInventory/room/room.service";
 import RoomHelperService from "../../../clanInventory/room/utils/room.helper.service";
 import {RoomSchema} from "../../../clanInventory/room/room.schema";
+import ClanInventoryCommonModule from "./clanInventoryCommon";
 
 export default class RoomModule {
     private constructor() {
     }
 
     static async getRoomService() {
-        const module = await ClanCommonModule.getModule();
+        const module = await ClanInventoryCommonModule.getModule();
         return await module.resolve(RoomService);
     }
 
     static async getRoomHelperService() {
-        const module = await ClanCommonModule.getModule();
+        const module = await ClanInventoryCommonModule.getModule();
         return await module.resolve(RoomHelperService);
     }
 
--- src/test/clanInventory/modules/stock.module.ts
+++ src/test/clanInventory/modules/stock.module.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
-import ClanCommonModule from "./clanInventoryCommon";
 import {ModelName} from "../../../common/enum/modelName.enum";
 import {StockService} from "../../../clanInventory/stock/stock.service";
 import {isStockExists} from "../../../clanInventory/stock/decorator/validation/IsStockExists.decorator";
 import {StockSchema} from "../../../clanInventory/stock/stock.schema";
+import ClanInventoryCommonModule from "./clanInventoryCommon";
 
 export default class StockModule {
     private constructor() {
     }
 
     static async getStockService() {
-        const module = await ClanCommonModule.getModule();
+        const module = await ClanInventoryCommonModule.getModule();
         return await module.resolve(StockService);
     }
 
     static async getIsStockExists() {
-        const module = await ClanCommonModule.getModule();
+        const module = await ClanInventoryCommonModule.getModule();
         return await module.resolve(isStockExists);
     }
 
