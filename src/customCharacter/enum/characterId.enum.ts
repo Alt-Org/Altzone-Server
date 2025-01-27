cdd6336b48120f38c2ef45f8b0ee9da413a948b1
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-01-27 16:46:36 +0200

    add base stats for the CustomerCharacter

---
+++ src/customCharacter/enum/characterId.enum.ts
@@ -0,0 +1,20 @@
+/**
+ * Defines the type of the CustomCharacter and therefore its base stats
+ */
+export enum CharacterId {
+    Racist_101 = '101',
+
+    Joker_201 = '201',
+    Prankster_202 = '202',
+
+    CaseHardened_301 = '301',
+    Provocateur_401 = '401',
+
+    Glutton_501 = '501',
+    Alcoholic_502 = '502',
+
+    SoulSisters_601 = '601',
+    WakefulnessEvader_603 = '603',
+
+    Wiseacre_701 = '701'
+}
