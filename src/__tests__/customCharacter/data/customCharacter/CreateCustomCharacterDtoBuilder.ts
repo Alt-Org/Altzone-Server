399279c6a181f81fd9b1fb91f87d4db7618f2465
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-01-27 16:48:34 +0200

    change CustomCharacter testing data builders

---
+++ src/__tests__/customCharacter/data/customCharacter/CreateCustomCharacterDtoBuilder.ts
@@ -0,0 +1,23 @@
+import {CreateCustomCharacterDto} from "../../../../customCharacter/dto/createCustomCharacter.dto";
+import {CharacterId} from "../../../../customCharacter/enum/characterId.enum";
+
+export default class CreateCustomCharacterDtoBuilder {
+    private readonly base: CreateCustomCharacterDto = {
+        characterId: CharacterId.Prankster_202,
+        level: 1
+    };
+
+    build(): CreateCustomCharacterDto {
+        return { ...this.base } as CreateCustomCharacterDto;
+    }
+
+    setCharacterId(characterId: CharacterId) {
+        this.base.characterId = characterId;
+        return this;
+    }
+
+    setLevel(level: number) {
+        this.base.level = level;
+        return this;
+    }
+}
