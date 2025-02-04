acf6c4dc5e779650877cca97ec4f9bc539407b32
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-02-04 02:22:36 +0200

    replace PlayerTasksModule with DailyTasksModule and add DailyTasksModule implementation

---
+++ src/dailyTasks/dailyTasks.module.ts
@@ -0,0 +1,27 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { RewarderModule } from "../rewarder/rewarder.module";
+import DailyTaskNotifier from "./dailyTask.notifier";
+import { DailyTasksService } from "./dailyTasks.service";
+import { DailyTasksController } from "./dailyTasks.controller";
+import { DailyTask, DailyTaskSchema } from "./dailyTasks.schema";
+import { BullModule } from "@nestjs/bullmq";
+import { DailyTaskProcessor, DailyTaskQueue } from "./dailyTask.queue";
+import { PlayerModule } from "../player/player.module";
+
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{ name: DailyTask.name, schema: DailyTaskSchema },
+		]),
+		BullModule.registerQueue({
+			name: "daily-tasks",
+		}),
+		RewarderModule,
+		PlayerModule,
+	],
+	providers: [DailyTasksService, DailyTaskNotifier, DailyTaskQueue, DailyTaskProcessor],
+	controllers: [DailyTasksController],
+	exports: [DailyTasksService],
+})
+export class DailyTasksModule {}
