143a52006379edb6da92ca9049328d9dc2b474c0
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-02-04 02:28:48 +0200

    add TASK_CONSTS for defining task-related constants and limits

---
+++ src/dailyTasks/consts/taskConstants.ts
@@ -0,0 +1,14 @@
+export const TASK_CONSTS = {
+	AMOUNT: {
+		MIN: 2,
+		MAX: 20,
+	},
+	POINTS: {
+		MIN: 5,
+		MAX: 100,
+	},
+	COINS: {
+		FACTOR: 0.5, // Coins are calculated as points * FACTOR
+	},
+	TIME: 1000 * 60 * 20 // Time to complete the task in milliseconds. 20 minutes
+};
