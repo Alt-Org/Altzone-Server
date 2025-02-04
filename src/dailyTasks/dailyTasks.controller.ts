e2efd2d404ad28c231162235b4f58c2f03635d4e
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-02-04 02:24:01 +0200

    add DailyTasksController with CRUD operations for daily tasks

---
+++ src/dailyTasks/dailyTasks.controller.ts
@@ -0,0 +1,54 @@
+import { Controller, Delete, Get, Param, Patch } from "@nestjs/common";
+import { DailyTasksService } from "./dailyTasks.service";
+import { LoggedUser } from "../common/decorator/param/LoggedUser.decorator";
+import { User } from "../auth/user";
+import { UniformResponse } from "../common/decorator/response/UniformResponse";
+import { ModelName } from "../common/enum/modelName.enum";
+import { PlayerService } from "../player/player.service";
+import { DailyTaskDto } from "./dto/dailyTask.dto";
+import { Serialize } from "../common/interceptor/response/Serialize";
+import { _idDto } from "../common/dto/_id.dto";
+
+@Controller("dailyTasks")
+export class DailyTasksController {
+	constructor(
+		private readonly dailyTasksService: DailyTasksService,
+		private readonly playerService: PlayerService
+	) {}
+
+	@Get()
+	@Serialize(DailyTaskDto)
+	@UniformResponse(ModelName.DAILY_TASK)
+	async getClanTasks(@LoggedUser() user: User) {
+		const clanId = await this.playerService.getPlayerClanId(user.player_id);
+		return await this.dailyTasksService.basicService.readMany<DailyTaskDto>({
+			filter: { clanId },
+		});
+	}
+
+	@Get(":_id")
+	@Serialize(DailyTaskDto)
+	@UniformResponse(ModelName.DAILY_TASK)
+	async getTask(@Param() param: _idDto) {
+		return await this.dailyTasksService.basicService.readOneById(param._id);
+	}
+
+	@Patch("/:_id")
+	@UniformResponse()
+	async reserveTask(@Param() param: _idDto, @LoggedUser() user: User) {
+		const clanId = await this.playerService.getPlayerClanId(user.player_id);
+		return await this.dailyTasksService.reserveTask(
+			user.player_id,
+			param._id,
+			clanId
+		);
+	}
+
+	@Delete("/:_id")
+	@Serialize(DailyTaskDto)
+	@UniformResponse(ModelName.DAILY_TASK)
+	async deleteTask(@Param() param: _idDto, @LoggedUser() user: User) {
+		const clanId = await this.playerService.getPlayerClanId(user.player_id);
+		return await this.dailyTasksService.deleteTask(param._id, clanId, user.player_id);
+	}
+}
