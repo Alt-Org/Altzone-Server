ced11393568baee2794663ac47d39be3aabc2acb
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-02-04 02:28:19 +0200

    add DailyTaskQueue and DailyTaskProcessor for managing daily task processing and queuing

---
+++ src/dailyTasks/dailyTask.queue.ts
@@ -0,0 +1,34 @@
+import { InjectQueue, Processor, WorkerHost } from "@nestjs/bullmq";
+import { forwardRef, Inject, Injectable } from "@nestjs/common";
+import { TASK_CONSTS } from "./consts/taskConstants";
+import { DailyTasksService } from "./dailyTasks.service";
+import { Job } from "bullmq";
+import { DailyTaskDto } from "./dto/dailyTask.dto";
+import { Queue } from "bull";
+
+@Injectable()
+export class DailyTaskQueue {
+	constructor(
+		@InjectQueue("daily-tasks") private readonly dailyTaskQueue: Queue
+	) {}
+
+	async addDailyTask(task: DailyTaskDto) {
+		if (!task.playerId || !task.startedAt) return;
+		const delay = TASK_CONSTS.TIME;
+		await this.dailyTaskQueue.add("tasks", task, { delay });
+	}
+}
+
+@Processor("daily-tasks")
+export class DailyTaskProcessor extends WorkerHost {
+	constructor(
+		@Inject(forwardRef(() => DailyTasksService))
+		private readonly dailyTasksService: DailyTasksService
+	) {
+		super();
+	}
+
+	async process(job: Job): Promise<any> {
+		await this.dailyTasksService.handleExpiredTask(job.data);
+	}
+}
