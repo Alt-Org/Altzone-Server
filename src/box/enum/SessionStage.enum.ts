d1e71bf3dab8962e1042d3c79b9f65f0e66cf913
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-02-11 19:33:27 +0200

    add enums for box module

---
+++ src/box/enum/SessionStage.enum.ts
@@ -0,0 +1,17 @@
+/**
+ * Stage of testing session
+ */
+export enum SessionStage {
+    /**
+     * Stage 2. Preparing for testing
+     */
+    PREPARING = 'Preparing',
+    /**
+     * Stage 3: Testing session is started
+     */
+    TESTING = 'Testing',
+    /**
+     * Stage 4: Testing session is ended
+     */
+    END = 'End'
+}
