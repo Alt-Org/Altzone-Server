7e539e063b897f53ccef8859593e6e97e9aefbc2
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-02-12 16:55:16 +0200

    add isTestingSession() util function

---
+++ src/__tests__/box/util/isTestingSession.test.ts
@@ -0,0 +1,14 @@
+import {envVars} from "../../../common/service/envHandler/envVars";
+import isTestingSession from "../../../box/util/isTestingSession";
+
+describe('isTestingSession() test suite', () => {
+    it('Should return true when environment variable is set to TESTING_SESSION', () => {
+        envVars.ENVIRONMENT = 'TESTING_SESSION';
+        expect(isTestingSession()).toBeTruthy();
+    });
+
+    it('Should return false when environment variable is not set to TESTING_SESSION', () => {
+        envVars.ENVIRONMENT = 'OTHER_ENVIRONMENT';
+        expect(isTestingSession()).toBeFalsy();
+    });
+});
