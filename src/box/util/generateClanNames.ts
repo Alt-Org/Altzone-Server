8d6ade96ed31080f8cd4845321099281e5d512d6
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-02-13 13:16:43 +0200

    add descriptions of required methods for initializing a box

---
+++ src/box/util/generateClanNames.ts
@@ -0,0 +1,11 @@
+/**
+ * Generates clan names with specified unique value. For example:
+ * uniqueValue = "john" => "john clan 1"
+ * @param uniqueValue unique value to be used
+ * @param nameCount amount of names to be generated
+ *
+ * @returns array of clan names
+ */
+export default function generateClanNames(uniqueValue: string, nameCount: number): string[] {
+    return null;
+}
