0ff9af497ec4ef9f045b97d1b5aa9e68f63f6345
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-02-14 03:18:45 +0200

    add device identifier and account claim endpoints to BoxController

--- src/box/box.controller.ts
+++ src/box/box.controller.ts
@@ -1,12 +1,47 @@
-import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
-import {BoxService} from "./box.service";
+import {
+	Body,
+	Controller,
+	Get,
+	Headers,
+	Ip,
+	Param,
+	Post,
+	Put,
+	Query,
+	Req,
+	Res,
+} from "@nestjs/common";
+import { BoxService } from "./box.service";
+import { Request, Response } from "express";
+import { NoAuth } from "../auth/decorator/NoAuth.decorator";
+import { UniformResponse } from "../common/decorator/response/UniformResponse";
 
-@Controller('box')
+@Controller("box")
 export class BoxController {
-    public constructor(
-        private readonly service: BoxService,
-    ) {
-    }
+	public constructor(private readonly service: BoxService) {}
 
+	@NoAuth()
+	@Get("identifier")
+	@UniformResponse()
+	async getDeviceIdentifier(
+		@Query("password") password: string,
+		@Headers("user-agent") userAgent: string,
+		@Ip() ip: string,
+		@Res({ passthrough: true }) res: Response
+	) {
+		const deviceIdentifier = await this.service.setDeviceIdentifier(
+			userAgent,
+			ip,
+			password
+		);
+		res.cookie("deviceIdentifier", deviceIdentifier);
+	}
 
+	@NoAuth()
+	@Get("claim-account")
+	@UniformResponse()
+	async claimAccount(@Req() req: Request, @Query("password") password: string) {
+		const { deviceIdentifier } = req.cookies;
+		return this.service.claimAccount(password, deviceIdentifier);
+	}
 }
