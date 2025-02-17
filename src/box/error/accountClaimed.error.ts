b6a0260621f8571dc7114b1fe4661443b9ccd23e
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-02-17 14:10:19 +0200

    remove accountClaimedError and BoxWithPopulatedRefs interface definitions

--- src/box/error/accountClaimed.error.ts
+++
@@ -1,7 +0,0 @@
-import { SEReason } from "../../common/service/basicService/SEReason";
-import ServiceError from "../../common/service/basicService/ServiceError";
-
-export const accountClaimedError = new ServiceError({
-	reason: SEReason.NOT_UNIQUE,
-	message: "Account already claimed",
-});
--- src/box/types/PopulatedRefs.type.ts
+++
@@ -1,8 +0,0 @@
-import { PlayerDto } from "../../player/dto/player.dto";
-import { Profile } from "../../profile/profile.schema";
-import { BoxDto } from "../dto/box.dto";
-
-export interface BoxWithPopulatedRefs extends BoxDto {
-	TesterPlayers: PlayerDto[];
-	TesterProfiles: Profile[];
-}
