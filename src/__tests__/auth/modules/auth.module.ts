09b5de0bd788b155d64e256a1923b356abaf4ea2
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-02-20 19:53:35 +0200

    add tests for BoxAuthService.signIn()

--- src/__tests__/auth/modules/auth.module.ts
+++ src/__tests__/auth/modules/auth.module.ts
@@ -1,17 +1,24 @@
 import {AuthService} from "../../../auth/auth.service";
 import AuthCommonModule from "./authCommonModule";
 import {AuthGuard} from "../../../auth/auth.guard";
+import BoxAuthService from "../../../auth/box/BoxAuthService";
 
 export default class AuthModule {
-    private constructor() {}
-
-    static async getAuthService(): Promise<AuthService> {
-        const module = await AuthCommonModule.getModule();
-        return await module.resolve(AuthService);
+    private constructor() {
     }
 
-    static async getAuthGuard(): Promise<AuthGuard> {
+    static async getAuthService() {
         const module = await AuthCommonModule.getModule();
-        return await module.resolve(AuthGuard);
+        return module.resolve(AuthService);
+    }
+
+    static async getAuthGuard() {
+        const module = await AuthCommonModule.getModule();
+        return module.resolve(AuthGuard);
+    }
+
+    static async getBoxAuthService() {
+        const module = await AuthCommonModule.getModule();
+        return module.resolve(BoxAuthService);
     }
 }
