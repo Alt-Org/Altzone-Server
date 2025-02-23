617883e6f77866c35a9ec740a619ab919ccd76b3
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-02-23 14:19:02 +0200

    add use of IsGroupAdmin decorator to box daily task controller

--- src/box/dailyTask/dailyTask.controller.ts
+++ src/box/dailyTask/dailyTask.controller.ts
@@ -10,12 +10,21 @@ import {UpdatePredefinedDailyTaskDto} from "./dto/updatePredefinedDailyTask.dto"
 import {_idDto} from "../../common/dto/_id.dto";
 import {ObjectId} from "mongodb";
 import {PredefinedDailyTaskDto} from "./dto/predefinedDailyTask.dto";
+import {APIError} from "../../common/controller/APIError";
+import {APIErrorReason} from "../../common/controller/APIErrorReason";
+import BoxAuthHandler from "../auth/BoxAuthHandler";
+import {IsGroupAdmin} from "../auth/decorator/IsGroupAdmin";
 
 @Controller('/box/dailyTask')
 export class DailyTaskController {
-    constructor(private readonly taskService: DailyTaskService) {}
+    constructor(
+        private readonly taskService: DailyTaskService,
+        private readonly boxAuthHandler: BoxAuthHandler
+    ) {
+    }
 
     @Post()
+    @IsGroupAdmin()
     @Serialize(PredefinedDailyTaskDto)
     @UniformResponse(ModelName.DAILY_TASK)
     async addOneDailyTask(@Body() body: CreatePredefinedDailyTaskDto, @LoggedUser() user: BoxUser) {
@@ -23,25 +32,38 @@ export class DailyTaskController {
     }
 
     @Post('/multiple')
+    @IsGroupAdmin()
     @Serialize(PredefinedDailyTaskDto)
     @UniformResponse(ModelName.DAILY_TASK)
     async addMultipleDailyTasks(@Body() body: CreatePredefinedDailyTaskDto[], @LoggedUser() user: BoxUser) {
+        const isAdmin = await this.boxAuthHandler.isGroupAdmin(user);
+        if (!isAdmin)
+            return [null, [new APIError({
+                reason: APIErrorReason.NOT_AUTHORIZED, field: 'groupAdmin', value: user.groupAdmin,
+                message: 'The logged-in user is not a group admin'
+            })]];
+
         return this.taskService.addMultiple(user.box_id, body);
     }
 
     @Put()
+    @IsGroupAdmin()
     @UniformResponse(ModelName.DAILY_TASK)
     async updateDailyTask(@Body() body: UpdatePredefinedDailyTaskDto, @LoggedUser() user: BoxUser) {
-        const [isUpdated, errors] = await this.taskService.updateOneById(user.box_id, {...body, _id: new ObjectId(body._id)});
-        if(errors)
+        const [isUpdated, errors] = await this.taskService.updateOneById(user.box_id, {
+            ...body,
+            _id: new ObjectId(body._id)
+        });
+        if (errors)
             return [null, errors];
     }
 
     @Delete('/:_id')
+    @IsGroupAdmin()
     @UniformResponse(ModelName.DAILY_TASK)
     async deleteDailyTask(@Param() param: _idDto, @LoggedUser() user: BoxUser) {
         const [isDeleted, errors] = await this.taskService.deleteOneById(user.box_id, param._id);
-        if(errors)
+        if (errors)
             return [null, errors];
     }
 }
