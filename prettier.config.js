132db59395d63907ea16b4a8ab4e9394c40c5410
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-03-08 10:08:54 +0200

    Add Prettier and ESLint configuration files, and update package.json for linting support

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+};
+
+module.exports = config;
