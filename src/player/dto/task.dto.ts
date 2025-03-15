845b3f0d08fbb8407763ef06784ca96891297082
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-03-15 09:38:26 +0200

    Add DailyTask to PlayerDto and the TaskDto

---
+++ src/player/dto/task.dto.ts
@@ -0,0 +1,11 @@
+import { Expose } from "class-transformer";
+import { ExtractField } from "../../common/decorator/response/ExtractField";
+
+export class TaskDto {
+	@ExtractField()
+	@Expose()
+	_id: string;
+
+	@Expose()
+	amountLeft: number;
+}
