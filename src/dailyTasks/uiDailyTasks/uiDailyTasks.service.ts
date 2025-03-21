3a49e1b99289fd4b53ddbebefbb0a1050499a8d7
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-03-21 20:08:37 +0200

    add method for updating UI daily task

--- src/dailyTasks/uiDailyTasks/uiDailyTasks.service.ts
+++ src/dailyTasks/uiDailyTasks/uiDailyTasks.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from "@nestjs/common";
-import {DailyTasksService} from "../dailyTasks.service";
 import {InjectModel} from "@nestjs/mongoose";
 import {DailyTask} from "../dailyTasks.schema";
 import {Model} from "mongoose";
@@ -8,12 +7,13 @@ import {UIDailyTaskData, uiDailyTasks} from "./uiDailyTasks";
 import {IServiceReturn} from "../../common/service/basicService/IService";
 import ServiceError from "../../common/service/basicService/ServiceError";
 import {SEReason} from "../../common/service/basicService/SEReason";
+import {DailyTaskDto} from "../dto/dailyTask.dto";
+import {ObjectId} from "mongodb";
 
 @Injectable()
 export default class UIDailyTasksService {
     constructor(
-        private readonly dailyTasksService: DailyTasksService,
-        @InjectModel(DailyTask.name) public readonly model: Model<DailyTask>,
+        @InjectModel(DailyTask.name) public readonly model: Model<DailyTask>
     ) {
         this.basicService = new BasicService(model);
     }
@@ -29,18 +29,51 @@ export default class UIDailyTasksService {
      * @returns an array containing daily tasks for UI side managed tasks or REQUIRED if clan_id is not provided
      */
     public getUITasksForClan(clan_id: string): IServiceReturn<Omit<DailyTask, '_id'>[]> {
-        if(!clan_id)
+        if (!clan_id)
             return [null, [new ServiceError({
                 reason: SEReason.REQUIRED, field: 'clan_id', value: clan_id,
                 message: 'clan_id is required'
             })]];
 
         const tasks: Omit<DailyTask, '_id'>[] = [];
-        for(const taskName in uiDailyTasks){
+        for (const taskName in uiDailyTasks) {
             const base: UIDailyTaskData = uiDailyTasks[taskName];
-            const task: Omit<DailyTask, '_id'> = {...base, clan_id, player_id: null, startedAt: null, amountLeft: base.amount};
+            const task: Omit<DailyTask, '_id'> = {
+                ...base,
+                clan_id,
+                player_id: null,
+                startedAt: null,
+                amountLeft: base.amount
+            };
             tasks.push(task);
         }
         return [tasks, null];
+    }
+
+    /**
+     * Updates the daily task for a given player. Decrements the amount left for the task.
+     *
+     * @param task_id - The ID of the task to update.
+     * @param player_id - The ID of the player whose task is being updated.
+     * @param amount - Amount of completed atomic tasks, default is 1
+     * @returns The updated task and status or ServiceErrors if any occurred.
+     */
+    async updateTask(task_id: string | ObjectId, player_id: string | ObjectId, amount = 1): Promise<IServiceReturn<['updated' | 'completed', DailyTask]>> {
+        const task_idStr = task_id.toString();
+
+        const [task, error] = await this.basicService.readOneById<DailyTaskDto>(task_idStr);
+        if (error)
+            return [null, error];
+
+        task.amountLeft -= amount;
+
+        if (task.amountLeft <= 0)
+            return [['completed', task], null];
+
+        const [_, updateError] = await this.basicService.updateOneById(task_idStr, task);
+        if (updateError)
+            return [null, updateError];
+
+        return [['updated', task], null];
     }
 }
