079bd7626fe38e2350548d311710c0397a0b7882
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-03-23 17:16:35 +0200

    update ESLint and Prettier configuration; add custom rules and update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['src/**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
+  {
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }], // Don't allow console.log but allow console.warn and console.error
+      '@typescript-eslint/no-explicit-any': 'off', // Allow the use of 'any' types
+    },
+  },
 ];
