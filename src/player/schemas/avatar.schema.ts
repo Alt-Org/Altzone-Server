5bdf43acd3c35e99e1f7367adb0575fd38d4e3bb
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-03-29 15:53:32 +0200

    fix formatting in AvatarSchema

--- src/player/schemas/avatar.schema.ts
+++ src/player/schemas/avatar.schema.ts
@@ -1,38 +1,39 @@
-import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
-import {ObjectId} from "mongodb";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-@Schema({ toJSON: { virtuals: true }, toObject: { virtuals: true } })
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  _id: false,
+})
 export class Avatar {
-    @Prop({ type: Number, required: true })
-    head: number;
-    
-    @Prop({ type: Number, required: true })
-    hair: number;
-    
-    @Prop({ type: Number, required: true })
-    eyes: number;
-        
-    @Prop({ type: Number, required: true })
-    nose: number;
-    
-    @Prop({ type: Number, required: true })
-    mouth: number;
-    
-    @Prop({ type: Number, required: true })
-    eyebrows: number;
-    
-    @Prop({ type: Number, required: true })
-    clothes: number;
-    
-    @Prop({ type: Number, required: true })
-    feet: number;
-    
-    @Prop({ type: Number, required: true })
-    hands: number;
+  @Prop({ type: Number, required: true })
+  head: number;
 
-    @Prop({ type: String, required: true })
-    skinColor: string;
+  @Prop({ type: Number, required: true })
+  hair: number;
 
-    _id: ObjectId;
+  @Prop({ type: Number, required: true })
+  eyes: number;
+
+  @Prop({ type: Number, required: true })
+  nose: number;
+
+  @Prop({ type: Number, required: true })
+  mouth: number;
+
+  @Prop({ type: Number, required: true })
+  eyebrows: number;
+
+  @Prop({ type: Number, required: true })
+  clothes: number;
+
+  @Prop({ type: Number, required: true })
+  feet: number;
+
+  @Prop({ type: Number, required: true })
+  hands: number;
+
+  @Prop({ type: String, required: true })
+  skinColor: string;
 }
 export const AvatarSchema = SchemaFactory.createForClass(Avatar);
