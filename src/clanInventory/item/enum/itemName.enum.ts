ac3a50c88d973208e98d4b45691447c68d350ce1
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-04-08 07:37:10 +0300

    feat: expand ItemName enum with additional item categories and names

--- src/clanInventory/item/enum/itemName.enum.ts
+++ src/clanInventory/item/enum/itemName.enum.ts
@@ -4,29 +4,94 @@
  * The Item name is used to determine Item common properties
  */
 export enum ItemName {
+  // SOFAS
   SOFA_TAAKKA = 'Sofa_Taakka',
-  MIRROR_TAAKKA = 'Mirror_Taakka',
-  FLOORLAMP_TAAKKA = 'Floorlamp_Taakka',
-  CLOSET_TAAKKA = 'Closet_Taakka',
-  SOFATABLE_TAAKKA = 'SofaTable_Taakka',
-  ARMCHAIR_TAAKKA = 'ArmChair_Taakka',
-
-  TOILET_SCHRODINGER = 'Toilet_Schrodinger',
-  SINK_SCHRODINGER = 'Sink_Schrodinger',
-  CARPET_SCHRODINGER = 'Carpet_Schrodinger',
-  MIRROR_SCHRODINGER = 'Mirror_Schrodinger',
-
-  CARPET_RAKKAUS = 'Carpet_Rakkaus',
-  MIRROR_RAKKAUS = 'Mirror_Rakkaus',
   SOFA_RAKKAUS = 'Sofa_Rakkaus',
+  SOFA_MUISTOJA = 'Sofa_Muistoja',
+  SOFA_UNI = 'Sofa_Uni',
+  SOFA_KIPU = 'Sofa_Kipu',
+  SOFA_KYLMÄ_TULEVAISUUS = 'Sofa_Kylmä_Tulevaisuus',
+
+  // CHAIRS
+  ARMCHAIR_TAAKKA = 'ArmChair_Taakka',
   ARMCHAIR_RAKKAUS = 'ArmChair_Rakkaus',
-  CLOSET_RAKKAUS = 'Closet_Rakkaus',
-  FLOORLAMP_RAKKAUS = 'Floorlamp_Rakkaus',
+  CHAIR_NEURO = 'Chair_Neuro',
+  STOOL_NEURO = 'Stool_Neuro',
+  CHAIR_POLARITY = 'Chair_Polarity',
+  ARMCHAIR_MUISTOJA = 'ArmChair_Muistoja',
+  ARMCHAIR_KYLMÄ_TULEVAISUUS = 'ArmChair_Kylmä_Tulevaisuus',
+  CHAIR_KIPU = 'Chair_Kipu',
+  HANGINGCHAIR_UNI = 'HangingChair_Uni',
+
+  // LAMPS
+  FLOORLAMP_TAAKKA = 'Floorlamp_Taakka',
+  LAMP_RAKKAUS = 'Lamp_Rakkaus',
+  TABLELAMP_UNI = 'Tablelamp_Uni',
+  CEILINGLAMP_UNI = 'Ceilinglamp_Uni',
+
+  // PLANTS
+  FICUS_MUISTOJA = 'Muistoja_Ficus',
+  FLOWERS_MUISTOJA = 'Muistoja_Flowers',
+
+  // TABLES
+  WORK_TABLE = 'WorkTable_Työ',
+  SOFATABLE_TAAKKA = 'SofaTable_Taakka',
+  SIDETABLE_TAAKKA = 'SideTable_Taakka',
   DININGTABLE_RAKKAUS = 'Diningtable_Rakkaus',
   SOFATABLE_RAKKAUS = 'SofaTable_Rakkaus',
-  BED_RAKKAUS = 'Bed_Rakkaus',
+  COFFEETABLE_MUISTOJA = 'CoffeeTable_Muistoja',
+  DRESSER_MUISTOJA = 'Dresser_Muistoja',
+  TABLE_POLARITY = 'Table_Polarity',
+  SOFATABLE_UNI = 'SofaTable_Uni',
+  NIGHTSTAND_UNI = 'Nightstand_Uni',
+  TABLE_KIPU = 'Table_Kipu',
 
-  CHAIR_NEURO = 'Chair_Neuro',
+  // BEDS
+  BED_RAKKAUS = 'Bed_Rakkaus',
+  BED_UNI = 'Bed_Uni',
+
+  // CARPETS
+  CARPET_SCHRODINGER = 'Carpet_Schrodinger',
+  CARPET_RAKKAUS = 'Carpet_Rakkaus',
+  CARPET_OLD_MUISTOJA = 'Carpet_Old_Muistoja',
+  CARPET_NEW_MUISTOJA = 'Carpet_New_Muistoja',
+  CARPET_UNI = 'Carpet_Uni',
+
+  // WALL DECOR
+  MIRROR_SCHRODINGER = 'Mirror_Schrodinger',
+  MIRROR_RAKKAUS = 'Mirror_Rakkaus',
+  CLOCK_NEURO = 'Clock_Neuro',
+  PAINTING_MUISTOJA = 'Painting_Muistoja',
+  DRAWINGS_MUISTOJA = 'Drawings_Muistoja',
+  PICTURES_MUISTOJA = 'Pictures_Muistoja',
+  WINDOW_MUISTOJA = 'Window_Muistoja',
+  WINDOW_UNI = 'Window_Uni',
+  PICTURES_UNI = 'Pictures_Uni',
+
+  // BIG MIRRORS, STATUES ETC...
+  MIRROR_TAAKKA = 'Mirror_Taakka',
+
+  // BATHROOM
+  TOILET_SCHRODINGER = 'Toilet_Schrodinger',
+  SINK_SCHRODINGER = 'Sink_Schrodinger',
+  BATHTUB_SCHRODINGER = 'Bathtub_Schrodinger',
+
+  // CLOSETS AND SHELVES
+  CLOSET_TAAKKA = 'Closet_Taakka',
+  CLOSET_RAKKAUS = 'Closet_Rakkaus',
   DRESSER_NEURO = 'Dresser_Neuro',
-  STOOL_NEURO = 'Stool_Neuro',
+  BOOKSHELF_POLARITY = 'Bookshelf_Polarity',
+  CLOSET_UNI = 'Closet_Uni',
+  CLOSET_KIPU = 'Closet_Kipu',
+
+  // MISC
+  TOY_FOX_MUISTOJA = 'Toy_Fox_Muistoja',
+  CANDLES_RAKKAUS = 'Candles_Rakkaus',
+  BOOK_PILE_RAKKAUS = 'Book_Pile_Rakkaus',
+  ELEPHANT_RAKKAUS = 'Elephant_Rakkaus',
+  FLOWER_RAKKAUS = 'Flower_Rakkaus',
+  BOOK_RAKKAUS = 'Book_Rakkaus',
+  PILLOW_RAKKAUS = 'Pillow_Rakkaus',
+  PLANT_KIPU = 'Plant_Kipu',
+  HOLOGRAM_KYLMÄ_TULEVAISUUS = 'Hologram_Kylmä_Tulevaisuus',
 }
