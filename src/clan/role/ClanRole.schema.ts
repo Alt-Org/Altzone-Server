c2185d363c13820fc484c5fb20d86c33dcd962f9
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-04-10 12:02:00 +0300

    add validation to rights field in ClanRole schema

--- src/clan/role/ClanRole.schema.ts
+++ src/clan/role/ClanRole.schema.ts
@@ -18,15 +18,36 @@ export class ClanRole {
    * Type of the role
    */
   @Prop({ type: String, enum: ClanRoleType, required: true })
-  claRoleType: ClanRoleType;
+  clanRoleType: ClanRoleType;
 
   /**
    * Object with basic rights that the role has
    */
-  @Prop({ type: Object, required: true })
+  @Prop({
+    type: Map,
+    of: Boolean,
+    required: true,
+    validate: {
+      validator: (map: Map<ClanBasicRight, true>) => areRightsValid(map),
+      message:
+        'Rights object must have a key which is a value of ClanBasicRight enum and the value is true',
+    },
+  })
   rights: Partial<Record<ClanBasicRight, true>>;
 
   _id: ObjectId;
 }
 
 export const ClanRoleSchema = SchemaFactory.createForClass(ClanRole);
+
+function areRightsValid(map: Map<ClanBasicRight, true>): boolean {
+  const allowedKeys = new Set(Object.values(ClanBasicRight));
+
+  for (const [key, value] of map.entries()) {
+    if (!allowedKeys.has(key)) return false;
+
+    if (value !== true) return false;
+  }
+
+  return true;
+}
