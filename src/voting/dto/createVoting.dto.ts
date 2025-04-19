176a2162b6363e3a3902eb35a7ce5e562d1b78a0
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-04-19 22:36:42 +0300

    feat: Add entity_name field to CreateVotingDto for enhanced voting context

--- src/voting/dto/createVoting.dto.ts
+++ src/voting/dto/createVoting.dto.ts
@@ -12,6 +12,7 @@ import { VotingType } from '../enum/VotingType.enum';
 import { Vote } from '../schemas/vote.schema';
 import { Type } from 'class-transformer';
 import { Organizer } from './organizer.dto';
+import { ItemName } from '../../clanInventory/item/enum/itemName.enum';
 
 @AddType('CreateVotingDto')
 export class CreateVotingDto {
@@ -33,6 +34,10 @@ export class CreateVotingDto {
   @IsMongoId()
   @IsOptional()
   entity_id?: string;
+
+  @IsEnum(ItemName)
+  @IsOptional()
+  entity_name?: ItemName;
 
   @IsArray()
   @IsOptional()
