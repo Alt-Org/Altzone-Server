d3c00099853216773aca1b7a386af90901363e71
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-04-22 18:13:37 +0300

    fix errors with online player testing module nest injections
    Errors were caused by not all required modules were imported to the testing module

--- src/__tests__/onlinePlayers/modules/onlinePlayersCommon.module.ts
+++ src/__tests__/onlinePlayers/modules/onlinePlayersCommon.module.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OnlinePlayersService } from '../../../onlinePlayers/onlinePlayers.service';
 import { CacheModule } from '@nestjs/cache-manager';
+import { PlayerModule } from '../../../player/player.module';
+import { RequestHelperModule } from '../../../requestHelper/requestHelper.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongooseOptions, mongoString } from '../../test_utils/const/db';
 
 export default class OnlinePlayersCommonModule {
   private static module: TestingModule;
@@ -8,7 +12,12 @@ export default class OnlinePlayersCommonModule {
   static async getModule() {
     if (!OnlinePlayersCommonModule.module)
       OnlinePlayersCommonModule.module = await Test.createTestingModule({
-        imports: [CacheModule.register()],
+        imports: [
+          MongooseModule.forRoot(mongoString, mongooseOptions),
+          CacheModule.register(),
+          PlayerModule,
+          RequestHelperModule,
+        ],
         providers: [OnlinePlayersService],
       }).compile();
 
