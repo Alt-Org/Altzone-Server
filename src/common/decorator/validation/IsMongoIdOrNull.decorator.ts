2e9667beff1406cb89eff34f64aa663c491a7328
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-04-28 22:27:25 +0300

    Add IsMongoIdOrNull decorator

---
+++ src/common/decorator/validation/IsMongoIdOrNull.decorator.ts
@@ -0,0 +1,36 @@
+import { registerDecorator,
+   ValidationArguments,
+   ValidationOptions, 
+   ValidatorConstraint, 
+   ValidatorConstraintInterface } from 'class-validator';
+
+
+export function IsMongoIdOrNull(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      propertyName,
+      name: IsMongoIdOrNull.name,
+      target: object.constructor,
+      options: validationOptions,
+      validator: IsMongoIdOrNullConstraint,
+    });
+  };
+}
+
+@ValidatorConstraint({ async: false })
+class IsMongoIdOrNullConstraint implements ValidatorConstraintInterface {
+  validate(value: any) {
+    
+    const mongoIdRegexPattern = /^[a-f\d]{24}$/i;
+
+    if (value === null) return true;
+    if (Array.isArray(value)) {
+      return value.every((item) => item == null || mongoIdRegexPattern.test(item));
+    }
+    return mongoIdRegexPattern.test(value);
+  }
+
+  defaultMessage(_args: ValidationArguments) {
+    return _args.property + ' must be a mongodb id or null';
+  }
+}
