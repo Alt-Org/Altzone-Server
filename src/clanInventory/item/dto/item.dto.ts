7958ac38838fa5985af29145e2be4fc8cd6b19a3
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 16:42:29 +0300

    add swagger definitions for /item endpoints

--- src/clanInventory/item/dto/item.dto.ts
+++ src/clanInventory/item/dto/item.dto.ts
@@ -10,49 +10,115 @@ import { Material } from '../enum/material.enum';
 
 @AddType('ItemDto')
 export class ItemDto {
+  /**
+   * Unique identifier of the item
+   *
+   * @example "665a1f29c3f4fa0012e7a900"
+   */
   @ExtractField()
   @Expose()
   _id: string;
 
+  /**
+   * Name of the item
+   *
+   * @example "Sofa_Taakka"
+   */
   @Expose()
   name: ItemName;
 
+  /**
+   * Weight of the item
+   *
+   * @example 1
+   */
   @Expose()
   weight: number;
 
+  /**
+   * Recycling type category
+   *
+   * @example "Wood"
+   */
   @Expose()
   recycling: Recycling;
 
+  /**
+   * Item rarity
+   *
+   * @example "common"
+   */
   @Expose()
   rarity: Rarity;
 
+  /**
+   * Materials that compose the item
+   *
+   * @example ["puu", "nahka"]
+   */
   @Expose()
   material: Material[];
 
+  /**
+   * Unity engine key for rendering
+   *
+   * @example "items/mystic_orb"
+   */
   @Expose()
   unityKey: string;
 
+  /**
+   * Price of the item in in-game currency
+   *
+   * @example 500
+   */
   @Expose()
   price: number;
 
+  /**
+   * Grid location of the item
+   *
+   * @example [1, 4]
+   */
   @Expose()
   location: Array<number>;
 
+  /**
+   * Whether the item is a piece of furniture
+   *
+   * @example false
+   */
   @Expose()
   isFurniture: boolean;
 
+  /**
+   * ID of the stock storing this item
+   *
+   * @example "666d99d3e3a12a001234abcd"
+   */
   @ExtractField()
   @Expose()
   stock_id: string;
 
+  /**
+   * Full stock object containing this item
+   */
   @Type(() => StockDto)
   @Expose()
   Stock: StockDto;
 
+  /**
+   * ID of the room containing the item
+   *
+   * @example "666c88a7f2a98e001298cdef"
+   */
   @ExtractField()
   @Expose()
   room_id: string;
 
+  /**
+   * Full room object containing this item
+   */
   @Type(() => RoomDto)
   @Expose()
   Room: RoomDto;
--- src/clanInventory/item/dto/moveItem.dto.ts
+++ src/clanInventory/item/dto/moveItem.dto.ts
@@ -4,13 +4,28 @@ import AddType from '../../../common/base/decorator/AddType.decorator';
 
 @AddType('MoveItemDto')
 export class MoveItemDto {
+  /**
+   * ID of the item to move
+   *
+   * @example "665a1f29c3f4fa0012e7a900"
+   */
   @IsMongoId()
   @IsString()
   item_id: string;
 
+  /**
+   * Destination type: ROOM or STOCK
+   *
+   * @example "Stock"
+   */
   @IsEnum(MoveTo)
   moveTo: MoveTo;
 
+  /**
+   * Destination ID (room or stock depending on moveTo)
+   *
+   * @example "666c88a7f2a98e001298cdef"
+   */
   @ValidateIf((o) => o.move_to === MoveTo.ROOM)
   @IsMongoId()
   destination_id: string;
