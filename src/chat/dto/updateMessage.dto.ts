20c7fc2b84c6370a1daff797d6402ea7878f8650
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 16:08:07 +0300

    add swagger definitions for /chat endpoints

--- src/chat/dto/updateMessage.dto.ts
+++ src/chat/dto/updateMessage.dto.ts
@@ -3,13 +3,28 @@ import AddType from '../../common/base/decorator/AddType.decorator';
 
 @AddType('UpdateMessageDto')
 export class UpdateMessageDto {
+  /**
+   * ID of the message to update
+   *
+   * @example 101
+   */
   @IsInt()
   id: number;
 
+  /**
+   * Updated message content
+   *
+   * @example "Let’s regroup at Soul Gate!"
+   */
   @IsOptional()
   @IsString()
   content?: string;
 
+  /**
+   * Updated feeling value (emotion indicator)
+   *
+   * @example 2
+   */
   @IsOptional()
   @IsInt()
   feeling?: number;
