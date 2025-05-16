449af3b7ebfeeda9862861f38b41666376812837
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 16:07:04 +0300

    add swagger definitions for /auth endpoints

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -3,9 +3,19 @@ import AddType from '../../common/base/decorator/AddType.decorator';
 
 @AddType('SignInDto')
 export class SignInDto {
+  /**
+   * Unique username used by the player to sign in
+   *
+   * @example "DragonSlayer42"
+   */
   @IsString()
   username: string;
 
+  /**
+   * Password for player authentication
+   *
+   * @example "myStrongP@ssw0rd"
+   */
   @IsString()
   password: string;
 }
