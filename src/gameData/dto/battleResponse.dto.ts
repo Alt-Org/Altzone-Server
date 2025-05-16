26ecbb5df7ff5eb0465997b0631ffa9daa338ef3
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 18:46:00 +0300

    add swagger definitions for /gameData endpoints

--- src/gameData/dto/battleResponse.dto.ts
+++ src/gameData/dto/battleResponse.dto.ts
@@ -1,5 +1,27 @@
-export type BattleResponseDto = {
+import { Expose } from 'class-transformer';
+
+export class BattleResponseDto {
+  /**
+   * Token required to steal items after a won battle
+   *
+   * @example "abc123-steal-token"
+   */
+  @Expose()
   stealToken: string;
+
+  /**
+   * Soul home ID where items can be stolen
+   *
+   * @example "665af23e5e982f0013aa7777"
+   */
+  @Expose()
   soulHome_id: string;
+
+  /**
+   * Rooms _ids where items can be stolen
+   *
+   * @example ["665af23e5e982f0013aa8888", "665af23e5e982f0013aa9999"]
+   */
+  @Expose()
   roomIds: string[];
-};
+}
--- src/gameData/dto/battleResult.dto.ts
+++ src/gameData/dto/battleResult.dto.ts
@@ -10,21 +10,46 @@ import {
 import { RequestType } from '../enum/requestType.enum';
 
 export class BattleResultDto {
+  /**
+   * Type of the request
+   *
+   * @example "result"
+   */
   @IsEnum(RequestType)
   type: RequestType.RESULT;
 
+  /**
+   * IDs of players in team 1
+   *
+   * @example ["665af23e5e982f0013aa1111", "665af23e5e982f0013aa2222"]
+   */
   @IsArray()
   @IsMongoId({ each: true })
   team1: string[];
 
+  /**
+   * IDs of players in team 2
+   *
+   * @example ["665af23e5e982f0013aa3333", "665af23e5e982f0013aa4444"]
+   */
   @IsArray()
   @IsMongoId({ each: true })
   team2: string[];
 
+  /**
+   * Duration of the battle in seconds
+   *
+   * @example 120
+   */
   @IsInt()
   @IsPositive()
   duration: number;
 
+  /**
+   * Number of the winning team (1 or 2)
+   *
+   * @example 1
+   */
   @IsInt()
   @Min(1)
   @Max(2)
