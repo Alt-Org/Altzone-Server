d5425c53a6c98876f11c7fbb7274296cee5ed7ff
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 17:43:09 +0300

    add swagger definitions for /clan-shop endpoints

--- src/clanShop/clanShop.controller.ts
+++ src/clanShop/clanShop.controller.ts
@@ -12,6 +12,8 @@ import { APIErrorReason } from '../common/controller/APIErrorReason';
 import { ItemName } from '../clanInventory/item/enum/itemName.enum';
 import HasClanRights from '../clan/role/decorator/guard/HasClanRights';
 import { ClanBasicRight } from '../clan/role/enum/clanBasicRight.enum';
+import ApiResponseDescription from '../common/swagger/response/ApiResponseDescription';
+import ClanShopItemDto from './dto/ClanShopItem.dto';
 
 @Controller('clan-shop')
 export class ClanShopController {
@@ -20,6 +22,21 @@ export class ClanShopController {
     private readonly service: ClanShopService,
   ) {}
 
+  /**
+   * Get all available items in clan shop
+   *
+   * @remarks Get a list of all items in the clan shop.
+   *
+   * Notice that item are rotated on every server restart and every day
+   */
+  @ApiResponseDescription({
+    success: {
+      status: 204,
+      dto: ClanShopItemDto,
+      modelName: ModelName.ITEM,
+    },
+    hasAuth: false,
+  })
   @Get('items')
   @NoAuth()
   @UniformResponse(ModelName.ITEM)
@@ -27,6 +44,23 @@ export class ClanShopController {
     return this.clanShopScheduler.currentShopItems;
   }
 
+  /**
+   * Buy an item from clan shop
+   *
+   * @remarks Buy an item from a clan shop.
+   *
+   * Notice that the item will not be bought right away, but before majority of clan members should vote to buy the item.
+   *
+   * There should be also enough coins to buy an item.
+   *
+   * Notice that the player must be in the same clan and it must have a basic right "Shop"
+   */
+  @ApiResponseDescription({
+    success: {
+      status: 204,
+    },
+    errors: [400, 401, 403],
+  })
   @Post('buy')
   @DetermineClanId()
   @HasClanRights([ClanBasicRight.SHOP])
