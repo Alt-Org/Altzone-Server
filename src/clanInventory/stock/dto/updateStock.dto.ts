46efd4bf50a17ae29087a9fe67ca0ee3bf420ee8
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 17:17:04 +0300

    add swagger definitions for /stock endpoints

--- src/clanInventory/stock/dto/updateStock.dto.ts
+++ src/clanInventory/stock/dto/updateStock.dto.ts
@@ -5,14 +5,29 @@ import AddType from '../../../common/base/decorator/AddType.decorator';
 
 @AddType('UpdateStockDto')
 export class UpdateStockDto {
+  /**
+   * ID of the stock to update
+   *
+   * @example "666fab12d2f0e10012ccbbdd"
+   */
   @IsStockExists()
   @IsMongoId()
   _id: string;
 
+  /**
+   * Updated number of item cells in the stock
+   *
+   * @example 25
+   */
   @IsInt()
   @IsOptional()
   cellCount: number;
 
+  /**
+   * Updated clan ID associated with the stock
+   *
+   * @example "666def12e1c2a50014bcaaff"
+   */
   @IsClanExists()
   @IsMongoId()
   @IsOptional()
