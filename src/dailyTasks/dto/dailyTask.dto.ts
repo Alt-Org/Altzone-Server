b2c1a5cebea897218ca2c1b92f4811e745f38785
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-16 17:59:05 +0300

    add swagger definitions for /dailyTask endpoints

--- src/dailyTasks/dto/dailyTask.dto.ts
+++ src/dailyTasks/dto/dailyTask.dto.ts
@@ -7,39 +7,94 @@ import { UITaskName } from '../enum/uiTaskName.enum';
 
 @AddType('DailyTaskDto')
 export class DailyTaskDto {
+  /**
+   * Unique identifier of the daily task
+   *
+   * @example "665af23e5e982f0013aa334b"
+   */
   @ExtractField()
   @Expose()
   _id: string;
 
+  /**
+   * ID of the clan associated with this task
+   *
+   * @example "665af23e5e982f0013aa1122"
+   */
   @ExtractField()
   @Expose()
   clan_id: string;
 
+  /**
+   * ID of the player assigned to the task
+   *
+   * @example "665af23e5e982f0013aa4455"
+   */
   @ExtractField()
   @Expose()
   player_id: string;
 
+  /**
+   * Title or brief description of the task
+   *
+   * @example {fi: "Lähetä 10 viestiä chatissa"}
+   */
   @Expose()
   title: TaskTitle;
 
+  /**
+   * Type of task, either server-defined or UI-triggered
+   *
+   * @example "write_chat_message"
+   */
   @Expose()
   type: ServerTaskName | UITaskName;
 
+  /**
+   * Number of points rewarded upon completion
+   *
+   * @example 50
+   */
   @Expose()
   points: number;
 
+  /**
+   * Amount of coins rewarded for finishing the task
+   *
+   * @example 100
+   */
   @Expose()
   coins: number;
 
+  /**
+   * Timestamp when the task was started
+   *
+   * @example "2025-05-16T14:25:00.000Z"
+   */
   @Expose()
   startedAt: Date;
 
+  /**
+   * Total amount required to complete the task
+   *
+   * @example 10
+   */
   @Expose()
   amount: number;
 
+  /**
+   * Amount remaining to complete the task
+   *
+   * @example 3
+   */
   @Expose()
   amountLeft: number;
 
+  /**
+   * Time limit to finish the task, in minutes
+   *
+   * @example 60
+   */
   @Expose()
   timeLimitMinutes: number;
 }
