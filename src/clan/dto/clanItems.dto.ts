256af848f0a577199c443a0bc9332aa0361aa0dd
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-18 14:20:20 +0300

    fix bug with /clan/items GET does not return items

--- src/clan/dto/clanItems.dto.ts
+++ src/clan/dto/clanItems.dto.ts
@@ -1,13 +1,18 @@
 import { ItemDto } from '../../clanInventory/item/dto/item.dto';
+import { Expose, Type } from 'class-transformer';
 
 export default class ClanItemsDto {
   /**
    * Clan items in its stock
    */
+  @Type(() => ItemDto)
+  @Expose()
   stockItems: ItemDto[];
 
   /**
    * Clan items in its soul home rooms
    */
+  @Type(() => ItemDto)
+  @Expose()
   soulHomeItems: ItemDto[];
 }
