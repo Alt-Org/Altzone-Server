f293c698a221740789fc78fb4ea10f71cff67faa
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-20 17:51:07 +0300

    fix failing tests after `status` field was added and `RedisService` refactored

--- src/__tests__/onlinePlayers/OnlinePlayersService/addPlayerOnline.test.ts
+++ src/__tests__/onlinePlayers/OnlinePlayersService/addPlayerOnline.test.ts
@@ -3,18 +3,14 @@ import OnlinePlayersModule from '../modules/onlinePlayers.module';
 import PlayerBuilderFactory from '../../player/data/playerBuilderFactory';
 import PlayerModule from '../../player/modules/player.module';
 import { CacheKeys } from '../../../common/service/redis/cacheKeys.enum';
-
-const redisSet = jest.fn();
-jest.mock('ioredis', () => {
-  return jest.fn().mockImplementation(() => ({
-    set: redisSet,
-    keys: jest.fn(),
-    on: jest.fn(),
-  }));
-});
+import { OnlinePlayerStatus } from '../../../onlinePlayers/enum/OnlinePlayerStatus';
+import OnlinePlayersCommonModule from '../modules/onlinePlayersCommon.module';
+import { RedisService } from '../../../common/service/redis/redis.service';
 
 describe('OnlinePlayersService.addPlayerOnline() test suite', () => {
   let service: OnlinePlayersService;
+
+  let redisService: RedisService;
 
   const playerBuilder = PlayerBuilderFactory.getBuilder('Player');
   const player1 = playerBuilder
@@ -30,14 +26,28 @@ describe('OnlinePlayersService.addPlayerOnline() test suite', () => {
 
     const player1Resp = await playerModel.create(player1);
     player1._id = player1Resp._id.toString();
+
+    redisService = (await OnlinePlayersCommonModule.getModule()).get(
+      RedisService,
+    );
   });
 
-  it('Should be able to add one player to cache and set 1 as its value', async () => {
-    await service.addPlayerOnline(player1._id);
+  it('Should be able to add one player to cache', async () => {
+    const expectedKey = `${CacheKeys.ONLINE_PLAYERS}:${player1._id}`;
+    const expectedPayload = JSON.stringify({
+      _id: player1._id,
+      name: player1.name,
+      status: OnlinePlayerStatus.BATTLE,
+    });
 
-    const expectedKey = `${CacheKeys.ONLINE_PLAYERS}:${JSON.stringify({ id: player1._id, name: player1.name })}`;
+    const redisSet = jest.spyOn(redisService, 'set');
+
+    await service.addPlayerOnline({
+      player_id: player1._id,
+      status: OnlinePlayerStatus.BATTLE,
+    });
 
     expect(redisSet).toHaveBeenCalledTimes(1);
-    expect(redisSet).toHaveBeenCalledWith(expectedKey, '1', 'EX', 300);
+    expect(redisSet).toHaveBeenCalledWith(expectedKey, expectedPayload, 90);
   });
 });
--- src/__tests__/onlinePlayers/OnlinePlayersService/getAllOnlinePlayers.test.ts
+++ src/__tests__/onlinePlayers/OnlinePlayersService/getAllOnlinePlayers.test.ts
@@ -3,21 +3,17 @@ import OnlinePlayersModule from '../modules/onlinePlayers.module';
 import { ObjectId } from 'mongodb';
 import PlayerBuilderFactory from '../../player/data/playerBuilderFactory';
 import PlayerModule from '../../player/modules/player.module';
-import { CacheKeys } from '../../../common/service/redis/cacheKeys.enum';
-
-const redisKeys = jest.fn();
-jest.mock('ioredis', () => {
-  return jest.fn().mockImplementation(() => ({
-    keys: redisKeys,
-    on: jest.fn(),
-  }));
-});
+import { OnlinePlayerStatus } from '../../../onlinePlayers/enum/OnlinePlayerStatus';
+import { RedisService } from '../../../common/service/redis/redis.service';
+import OnlinePlayersCommonModule from '../modules/onlinePlayersCommon.module';
 
 describe('OnlinePlayersService.getAllOnlinePlayers() test suite', () => {
   let service: OnlinePlayersService;
 
-  const _id1 = new ObjectId();
-  const _id2 = new ObjectId();
+  let redisService: RedisService;
+
+  const _id1 = new ObjectId().toString();
+  const _id2 = new ObjectId().toString();
 
   const playerBuilder = PlayerBuilderFactory.getBuilder('Player');
   const player1 = playerBuilder
@@ -36,18 +32,30 @@ describe('OnlinePlayersService.getAllOnlinePlayers() test suite', () => {
   beforeEach(async () => {
     jest.clearAllMocks();
     service = await OnlinePlayersModule.getOnlinePlayersService();
+
+    redisService = (await OnlinePlayersCommonModule.getModule()).get(
+      RedisService,
+    );
   });
 
   it('Should return player_ids from the cache', async () => {
     await playerModel.create(player1);
     await playerModel.create(player2);
-    const payload1 = { name: player1.name, id: _id1.toString() };
-    const payload2 = { name: player2.name, id: _id2.toString() };
+    const payload1 = {
+      name: player1.name,
+      _id: _id1,
+      status: OnlinePlayerStatus.UI,
+    };
+    const payload2 = {
+      name: player2.name,
+      _id: _id2,
+      status: OnlinePlayerStatus.BATTLE,
+    };
 
-    redisKeys.mockReturnValueOnce([
-      `${CacheKeys.ONLINE_PLAYERS}:${JSON.stringify(payload1)}`,
-      `${CacheKeys.ONLINE_PLAYERS}:${JSON.stringify(payload2)}`,
-    ]);
+    jest.spyOn(redisService, 'getValuesByKeyPattern').mockResolvedValue({
+      [_id1]: JSON.stringify(payload1),
+      [_id2]: JSON.stringify(payload2),
+    });
 
     const player_ids = await service.getAllOnlinePlayers();
 
@@ -55,14 +63,8 @@ describe('OnlinePlayersService.getAllOnlinePlayers() test suite', () => {
     expect(player_ids).toContainEqual(payload2);
   });
 
-  it('Should return an empty array if there are no players set', async () => {
-    const player_ids = await service.getAllOnlinePlayers();
-
-    expect(player_ids).toHaveLength(0);
-  });
-
-  it('Should not return players _ids which TTL was expired', async () => {
-    redisKeys.mockReturnValueOnce(undefined);
+  it('Should not return players if there are no players', async () => {
+    jest.spyOn(redisService, 'getValuesByKeyPattern').mockResolvedValue({});
 
     const player_ids = await service.getAllOnlinePlayers();
 
