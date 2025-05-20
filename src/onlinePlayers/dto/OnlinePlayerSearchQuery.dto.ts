793f373b49beb31ce9d2745d3983ec0c0da08a59
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-20 19:04:24 +0300

    add filtering for /online-players by `status` field

---
+++ src/onlinePlayers/dto/OnlinePlayerSearchQuery.dto.ts
@@ -0,0 +1,18 @@
+import { OnlinePlayerStatus } from '../enum/OnlinePlayerStatus';
+import { IsArray, IsEnum, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export default class OnlinePlayerSearchQueryDto {
+  @IsOptional()
+  @IsArray()
+  @IsEnum(OnlinePlayerStatus, { each: true })
+  @Transform(({ value }) => {
+    if (!value) return undefined;
+
+    const matches = value.match(/status="(.*?)"/g);
+    if (!matches) return [];
+
+    return matches.map((m) => m.replace(/status="|"$/g, ''));
+  })
+  search?: OnlinePlayerStatus[];
+}
