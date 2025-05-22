bbec71689dd38e23bd3f0bb8f4309fde518ddbd0
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-05-22 21:54:21 +0300

    clanCoins.dto: drop clan_id

--- src/shop/buy/dto/clanCoins.dto.ts
+++ src/shop/buy/dto/clanCoins.dto.ts
@@ -1,19 +1,11 @@
 import {
   IsEnum,
-  IsMongoId,
-  IsOptional,
 } from 'class-validator';
 import AddType from '../../../common/base/decorator/AddType.decorator';
 import { Coins } from '../../../shop/enum/coins.enum.dto';
-import { IsClanExists } from '../../../clan/decorator/validation/IsClanExists.decorator';
 
 @AddType('ClanCoinsDto')
 export class ClanCoinsDto {
-  
-  @IsClanExists()
-  @IsMongoId()
-  @IsOptional()
-  clan_id: string;
 
   @IsEnum(Coins)
   amount: Coins;
