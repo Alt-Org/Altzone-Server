36f5af1e5556a1d9b0214b1e4174723bc77834d3
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-23 20:52:25 +0300

    add swagger description for ClanCoinsDto

--- src/shop/buy/dto/clanCoins.dto.ts
+++ src/shop/buy/dto/clanCoins.dto.ts
@@ -1,9 +1,13 @@
 import { IsEnum } from 'class-validator';
 import AddType from '../../../common/base/decorator/AddType.decorator';
-import { Coins } from '../../../shop/enum/coins.enum.dto';
+import { Coins } from '../../enum/coins.enum.dto';
 
 @AddType('ClanCoinsDto')
 export class ClanCoinsDto {
+  /**
+   * Amount of coins to buy
+   * @example 100
+   */
   @IsEnum(Coins)
   amount: Coins;
 }
