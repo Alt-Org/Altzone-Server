b78e1773dc27a7edabd3a7431652f44522aba7c5
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-23 21:13:01 +0300

    add comments for `ClanCoinsService`

--- src/shop/buy/clanCoins.service.ts
+++ src/shop/buy/clanCoins.service.ts
@@ -5,8 +5,16 @@ import { ClanService } from '../../clan/clan.service';
 export class ClanCoinsService {
   public constructor(private readonly clanService: ClanService) {}
 
+  /**
+   * Add coins to a specified clan
+   *
+   * @param clan_id clan _id for which coins should be added
+   * @param amount amount of coins to add
+   *
+   * @return true if succeeded and ServiceError NOT_FOUND if clan is not found
+   */
   async addCoins(clan_id: string, amount: number) {
-    return await this.clanService.basicService.updateOneById(clan_id, {
+    return this.clanService.basicService.updateOneById(clan_id, {
       $inc: { gameCoins: amount },
     });
   }
