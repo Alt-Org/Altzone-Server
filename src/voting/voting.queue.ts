b0f2e1102c042b2191da3d8790f72574661c425b
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-06-02 09:44:33 +0300

    refactor: update VotingQueue to handle multiple queues for voting check jobs

--- src/voting/voting.queue.ts
+++ src/voting/voting.queue.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bullmq';
-import { VotingQueueParams } from '../fleaMarket/types/votingQueueParams.type';
 import { Queue } from 'bullmq';
+import { VotingQueueParams } from '../fleaMarket/types/votingQueueParams.type';
+import { VotingQueueName } from './enum/VotingQueue.enum';
 
 @Injectable()
 export class VotingQueue {
-  /**
-   * Creates an instance of VotingQueue.
-   * @param votingQueue - The Bull queue instance for voting.
-   */
-  constructor(@InjectQueue('voting') private readonly votingQueue: Queue) {}
+  constructor(
+    @InjectQueue(VotingQueueName.CLAN_ROLE)
+    private readonly clanRoleQueue: Queue,
+    @InjectQueue(VotingQueueName.CLAN_SHOP)
+    private readonly clanShopQueue: Queue,
+    @InjectQueue(VotingQueueName.FLEA_MARKET)
+    private readonly fleaMarketQueue: Queue,
+  ) {}
 
   /**
-   * Adds a voting check job to the queue with a specified delay.
-   * @param params - The parameters for the voting check job.
+   * Adds a voting check job to the appropriate queue with a delay based on the voting's scheduled end time.
+   *
+   * @param params - The parameters for the voting check job, including voting details, queue type, and optional item IDs.
+   * @throws Error if the queue type is unknown.
    */
   async addVotingCheckJob(params: VotingQueueParams) {
     const delay = params.voting.endsOn.getTime() - Date.now();
-    await this.votingQueue.add(params.queue, params, { delay });
+
+    switch (params.queue) {
+      case VotingQueueName.CLAN_ROLE:
+        await this.clanRoleQueue.add(params.queue, params, { delay });
+        break;
+      case VotingQueueName.CLAN_SHOP:
+        await this.clanShopQueue.add(params.queue, params, { delay });
+        break;
+      case VotingQueueName.FLEA_MARKET:
+        await this.fleaMarketQueue.add(params.queue, params, { delay });
+        break;
+      default:
+        throw new Error(`Unknown queue: ${params.queue}`);
+    }
   }
 }
