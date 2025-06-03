5bd97039e90d3eb6168933a151eda3ffb6cfa0ad
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-06-03 13:25:11 +0300

    add jenkins config

---
+++ jest.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'jest';
+
+const root = '<rootDir>';
+const codeDir = `${root}/src`;
+const testDir = `${root}/src/__tests__`;
+
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+const config: Config = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+
+  testMatch: [`${testDir}/**/*.test.ts`],
+  testPathIgnorePatterns: [`${testDir}/test_utils`],
+  collectCoverageFrom: [`${codeDir}/**/*.ts`],
+  coverageDirectory: './coverage',
+  coverageReporters: ['cobertura'],
+  reporters: ['jest-junit'],
+
+  setupFiles: [
+    `${testDir}/test_utils/env.ts`,
+    `${testDir}/test_utils/disable-console-log.ts`,
+  ],
+  setupFilesAfterEnv: [
+    `${testDir}/test_utils/connect-to-db.ts`,
+    `${testDir}/test_utils/add-custom-matchers.ts`,
+  ],
+
+  globalSetup: `${testDir}/test_utils/globalSetup.ts`,
+  globalTeardown: `${testDir}/test_utils/globalTeardown.ts`,
+};
+
+export default config;
