4f11c742cf58ca56bcc41161e404d53a869fd1bc
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-06-18 16:26:40 +0300

    lint fixes

--- src/gameData/gameData.service.ts
+++ src/gameData/gameData.service.ts
@@ -97,20 +97,20 @@ export class GameDataService {
    * @returns - Returns a promise that contains the expected version number or an API error.
    */
   async getMinVersion(): Promise<IServiceReturn<string>> {
-
-
-    
-  if (envVars.MINIMUM_VERSION.length === 0  || envVars.MINIMUM_VERSION === '0') {
-    return [
-      null,
-      [
-        new ServiceError({
-          reason: SEReason.MISCONFIGURED,
-          message: 'MINIMUM_VERSION is not set in the environment variables.',
-        }),
-      ],
-    ];
-  }
+    if (
+      envVars.MINIMUM_VERSION.length === 0 ||
+      envVars.MINIMUM_VERSION === '0'
+    ) {
+      return [
+        null,
+        [
+          new ServiceError({
+            reason: SEReason.MISCONFIGURED,
+            message: 'MINIMUM_VERSION is not set in the environment variables.',
+          }),
+        ],
+      ];
+    }
     return [envVars.MINIMUM_VERSION, null];
   }
 
