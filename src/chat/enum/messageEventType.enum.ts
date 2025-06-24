2ffb8cf373c00aa2536752d517edb8421a41d486
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-06-24 14:51:48 +0300

    fix: add missing documentation for MessageEventType enum

--- src/chat/enum/messageEventType.enum.ts
+++ src/chat/enum/messageEventType.enum.ts
@@ -1,3 +1,6 @@
+/**
+ * Enum used to specify the event type of websocket messages sent from server.
+ */
 export enum MessageEventType {
   NEW_MESSAGE = 'newMessage',
   NEW_REACTION = 'newReaction',
