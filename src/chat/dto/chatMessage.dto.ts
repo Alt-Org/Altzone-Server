583a755e07df99cceb344b70db7e7a7f379218dd
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-06-24 14:43:45 +0300

    fix: clarify recipient ID property name in ChatMessageDto

--- src/chat/dto/chatMessage.dto.ts
+++ src/chat/dto/chatMessage.dto.ts
@@ -25,11 +25,11 @@ export class ChatMessageDto {
   sender: Sender;
 
   /**
-   * ID of the recipient (for private messages)
+   * ID of the recipient player (for private messages)
    * @example "60d21b4667d0d8992e610c85"
    */
   @Expose()
-  recipient_id?: string;
+  recipientPlayer_id?: string;
 
   /**
    * Content of the chat message
