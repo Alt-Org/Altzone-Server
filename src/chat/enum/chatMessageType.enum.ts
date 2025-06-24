b7f14a89413b4c14ab5b39370058de2e0078f845
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-06-24 15:00:51 +0300

    fix: add missing documentation for ChatType enum

--- src/chat/enum/chatMessageType.enum.ts
+++ src/chat/enum/chatMessageType.enum.ts
@@ -1,3 +1,7 @@
+/**
+ * Specifies the type of chat message.
+ * Indicates whether the message is sent to global, clan, or private chat.
+ */
 export enum ChatType {
   CLAN = 'clan',
   PRIVATE = 'private',
