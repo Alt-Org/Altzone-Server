3fd83b2ce05295747251df454245cf1845dab981
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-06-24 14:41:54 +0300

    add jsDocs for WsUser and WebSocketUser types

--- src/chat/types/WsUser.type.ts
+++ src/chat/types/WsUser.type.ts
@@ -1,11 +1,23 @@
 import { WebSocket } from 'ws';
 
+/**
+ * Represents a user connected via WebSocket in the chat system.
+ *
+ * @property playerId - The _id of the player.
+ * @property clanId - Optional _id of the clan the user belongs to.
+ * @property name - The name of the player.
+ */
 export type WsUser = {
   playerId: string;
   clanId?: string;
   name: string;
 };
 
+/**
+ * Extends the WebSocket interface to optionally include a user object.
+ *
+ * @property user - An optional user object associated with the WebSocket connection.
+ */
 export type WebSocketUser = WebSocket & {
   user?: WsUser;
 };
