2333df3970cfebb2f210a6c978fe9ed3a82d7a8f
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-07-06 17:45:44 +0300

    make /box/claim-account GET to be POST
    Change was required due to conflicting GET signatures with /box/_id GET

--- src/box/accountClaimer/accountClaimer.controller.ts
+++ src/box/accountClaimer/accountClaimer.controller.ts
@@ -1,13 +1,12 @@
 import SwaggerTags from '../../common/swagger/tags/SwaggerTags.decorator';
-import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import ApiResponseDescription from '../../common/swagger/response/ApiResponseDescription';
 import { NoAuth } from '../../auth/decorator/NoAuth.decorator';
 import { UniformResponse } from '../../common/decorator/response/UniformResponse';
-import { APIError } from '../../common/controller/APIError';
-import { APIErrorReason } from '../../common/controller/APIErrorReason';
-import { Request, Response } from 'express';
 import AccountClaimerService from './accountClaimer.service';
 import ClaimedAccountDto from './dto/claimedAccount.dto';
+import ClaimAccountDto from './dto/claimAccount.dto';
+import { ModelName } from '../../common/enum/modelName.enum';
 
 @SwaggerTags('Box')
 @Controller('/box/claim-account')
@@ -19,10 +18,8 @@ export class AccountClaimerController {
    *
    * @remarks Tester can claim his/her account for the testing box.
    *
-   * Notice that the tester should know the shared testers password in order to be able to clain the account.
+   * Notice that the tester should know the shared testers password in order to be able to claim the account.
    * This password is available after the group admin has started the session.
-   *
-   * Notice that the endpoint should be called only from browser, since a cookie should be added by the API
    */
   @ApiResponseDescription({
     success: {
@@ -33,25 +30,9 @@ export class AccountClaimerController {
     hasAuth: false,
   })
   @NoAuth()
-  @Get('claim-account')
-  @UniformResponse(undefined, ClaimedAccountDto)
-  async claimAccount(
-    @Req() req: Request,
-    @Res() res: Response,
-    @Query('password') password: string,
-  ) {
-    const { accountClaimed } = req.cookies;
-    if (accountClaimed)
-      throw new APIError({
-        reason: APIErrorReason.NOT_AUTHORIZED,
-        message: 'Account already claimed from this device.',
-      });
-
-    const data = await this.accountService.claimAccount(password);
-    res.cookie('accountClaimed', true, {
-      httpOnly: false,
-      maxAge: 15 * 60 * 1000,
-    });
-    return res.send(data);
+  @Post()
+  @UniformResponse(ModelName.PLAYER, ClaimedAccountDto)
+  async claimAccount(@Body() body: ClaimAccountDto) {
+    return this.accountService.claimAccount(body.sharedPassword);
   }
 }
