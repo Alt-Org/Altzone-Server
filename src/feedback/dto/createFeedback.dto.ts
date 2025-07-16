fc0eeb357c277b56dbfc0cd35464a4fabe6a4574
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-07-16 21:19:42 +0300

    Add examples to the Feedback.dto.ts fields.

--- src/feedback/dto/createFeedback.dto.ts
+++ src/feedback/dto/createFeedback.dto.ts
@@ -5,17 +5,23 @@ import { IsDate, IsString } from 'class-validator';
 export class CreateFeedbackDto {
   /**
    * Profile Id
+   * 
+   * @example "665af23e5e982f0013aa8899"
    */
   @IsString()
   profile_id: string;
   /**
    * Feedbacks text message
+   * 
+   * @example "This is a feedback message."
    */
   @IsString()
   text: string;
 
   /**
    * Feedbacks captured date
+   * 
+   * @example "2023-10-01T12:00:00Z"
    */
   @IsDate()
   capturedAt?: Date;
