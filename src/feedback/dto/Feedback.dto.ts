dfe98c4f69ac8c81886cebc599b2bc11f8086a3a
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-07-16 22:01:41 +0300

    Add example to the FeedbackDto fields

--- src/feedback/dto/Feedback.dto.ts
+++ src/feedback/dto/Feedback.dto.ts
@@ -5,6 +5,8 @@ import AddType from '../../common/base/decorator/AddType.decorator';
 export class FeedbackDto {
   /**
    * Feedbacks text message
+   * 
+   * * @example "This is a feedback message."
    */
   @IsString()
   text: string;
