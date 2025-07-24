03b287907d2b573b76cb38c9c274bd5e57bbfdea
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-07-24 22:08:25 +0300

    add test to ChatService.deleteChatMessageById() test suite

--- src/__tests__/chat/chatService/deleteChatMessageById.test.ts
+++ src/__tests__/chat/chatService/deleteChatMessageById.test.ts
@@ -5,9 +5,9 @@ import { ChatType } from '../../../chat/enum/chatMessageType.enum';
 import ChatBuilderFactory from '../data/chatBuilderFactory';
 import { env } from 'process';
 import { SEReason } from '../../../common/service/basicService/SEReason';
-import createMockSession from '../../common/MongooseSession/CreateMockSession';
 import { ChatMessage } from '../../../chat/schema/chatMessage.schema';
-import { Model } from 'mongoose';
+import { getNonExisting_id } from '../../test_utils/util/getNonExisting_id';
+import { Environment } from '../../../common/service/envHandler/enum/environment.enum';
 
 describe('ChatService.deleteChatMessageById() test suite', () => {
   let chatService: ChatService;
@@ -18,21 +18,10 @@ describe('ChatService.deleteChatMessageById() test suite', () => {
   const player1ID = new ObjectId();
   let clanChatToCreate1: ChatMessage;
 
-  const globalMessages = [];
   const clanMessages = [];
-  let model: Model<ChatMessage>;
-  let sessionMock: any;
 
   beforeAll(() => {
-    env.ENVIRONMENT = 'TESTING_SESSION';
-
-    const globalChatToCreate = chatMessageBuilder
-      .setType(ChatType.GLOBAL)
-      .setSenderId(new ObjectId())
-      .build();
-
-    globalMessages.push(globalChatToCreate);
-
+  
     clanChatToCreate1 = chatMessageBuilder
       .setType(ChatType.CLAN)
       .setSenderId(player1ID)
@@ -43,40 +32,26 @@ describe('ChatService.deleteChatMessageById() test suite', () => {
   });
 
   beforeEach(async () => {
+    env.ENVIRONMENT = Environment.TESTING_SESSION;
     await chatModel.deleteMany({});
     jest.clearAllMocks();
 
     chatService = await ChatModule.getChatService();
-
-    model = chatService.model;
-    sessionMock = createMockSession(model);
-    await chatModel.create([...clanMessages]);
   });
 
   it('Should delete a chat by Id', async () => {
     const chatResp = await chatModel.create(clanMessages);
-    //const chat = await chatModel.find({ clan_id: clan1ID });
     const existingChat = chatResp[0];
     const chatId = existingChat.id.toString();
-    // const chat = await chatModel.find({ clan_id: clan1ID });
-    //const chatId = chat[0].id.toString();
-
-    const deletedChat1 = await chatModel.findById(chatId);
-
+    
     const [message, err] = await chatService.deleteChatMessageById(chatId);
 
     const deletedChat = await chatModel.findById(chatId);
 
+    expect(message).toBeTruthy();
     expect(err).toBeNull();
-    expect(message).toBeUndefined();
-    expect(deletedChat1).toBeDefined();
     expect(deletedChat).toBeNull();
-    expect(sessionMock.startTransaction).toHaveBeenCalled();
-    expect(sessionMock.commitTransaction).toHaveBeenCalled();
-    expect(sessionMock.endSession).toHaveBeenCalled();
-    expect(sessionMock.startTransaction).toHaveBeenCalledTimes(1);
-    expect(sessionMock.commitTransaction).toHaveBeenCalledTimes(1);
-    expect(sessionMock.endSession).toHaveBeenCalledTimes(1);
+  
   });
 
   it('Should return with error if the ENVIRONMENT is NOT TESTING_SESSION', async () => {
@@ -90,4 +65,13 @@ describe('ChatService.deleteChatMessageById() test suite', () => {
     );
     expect(message).toBeFalsy();
   });
+
+  it('Should return ServiceError NOT_FOUND if the object with provided _id does not exist', async () => {
+      const nonExisting_id = getNonExisting_id();
+      const [wasDeleted, errors] =
+        await chatService.deleteChatMessageById(nonExisting_id);
+  
+      expect(wasDeleted).toBeFalsy();
+      expect(errors).toContainSE_NOT_FOUND();
+    });
 });
