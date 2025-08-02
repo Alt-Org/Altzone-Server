aabc39f9e44db6b5e4dbe7464f374af443eb4ab5
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-08-02 15:11:00 +0300

    change box schedule logic to use new session reset and box removal logic

--- src/box/box.scheduler.ts
+++ src/box/box.scheduler.ts
@@ -1,18 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import BoxCreator from './boxCreator';
 import { BoxService } from './box.service';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { InjectModel } from '@nestjs/mongoose';
-import { Box } from './schemas/box.schema';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class BoxScheduler {
-  constructor(
-    private readonly boxCreator: BoxCreator,
-    private readonly boxService: BoxService,
-    @InjectModel(Box.name) private readonly boxModel: Model<Box>,
-  ) {}
+  constructor(private readonly boxService: BoxService) {}
 
   /**
    * Resets testing sessions by deleting all boxes that have reached the end session stage.
@@ -34,8 +26,8 @@ export class BoxScheduler {
     for (const box of boxes) {
       if (box.boxRemovalTime <= currentTime.getTime()) {
         await this.boxService.deleteBox(box._id.toString());
-        continue;
       }
+
       if (box.sessionResetTime <= currentTime.getTime()) {
         await this.boxService.reset(box._id.toString());
       }
