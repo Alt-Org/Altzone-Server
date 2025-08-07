28d4406a41a87c87fa8d113b650360034f28adbd
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-08-07 19:49:00 +0300

    add default DB value for clan stall

--- src/clan/defaultValues/stall.ts
+++ src/clan/defaultValues/stall.ts
@@ -1,8 +1,26 @@
+import { Stall } from '../stall/stall.schema';
+
 /**
  * Function where to define stall related default values
  */
 export function getStallDefaultValues() {
   return {
     stallSlotPrice: 200,
+  };
+}
+
+/**
+ * Get default stall to be saved in DB
+ *
+ * @returns default stall
+ */
+export function getDefaultStall(): Stall {
+  return {
+    adPoster: {
+      border: '#000000',
+      colour: '#FFFFFF',
+      mainFurniture: '',
+    },
+    maxSlots: 10,
   };
 }
