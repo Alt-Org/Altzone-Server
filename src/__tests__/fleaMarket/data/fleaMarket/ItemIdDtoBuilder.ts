c8413a0cb311e6aa83f379b8417e85570fac7820
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-08-14 22:13:31 +0300

    Replace fleaMarketService.handleSellItem itemId param with ItemIdDto param. Fix the relevant tests

---
+++ src/__tests__/fleaMarket/data/fleaMarket/ItemIdDtoBuilder.ts
@@ -0,0 +1,23 @@
+import { ItemIdDto } from '../../../../fleaMarket/dto/itemId.dto';
+
+export default class ItemIdtoBuilder {
+  private readonly base: Partial<ItemIdDto> = {
+    item_id: '665af23e5e982f0013aa5566',
+    price: 300,
+  };
+
+  build(): ItemIdDto {
+    return { ...this.base } as ItemIdDto;
+  }
+
+  setItemId(itemId: string) {
+    this.base.item_id = itemId;
+    return this;
+  }
+  setPrice(price: number) {
+    this.base.price = price;
+    return this;
+  }
+
+  
+}
