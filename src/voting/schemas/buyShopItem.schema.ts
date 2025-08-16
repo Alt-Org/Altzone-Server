c0592465bc91cc40302d166e6c75a9fbd3bf7219
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-08-16 20:47:58 +0300

    Enhance BuyClanShopItemVoting schema by making shopItemName required

--- src/voting/schemas/buyShopItem.schema.ts
+++ src/voting/schemas/buyShopItem.schema.ts
@@ -1,11 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ItemName } from '../../clanInventory/item/enum/itemName.enum';
-
 import { Voting } from './voting.schema';
 
 @Schema()
 export class BuyClanShopItemVoting extends Voting {
-  @Prop({ type: String, enum: ItemName })
+  @Prop({ type: String, enum: ItemName, required: true })
   shopItemName: ItemName;
 }
 
