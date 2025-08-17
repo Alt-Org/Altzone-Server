84e773e81e7987198c57e0245e192092e633034d
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-08-17 16:52:40 +0300

    fix tests after changing selling voting logic

--- src/__tests__/voting/data/voting/FleaMarketItemVotingBuilder.ts
+++ src/__tests__/voting/data/voting/FleaMarketItemVotingBuilder.ts
@@ -3,17 +3,24 @@ import { VotingBuilder } from './VotingBuilder';
 import { FleaMarketItemVoting } from '../../../../voting/schemas/fleamarketItemVoting.schema';
 
 export class FleaMarketItemVotingBuilder extends VotingBuilder {
-  fleaMarketItem_id: string = new ObjectId().toString();
+  private fleaMarketItem_id: string = new ObjectId().toString();
+  private price = 0;
+
+  override build() {
+    return {
+      ...this.base,
+      fleaMarketItem_id: this.fleaMarketItem_id,
+      price: this.price,
+    } as FleaMarketItemVoting;
+  }
 
   setFleaMarketItem_id(_id: string) {
     this.fleaMarketItem_id = _id;
     return this;
   }
 
-  override build() {
-    return {
-      ...this.base,
-      fleaMarketItem_id: this.fleaMarketItem_id,
-    } as FleaMarketItemVoting;
+  setPrice(price: number) {
+    this.price = price;
+    return this;
   }
 }
