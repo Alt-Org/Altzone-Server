2cd89f1ae741c6e5bb69c04c6fec077407ec9ae6
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-09-17 21:48:25 +0300

    Add GameDataService.handleResultType() test suite, only files and services

---
+++ src/__tests__/gameData/gameDataService/handleResultType.test.ts
@@ -0,0 +1,25 @@
+import { RoomService } from '../../../clanInventory/room/room.service';
+import { ClanService } from '../../../clan/clan.service';
+import { PlayerService } from '../../../player/player.service';
+import GameDataModule from '../modules/gameData.module';
+import { GameEventsHandler } from '../../../gameEventsHandler/gameEventsHandler';
+
+describe('GameDataService.handleResultType() test suite', () => {
+  let playerService: PlayerService;
+  let clanService: ClanService;
+  let roomService: RoomService;
+  let gameEventsHandler: GameEventsHandler;
+
+  beforeEach(async () => {
+    playerService = await GameDataModule.getPlayerService();
+    clanService = await GameDataModule.getClanService();
+    roomService = await GameDataModule.getRoomService();
+    gameEventsHandler = await GameDataModule.getGameEventHandler();
+    jest.clearAllMocks();
+  });
+
+
+  it('Should emit a sync event and be received by a listener', (done) => {
+   
+  });
+});
--- src/__tests__/gameData/modules/gameData.module.ts
+++ src/__tests__/gameData/modules/gameData.module.ts
@@ -3,6 +3,10 @@ import GameDataCommonModule from './gameDataCommon';
 import { GameDataService } from '../../../gameData/gameData.service';
 import { ModelName } from '../../../common/enum/modelName.enum';
 import { GameSchema } from '../../../gameData/game.schema';
+import { PlayerService } from '../../../player/player.service';
+import { ClanService } from '../../../clan/clan.service';
+import { RoomService } from '../../../clanInventory/room/room.service';
+import { GameEventsHandler } from '../../../gameEventsHandler/gameEventsHandler';
 
 export default class GameDataModule {
   private constructor() {}
@@ -15,4 +19,26 @@ export default class GameDataModule {
   static getGameModel() {
     return mongoose.model(ModelName.GAME, GameSchema);
   }
+
+  static async getPlayerService() {
+      const module = await GameDataCommonModule.getModule();
+      return module.resolve(PlayerService);
+  }
+
+  static async getClanService() {
+      const module = await GameDataCommonModule.getModule();
+      return module.resolve(ClanService);
+  }
+
+  static async getRoomService() {
+      const module = await GameDataCommonModule.getModule();
+      return module.resolve(RoomService);
+  }
+
+  static async getGameEventHandler() {
+      const module = await GameDataCommonModule.getModule();
+      return module.resolve(GameEventsHandler);
+  }
+  
+
 }
