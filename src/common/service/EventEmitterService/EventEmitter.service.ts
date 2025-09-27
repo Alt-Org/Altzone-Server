7b45091b4b06ff324d4582a52a10a4e3c668778c
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-09-27 11:25:11 +0300

    Move the DailyTaskEvent emitter code to the common/EventEmitterService

---
+++ src/common/service/EventEmitterService/EventEmitter.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+/**
+ Service for emitt Server Events
+ */
+@Injectable()
+export default class EventEmitterService {
+  constructor(
+      private readonly eventEmitter: EventEmitter2,
+    ) {}
+
+  /**
+   * Emit a new daily task event
+   * @param player_Id id of the player
+   * @param message free text
+   * @param serverTaskName  name of the server task
+   */
+  public async EmittNewDailyTaskEvent(player_Id, message, serverTaskName)
+  {
+    await this.eventEmitter.emitAsync('newDailyTaskEvent', {
+          playerId: player_Id,
+          message,
+          serverTaskName: serverTaskName,
+        });
+  }  
+}
+
+  
