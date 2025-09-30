cd50fb194bd5abde707bb2548ed9eab515a1f3f7
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-09-30 13:02:50 +0300

    add teacher profile schema

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { TeacherController } from './teacher.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ModelName } from '../common/enum/modelName.enum';
+import { TeacherProfileSchema } from './teacher-profile.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ModelName.TEACHER_PROFILE, schema: TeacherProfileSchema },
+    ]),
+    AuthModule,
+  ],
   providers: [TeacherService],
   controllers: [TeacherController],
 })
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { TeacherProfile } from './teacher-profile.schema';
+import { Model } from 'mongoose';
+import BasicService from '../common/service/basicService/BasicService';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+  constructor(
+    @InjectModel(TeacherProfile.name) model: Model<TeacherProfile>,
+    jwtService: JwtService,
+  ) {
+    this.basicService = new BasicService(model);
+  }
+
+  readonly basicService: BasicService;
+}
