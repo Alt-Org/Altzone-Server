3ec1bf0f11b247b55cbd5914aeb57c05b471d554
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-09-30 13:43:06 +0300

    add register dto

---
+++ src/teacher/dto/register.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @ApiProperty({ uniqueItems: true })
+  @IsString()
+  @IsNotEmpty({ message: 'Username is required' })
+  @MinLength(3, { message: 'Username must be at least 3 charaters' })
+  @MaxLength(20, { message: 'Username must be at most 20 characters ' })
+  username: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  @MaxLength(32, { message: 'Password must be at most 32 characters' })
+  password: string;
+}
