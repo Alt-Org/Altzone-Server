dbf0ff2b393d6e0647d644269f7f0879a51a2ecc
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-10-01 21:02:41 +0300

    Add doc and examples to the teacher dtos

--- src/teacher/dto/access-token.dto.ts
+++ src/teacher/dto/access-token.dto.ts
@@ -1,6 +1,11 @@
 import { Expose } from 'class-transformer';
 
 export class AccessTokenDto {
+  /**
+   * Access token (JWT)
+   *
+   * @example eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlYWNoZXIxIiwiaWF0IjoxNjg3MjM3MjIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+   */
   @Expose()
   accessToken: string;
 }
--- src/teacher/dto/credentials.dto.ts
+++ src/teacher/dto/credentials.dto.ts
@@ -2,6 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CredentialsDto {
+  /**
+   * name of the teacher
+   *
+   * @example teacher1
+   */
   @ApiProperty({ uniqueItems: true })
   @IsString()
   @IsNotEmpty({ message: 'Username is required' })
@@ -9,6 +14,11 @@ export class CredentialsDto {
   @MaxLength(20, { message: 'Username must be at most 20 characters ' })
   username: string;
 
+/**
+   * Password of the teacher
+   *
+   * @example P@ssw0rd
+   */
   @IsString()
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(6, { message: 'Password must be at least 6 characters' })
