4d91762af0eae936c396f15f9be8092c58b32060
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-07 13:42:33 +0300

    add if statement so that the box_id is not added to box, teacher or subschemas where it's not needed

--- src/common/plugin/addBoxIdToSchema.plugin.ts
+++ src/common/plugin/addBoxIdToSchema.plugin.ts
@@ -1,4 +1,5 @@
 import { Schema } from 'mongoose';
+import { ModelName } from '../enum/modelName.enum';
 
 /**
  * Adds box_id field to a DB schema
@@ -6,6 +7,16 @@ import { Schema } from 'mongoose';
  * @param schema where box_id field to add
  */
 export function addBoxIdToSchemaPlugin(schema: Schema) {
+  const collection = schema.get('collection');
+  //TODO: Delete v2box later
+  if (
+    collection === ModelName.TEACHER_PROFILE ||
+    collection === ModelName.BOX ||
+    collection === 'v2Box' ||
+    !collection
+  )
+    return;
+
   schema.add({
     box_id: {
       type: String,
