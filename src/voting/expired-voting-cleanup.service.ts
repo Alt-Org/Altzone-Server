a517f615d139f0e128fab5a5ea09d71af607b278
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-13 22:24:09 +0300

    add cleanup service to handle deletion of old votings

---
+++ src/voting/expired-voting-cleanup.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { ModelName } from '../common/enum/modelName.enum';
+import { Voting } from './schemas/voting.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ExpiredVotingCleanupService {
+  constructor(
+    @InjectModel(ModelName.VOTING) private readonly model: Model<Voting>,
+  ) {}
+
+  @Cron(CronExpression.EVERY_3_HOURS)
+  async handleExpiredVotingCleanup() {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    await this.model.deleteMany({ endsOn: { $lt: oneWeekAgo } });
+  }
+}
