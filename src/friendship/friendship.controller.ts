65e331698da66cf11eb9d0e4ccbe896992a6272f
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-22 21:14:15 +0300

    add addFriend endpoint to friendship controller

--- src/friendship/friendship.controller.ts
+++ src/friendship/friendship.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { FriendshipService } from './friendship.service';
 import { LoggedUser } from '../common/decorator/param/LoggedUser.decorator';
 import { User } from '../auth/user';
 import { UniformResponse } from '../common/decorator/response/UniformResponse';
 import { ModelName } from '../common/enum/modelName.enum';
 import { FriendlistDto } from './dto/friend-list.dto';
+import { _idDto } from '../common/dto/_id.dto';
 
 @Controller('friendship')
 export class FriendshipController {
@@ -14,5 +15,15 @@ export class FriendshipController {
   @UniformResponse(ModelName.FRIENDSHIP, FriendlistDto)
   async getFriendlist(@LoggedUser() user: User) {
     return await this.service.getPlayerFriendlist(user.player_id);
+  }
+
+  @Post('add/:_id')
+  @UniformResponse(ModelName.FRIENDSHIP)
+  async addFriend(@Param() param: _idDto, @LoggedUser() user: User) {
+    return await this.service.basicService.createOne({
+      playerA: user.player_id,
+      playerB: param._id,
+      requester: user.player_id,
+    });
   }
 }
