5d1924663f89d14107de53fe851bedfb2536969c
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-22 22:03:01 +0300

    fix getFriendlist query

--- src/friendship/friendship.service.ts
+++ src/friendship/friendship.service.ts
@@ -10,6 +10,7 @@ import BasicService, {
 } from '../common/service/basicService/BasicService';
 import { PopulatedFriendship } from './type/populated-friendship.type';
 import { NotFoundServiceError } from './error/not-found.error';
+import { FriendlistDto } from './dto/friend-list.dto';
 
 @Injectable()
 export class FriendshipService {
@@ -28,38 +29,39 @@ export class FriendshipService {
       const friendships = await this.model
         .find({
           status: FriendshipStatus.ACCEPTED,
-          $or: [{ playerA: playerId, playerB: playerId }],
+          $or: [{ playerA: playerId }, { playerB: playerId }],
         })
         .populate([
           {
             path: 'playerA',
-            select: 'name avatar clan',
-            populate: { path: 'clan', select: 'name' },
+            select: 'name avatar clan_id',
+            populate: { path: ModelName.CLAN, select: 'name' },
           },
           {
             path: 'playerB',
-            select: 'name avatar clan',
-            populate: { path: 'clan', select: 'name' },
+            select: 'name avatar clan_id',
+            populate: { path: ModelName.CLAN, select: 'name' },
           },
         ])
-        .lean<PopulatedFriendship[]>({
-          transform: (doc) => {
-            const d = doc as PopulatedFriendship;
-            const me = playerId.toString();
-            const friend =
-              d.playerA._id.toString() === me ? d.playerB : d.playerA;
-            return {
-              id: friend._id,
-              name: friend.name,
-              avatar: friend.avatar,
-              clan: friend.clan?.name ?? null,
-            };
-          },
-        });
+        .lean<PopulatedFriendship[]>();
 
       if (!friendships.length) return NotFoundServiceError;
 
-      return [friendships, null];
+      const filtered: FriendlistDto[] = friendships.map((doc) => {
+        if (!doc.playerA || !doc.playerB) return null;
+        const me = playerId.toString();
+        const friend =
+          doc.playerA._id.toString() === me ? doc.playerB : doc.playerA;
+
+        return {
+          id: friend._id.toString(),
+          name: friend.name,
+          avatar: friend.avatar,
+          clan: friend.Clan?.name ?? null,
+        };
+      });
+
+      return [filtered as any, null];
     } catch (error) {
       const errors = convertMongooseToServiceErrors(error);
       return [null, errors];
