a2693538c126f98d7eb202c9c32f56e34829eddb
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-22 20:47:21 +0300

    add friendship schema to module

--- src/friendship/friendship.module.ts
+++ src/friendship/friendship.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FriendshipService } from './friendship.service';
 import { FriendshipController } from './friendship.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ModelName } from '../common/enum/modelName.enum';
+import { FriendshipSchema } from './friendship.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ModelName.FRIENDSHIP, schema: FriendshipSchema },
+    ]),
+  ],
   providers: [FriendshipService],
   controllers: [FriendshipController],
 })
