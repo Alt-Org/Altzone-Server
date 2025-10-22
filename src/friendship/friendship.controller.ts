c8b6dfbfdc29096d8d3b95a653302101a86a355e
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-22 20:48:52 +0300

    add get friendlist endpoint

--- src/friendship/friendship.controller.ts
+++ src/friendship/friendship.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { FriendshipService } from './friendship.service';
+import { LoggedUser } from '../common/decorator/param/LoggedUser.decorator';
+import { User } from '../auth/user';
+import { UniformResponse } from '../common/decorator/response/UniformResponse';
+import { ModelName } from '../common/enum/modelName.enum';
+import { FriendlistDto } from './dto/friend-list.dto';
 
 @Controller('friendship')
-export class FriendshipController {}
+export class FriendshipController {
+  constructor(private readonly service: FriendshipService) {}
+
+  @Get()
+  @UniformResponse(ModelName.FRIENDSHIP, FriendlistDto)
+  async getFriendlist(@LoggedUser() user: User) {
+    return await this.service.getPlayerFriendlist(user.player_id);
+  }
+}
