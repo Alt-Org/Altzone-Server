bd8bfb8a3ec74d8b51da6a715c8db1c28ae79484
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-23 11:02:21 +0300

    add docs

--- src/friendship/dto/friend-list.dto.ts
+++ src/friendship/dto/friend-list.dto.ts
@@ -1,16 +1,35 @@
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 import { AvatarDto } from '../../player/dto/avatar.dto';
 
 export class FriendlistDto {
   @Expose()
-  id: string;
+  /**
+   * player ID of the friend
+   *
+   * @example "60f7c2d9a2d3c7b7e56d01df"
+   */
+  player_id: string;
 
+  /**
+   * name of the frien
+   *
+   * @example "PlayJeri"
+   */
   @Expose()
   name: string;
 
+  /**
+   * Player's avatar
+   */
+  @Type(() => AvatarDto)
   @Expose()
-  avatar: AvatarDto;
+  avatar?: AvatarDto;
 
+  /**
+   * Name of the clan friend belongs to
+   *
+   * @example "MahtiSonnit"
+   */
   @Expose()
   clan: string;
 }
