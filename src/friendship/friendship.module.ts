1375306fb4567fc02e574a9a87e8ff3ff36844c1
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-10-26 12:10:19 +0200

    add player model and friendship notifier to module

--- src/friendship/friendship.module.ts
+++ src/friendship/friendship.module.ts
@@ -4,14 +4,17 @@ import { FriendshipController } from './friendship.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ModelName } from '../common/enum/modelName.enum';
 import { FriendshipSchema } from './friendship.schema';
+import FriendshipNotifier from './friendship.notifier';
+import { PlayerSchema } from '../player/schemas/player.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ModelName.FRIENDSHIP, schema: FriendshipSchema },
+      { name: ModelName.PLAYER, schema: PlayerSchema },
     ]),
   ],
-  providers: [FriendshipService],
+  providers: [FriendshipService, FriendshipNotifier],
   controllers: [FriendshipController],
 })
 export class FriendshipModule {}
