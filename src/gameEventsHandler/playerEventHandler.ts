fd4611eccb8b04624f1e123a3b41ec23f0a2ed6b
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-11-04 21:52:18 +0200

    Add transaction support to PlayerEventHandler

--- src/gameEventsHandler/playerEventHandler.ts
+++ src/gameEventsHandler/playerEventHandler.ts
@@ -3,20 +3,34 @@ import { PlayerRewarder } from '../rewarder/playerRewarder/playerRewarder.servic
 import { PlayerStatisticService } from '../statisticsKeeper/playerStatisticKeeper/playerStatisticKeeper.service';
 import { PlayerEvent } from '../rewarder/playerRewarder/enum/PlayerEvent.enum';
 import ServiceError from '../common/service/basicService/ServiceError';
+import {
+  cancelTransaction,
+  endTransaction,
+  InitializeSession,
+} from '../common/function/Transactions';
+import { Connection } from 'mongoose';
+import { InjectConnection } from '@nestjs/mongoose';
 
 @Injectable()
 export class PlayerEventHandler {
   constructor(
     private readonly playerRewarder: PlayerRewarder,
     private readonly playerStatistics: PlayerStatisticService,
+    @InjectConnection() private readonly connection: Connection,
   ) {}
 
   async handlePlayerEvent(
     player_id: string,
     event: PlayerEvent,
   ): Promise<[boolean, ServiceError[]]> {
-    await this.playerStatistics.updatePlayerStatistic(player_id, event);
-    await this.playerRewarder.rewardForPlayerEvent(player_id, event);
-    return [true, null];
+    const session = await InitializeSession(this.connection);
+
+    const [_, playerStatError] = await this.playerStatistics.updatePlayerStatistic(player_id, event);
+    if (playerStatError) return await cancelTransaction(session, playerStatError as ServiceError[]);
+
+    const [__, playerEventError] =await this.playerRewarder.rewardForPlayerEvent(player_id, event);
+    if (playerEventError) return await cancelTransaction(session, playerEventError);
+    
+    return await endTransaction(session);
   }
 }
