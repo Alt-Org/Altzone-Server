0ffd892cb761fdef56c4f103b02d62e0646e1f7c
Jeri Nurmi <nurmi.jeri@gmail.com>
2025-11-08 15:23:57 +0200

    add find clan for new player method

--- src/clan/join/join.service.ts
+++ src/clan/join/join.service.ts
@@ -15,6 +15,8 @@ import { ClanDto } from '../dto/clan.dto';
 import { IServiceReturn } from '../../common/service/basicService/IService';
 import ServiceError from '../../common/service/basicService/ServiceError';
 import { SEReason } from '../../common/service/basicService/SEReason';
+import { ClanDocument } from '../clan.schema';
+import { OnEvent } from '@nestjs/event-emitter';
 
 @Injectable()
 export class JoinService {
@@ -175,5 +177,24 @@ export class JoinService {
       },
     );
     await this.playerCounter.increaseByIdOnOne(clan_id);
+  }
+
+  /**
+   * Finds a clan and joins a newly created player to it.
+   *
+   * @param playerId _id of the player
+   */
+  @OnEvent('player.created')
+  async findClanForNewPlayer(playerId: string) {
+    const randomClan = await this.clanService.model
+      .aggregate<ClanDocument>([
+        { $match: { isOpen: true, playerCount: { $lt: 30 } } },
+        { $sample: { size: 1 } },
+      ])
+      .then((res) => res[0]);
+
+    if (!randomClan) return;
+
+    await this.joinClan(playerId, randomClan._id.toString());
   }
 }
