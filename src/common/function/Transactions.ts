d539cfc24cbfa45f87a81fa3663331e6b0e04330
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2025-11-08 21:40:31 +0200

    Extend transactions to handle to avoid the nested sessions.

--- src/common/function/Transactions.ts
+++ src/common/function/Transactions.ts
@@ -6,12 +6,17 @@ import { IServiceReturn } from '../service/basicService/IService';
  * Initializes and starts a database session for transactions.
  *
  * @param connection - Mongoose database connection.
+ * 
+ * @param openSession - (Optional) An already opened ClientSession to use.
  *
  * @returns A promise that resolves to the started database session.
  */
 export async function InitializeSession(
   connection: Connection,
+  openSession?: ClientSession,
 ): Promise<ClientSession> {
+  if (openSession) return openSession;
+
   const session = await connection.startSession();
   session.startTransaction();
   return session;
@@ -22,13 +27,15 @@ export async function InitializeSession(
  *
  * @param session - Started database session.
  * @param error - The error to be thrown.
- *
+ * @param openSession - (Optional) An already opened ClientSession to use.
  * @throws Will throw an unexpected service error.
  */
 export async function cancelTransaction(
   session: ClientSession,
   error: ServiceError[],
+  openSession?: ClientSession,
 ): Promise<IServiceReturn<any>> {
+  if (openSession) return [null, error];
   await session.abortTransaction();
   await session.endSession();
   return [null, error];
@@ -39,11 +46,12 @@ export async function cancelTransaction(
  *
  * @param session - Started database session.
  * @param returnValue - (Optional) Value to return upon successful transaction completion.
- *
+ * @param openSession - (Optional) An already opened ClientSession to use.
  * @returns A promise that resolves to a successful service return.
  */
 export async function endTransaction(
   session: ClientSession,
+  openSession?: ClientSession,
 ): Promise<IServiceReturn<true>>;
 export async function endTransaction<T>(
   session: ClientSession,
@@ -52,11 +60,18 @@ export async function endTransaction<T>(
 export async function endTransaction<T = true>(
   session: ClientSession,
   returnValue?: T,
+  openSession?: ClientSession,
 ): Promise<IServiceReturn<T | true>> {
+
+  const result: T | true =
+      typeof returnValue === 'undefined' ? (true as const) : returnValue;
+
+  if (openSession) {
+
+    return [result, null];
+  }
   await session.commitTransaction();
   await session.endSession();
 
-  const result: T | true =
-    typeof returnValue === 'undefined' ? (true as const) : returnValue;
   return [result, null];
 }
