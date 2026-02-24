c0796b9085be0ba74af3ee8d9e4cc5766825b94b
CapoMK25 <parisburner6969@gmail.com>
2026-02-24 11:07:22 +0200

    Configured migrate-mongo as the DB migrations tool

---
+++ migration-template.js
@@ -0,0 +1,15 @@
+/**
+ * @param db {import('mongodb').Db}
+ * @param client {import('mongodb').MongoClient}
+ */
+export const up = async (db, client) => {
+  // TODO: Write your migration logic here when applicable
+};
+
+/**
+ * @param db {import('mongodb').Db}
+ * @param client {import('mongodb').MongoClient}
+ */
+export const down = async (db, client) => {
+  // TODO: Write your rollback logic here when applicable
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -70,6 +70,7 @@ const authGuardClassToUse = isTestingSession() ? BoxAuthGuard : AuthGuard;
         return {
           uri: mongoString,
           dbName,
+          autoIndex: false,
         };
       },
     }),
