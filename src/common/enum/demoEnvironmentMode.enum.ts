8739b0071042056687992df8a2c5354ddb1c77af
constf03 <stefano.confalone03@gmail.com>
2026-04-17 13:45:18 +0300

    add common enum for identifying demo environment mode

---
+++ src/common/enum/demoEnvironmentMode.enum.ts
@@ -0,0 +1,18 @@
+/**
+ * Enum used for identifying the game's environment mode.
+ * Game Art Teaching Demo (default) or Open Game Demo for All.
+ *
+ * This environment flag must be respected across all relevant data:
+ * - users / accounts
+ * - clans
+ * - matches / battle results
+ * - leaderboards (including clan leaderboards)
+ * - inventory / assets
+ * - soulhome, jukebox, flea market, etc.
+ *
+ * Important: No query should ever mix data between environments.
+ */
+export enum DemoEnvironmentMode {
+  TEACHING_DEMO = 0,
+  OPEN_DEMO = 1,
+}
