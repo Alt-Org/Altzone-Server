249a1534e761119d8f4b1e2bc0b5d71af17e4b8c
constf03 <stefano.confalone03@gmail.com>
2026-05-13 15:31:30 +0300

    remove unused basicService

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -3,16 +3,12 @@ import { Action } from '../authorization/enum/action.enum';
 import { AdminProfileDto } from './dto/AdminProfile.dto';
 import { AdminService } from './admin.service';
 import { Authorize } from '../authorization/decorator/Authorize';
-import BasicService from '../common/service/basicService/BasicService';
 import { LoggedUser } from '../common/decorator/param/LoggedUser.decorator';
 import { User } from '../auth/user';
 
 @Controller('admin/demo/teaching')
 export class AdminController {
-  constructor(
-    private readonly adminService: AdminService,
-    private readonly basicService: BasicService,
-  ) {}
+  constructor(private readonly adminService: AdminService) {}
 
   @Post('reset')
   @Authorize({ action: Action.delete, subject: AdminProfileDto })
