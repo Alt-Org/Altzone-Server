f7336280282a05b7417f5add9be655c959487fc0
constf03 <stefano.confalone03@gmail.com>
2026-05-13 15:25:54 +0300

    move admin check to admin.service.ts, fix import paths

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -2,11 +2,9 @@ import { Controller, Post } from '@nestjs/common';
 import { Action } from '../authorization/enum/action.enum';
 import { AdminProfileDto } from './dto/AdminProfile.dto';
 import { AdminService } from './admin.service';
-import { Authorize } from 'src/authorization/decorator/Authorize';
-import BasicService from 'src/common/service/basicService/BasicService';
-import { LoggedUser } from 'src/common/decorator/param/LoggedUser.decorator';
-import { SEReason } from 'src/common/service/basicService/SEReason';
-import ServiceError from 'src/common/service/basicService/ServiceError';
+import { Authorize } from '../authorization/decorator/Authorize';
+import BasicService from '../common/service/basicService/BasicService';
+import { LoggedUser } from '../common/decorator/param/LoggedUser.decorator';
 import { User } from '../auth/user';
 
 @Controller('admin/demo/teaching')
@@ -19,22 +17,6 @@ export class AdminController {
   @Post('reset')
   @Authorize({ action: Action.delete, subject: AdminProfileDto })
   public async resetTeachingDemo(@LoggedUser() user: User): Promise<void> {
-    const { profile_id } = user;
-    const [profile, profileErrors] =
-      await this.basicService.readOneById<AdminProfileDto>(profile_id);
-
-    if (profileErrors) {
-      throw profileErrors;
-    }
-
-    if (!profile.isSystemAdmin) {
-      throw new ServiceError({
-        reason: SEReason.NOT_AUTHORIZED,
-        value: 403,
-        message: 'Only system admins can manually erase teaching demo data.',
-      });
-    }
-
-    await this.adminService.resetTeachingDemo();
+    await this.adminService.resetTeachingDemo(user.profile_id);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AdminProfileDto } from './dto/AdminProfile.dto';
 import { Clan } from '../clan/clan.schema';
-import { Profile } from '../profile/profile.schema';
-import { Player } from '../player/schemas/player.schema';
-import { Stock } from '../clanInventory/stock/stock.schema';
+import BasicService from '../common/service/basicService/BasicService';
 import { Environment } from '../common/enum/environment.enum';
+import { Player } from '../player/schemas/player.schema';
+import { Profile } from '../profile/profile.schema';
+import { SEReason } from '../common/service/basicService/SEReason';
+import ServiceError from '../common/service/basicService/ServiceError';
+import { Stock } from '../clanInventory/stock/stock.schema';
 
 @Injectable()
 export class AdminService {
@@ -14,9 +18,35 @@ export class AdminService {
     @InjectModel(Profile.name) private profileModel: Model<Profile>,
     @InjectModel(Player.name) private playerModel: Model<Player>,
     @InjectModel(Stock.name) private stockModel: Model<Stock>,
-  ) {}
+  ) {
+    this.profileService = new BasicService(profileModel);
+  }
 
-  async resetTeachingDemo(): Promise<void> {
+  public readonly profileService: BasicService;
+
+  public async resetTeachingDemo(
+    profile_id: string,
+  ): Promise<void | [null, ServiceError[]]> {
+    const [profile, profileErrors] =
+      await this.profileService.readOneById<AdminProfileDto>(profile_id);
+
+    if (profileErrors) {
+      return [null, profileErrors];
+    }
+
+    if (!profile.isSystemAdmin) {
+      return [
+        null,
+        [
+          new ServiceError({
+            reason: SEReason.NOT_AUTHORIZED,
+            message:
+              'Only system admins can manually erase teaching demo data.',
+          }),
+        ],
+      ];
+    }
+
     await this.clanModel.deleteMany({ environment: Environment.TEACHING_DEMO });
 
     await this.profileModel.deleteMany({
