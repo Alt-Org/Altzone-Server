30ee33e59bf633bf3f0f83c286f916738b19bb11
tickBit <marjukosk@gmail.com>
2026-05-28 12:45:48 +0300

    Update jukebox playlist

--- src/jukebox/jukebox.notifier.ts
+++ src/jukebox/jukebox.notifier.ts
@@ -3,21 +3,25 @@ import { NotificationResource } from '../common/service/notificator/enum/Notific
 import { NotificationStatus } from '../common/service/notificator/enum/NotificationStatus.enum';
 import NotificationSender from '../common/service/notificator/NotificationSender';
 import { SongChangeNotificationDto } from './dto/SongChangeNotification.dto';
+import { Jukebox } from './type/playlist';
 
 export default class JukeboxNotifier {
   private readonly group = NotificationGroup.CLAN;
   private readonly resource = NotificationResource.JUKEBOX;
 
-  /**
-   * Sends a notification about song change
-   * @param newSong - The current song payload to send
-   * @param clanId - Id of the clan to receive the notification
-   */
   async songChange(newSong: SongChangeNotificationDto, clanId: string) {
     const topic = `/clan/${clanId}/jukebox/song/update`;
     NotificationSender.buildNotification()
       .addGroup(this.group, clanId)
       .addResource(this.resource, 'song')
       .send(NotificationStatus.UPDATE, { topic, song: newSong });
+  }
+
+  async playlistUpdate(jukebox: Jukebox, clanId: string) {
+    const topic = `/clan/${clanId}/jukebox/playlist/update`;
+    NotificationSender.buildNotification()
+      .addGroup(this.group, clanId)
+      .addResource(this.resource, 'playlist')
+      .send(NotificationStatus.UPDATE, { topic, playlist: jukebox });
   }
 }
