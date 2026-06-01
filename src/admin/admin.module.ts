7acb4c33f5da75ca4d51a14a4235eca6948942e2
constf03 <stefano.confalone03@gmail.com>
2026-06-01 16:39:01 +0300

    add Soulhome and Game modules into AdminModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,16 +3,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { Clan, ClanSchema } from '../clan/clan.schema';
+import { Game, GameSchema } from '../gameData/game.schema';
 import { Profile, ProfileSchema } from '../profile/profile.schema';
 import { Player, PlayerSchema } from '../player/schemas/player.schema';
+import {
+  SoulHome,
+  SoulhomeSchema,
+} from '../clanInventory/soulhome/soulhome.schema';
 import { Stock, StockSchema } from '../clanInventory/stock/stock.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Clan.name, schema: ClanSchema },
+      { name: Game.name, schema: GameSchema },
       { name: Profile.name, schema: ProfileSchema },
       { name: Player.name, schema: PlayerSchema },
+      { name: SoulHome.name, schema: SoulhomeSchema },
       { name: Stock.name, schema: StockSchema },
     ]),
   ],
