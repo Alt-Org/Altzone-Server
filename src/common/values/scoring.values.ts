e2cd7ae23c926e0e7ddbca816826b21f1c1fc469
tickBit <marjukosk@gmail.com>
2026-08-24 10:45:55 +0300

    Add shared scoring file

---
+++ src/common/values/scoring.values.ts
@@ -0,0 +1,16 @@
+export const Score = {
+    
+    // battlepoints
+    BATTLE: {
+        WIN: 10,    // battle win (all kinds of battles)
+        LOSS: -5,   // battle loss (all kinds of battles)
+    },
+    
+    // points from daily tasks
+    DAILY_TASK: {
+        COMPLETED: 10,
+        COMPLETED_WITH_HINT: 5,
+        CANCELED: 0,
+    }
+    
+}
