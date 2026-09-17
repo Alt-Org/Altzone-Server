813f344b005d61f2f8272c7cd24829931aa5c28d
tickBit <marjukosk@gmail.com>
2026-09-17 09:45:40 +0300

    Add logic for daily task LETTING GO OF THE OLD

--- src/fleaMarket/fleaMarket.service.ts
+++ src/fleaMarket/fleaMarket.service.ts
@@ -41,6 +41,7 @@ import { ItemDto } from '../clanInventory/item/dto/item.dto';
 import StockNotifier from '../clanInventory/stock/stock.notifier';
 import { StockNotificationSource } from '../clanInventory/stock/type/stockNotificationPayload.type';
 import { StallResponse } from './stall/dto/stallResponse.dto';
+import { SellItemResult } from './enum/sellItemResult.enum';
 
 @Injectable()
 export class FleaMarketService {
@@ -192,7 +193,7 @@ export class FleaMarketService {
     sellFleaMarketItemDto: SellFleaMarketItemDto,
     clanId: string,
     playerId: string,
-  ): Promise<IServiceReturn<boolean>> {
+  ): Promise<IServiceReturn<SellItemResult>> {
     const [player, playerError] =
       await this.playerService.getPlayerById(playerId);
     if (playerError) return [null, playerError as ServiceError[]];
@@ -226,7 +227,7 @@ export class FleaMarketService {
     sellFleaMarketItemDto: SellFleaMarketItemDto,
     clanId: string,
     playerId: string,
-  ): Promise<IServiceReturn<boolean>> {
+  ): Promise<IServiceReturn<SellItemResult>> {
     const session = await this.connection.startSession();
     session.startTransaction();
 
@@ -282,7 +283,7 @@ export class FleaMarketService {
     await session.commitTransaction();
     await session.endSession();
 
-    return [true, null];
+    return [SellItemResult.VOTING_STARTED, null];
   }
 
   /**
@@ -324,7 +325,7 @@ export class FleaMarketService {
   private async sellDirectly(
     sellFleaMarketItemDto: SellFleaMarketItemDto,
     clanId: string,
-  ): Promise<IServiceReturn<boolean>> {
+  ): Promise<IServiceReturn<SellItemResult>> {
     const session = await this.connection.startSession();
     session.startTransaction();
 
@@ -352,7 +353,7 @@ export class FleaMarketService {
     await session.commitTransaction();
     await session.endSession();
 
-    return [true, null];
+    return [SellItemResult.SOLD_DIRECTLY, null];
   }
 
   /**
